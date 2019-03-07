5846338a5c06b6b668f7070edc974e697b5b3b95
Brian Retterer <bretterer@gmail.com>
2019-03-07 18:33:10 -0500

    WIP

--- packages/@okta/vuepress-theme-default/components/ChangeLog.vue
+++ packages/@okta/vuepress-theme-default/components/ChangeLog.vue
@@ -9,23 +9,37 @@
     <!-- End Content -->
 
     <!-- Begin Table Of Contents -->
-    <TableOfContents class="TableOfContents" :items="tableOfContentsItems"></TableOfContents>
+    <TableOfContents class="TableOfContents" :items="toc"></TableOfContents>
     <!-- End Table Of Contents -->
   </section>
 </template>
 
 <script>
+
+  import _ from "lodash"
 export default {
   components: {
     TableOfContents: () => import('./TableOfContents.vue')
   },
+
   computed: {
-    tableOfContentsItems () {
-      console.log(this.$page.headers)
-      return [
-        "1",
-        "2"
-      ]
+
+    toc () {
+      let pages = this.$site.pages
+        .filter(pages => pages.path.startsWith("/docs/change-log/2"))
+
+      pages =  _.orderBy(pages, 'path').reverse()
+
+      return Array.from(pages).map(page => {
+
+
+        return  {
+          level: page.headers[0].level,
+          title: page.headers[0].title,
+          href: '#'+page.headers[0].slug,
+          display: ''
+        }
+      })
     }
   }
 }
