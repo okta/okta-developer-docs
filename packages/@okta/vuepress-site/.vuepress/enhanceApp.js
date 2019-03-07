a2350f7e46f3556d9a4c56ca5bb6fdc0faa71048
Brian Retterer <bretterer@gmail.com>
2019-03-07 14:16:04 -0500

    Removes the need for Iframes in quickstart
    This registers all the components into Vue.  This ia a temp fix until github.com/vuejs/vuepress/issues/1173 is patched

--- packages/@okta/vuepress-site/.vuepress/enhanceApp.js
+++ packages/@okta/vuepress-site/.vuepress/enhanceApp.js
@@ -1,5 +1,10 @@
 var redirectsJson = require('./redirects.json');
+import pageComponents from '@internal/page-components'
 
-export default ({ router }) => {
+export default ({ router, Vue }) => {
   router.addRoutes(redirectsJson)
+
+  for (const [name, component] of Object.entries(pageComponents)) {
+    Vue.component(name, component)
+  }
 }
