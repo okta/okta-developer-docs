0069e5113f0f0c9a1e0dbb2e8720884835b53efc
Brian Retterer <brian.retterer@okta.com>
2019-03-20 21:18:30 -0400

    Adds the full headers list to the page properties within the AHL plugin

--- packages/@okta/vuepress-plugin-active-header-links/index.js
+++ packages/@okta/vuepress-plugin-active-header-links/index.js
@@ -1,10 +1,17 @@
-const { path } = require('@vuepress/shared-utils')
+const { path, extractHeaders } = require('@vuepress/shared-utils')
 
-module.exports = (options) => ({
+module.exports = (options, context) => ({
   clientRootMixin: path.resolve(__dirname, 'clientRootMixin.js'),
   define: {
     AHL_SIDEBAR_LINK_SELECTOR: options.sidebarLinkSelector || '.sidebar-link',
     AHL_HEADER_ANCHOR_SELECTOR: options.headerAnchorSelector || '.header-anchor',
     AHL_HEADER_TOP_OFFSET: options.headerTopOffset || 90
+  },
+  extendPageData ($page) {
+    $page.fullHeaders = extractHeaders(
+      $page._strippedContent,
+      ['h2', 'h3', 'h4'],
+      context.markdown
+    )
   }
 })
