ba0c0d42a6ecbef3daec7f98eaef756b692db8ed
Brian Retterer <brian.retterer@okta.com>
2019-03-21 10:55:06 -0400

    Adds Favicon

--- packages/@okta/vuepress-site/.vuepress/config.js
+++ packages/@okta/vuepress-site/.vuepress/config.js
@@ -6,7 +6,12 @@ module.exports = {
    */
   head: [
     ['script', { src: "https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/7.2.5/polyfill.min.js" }],
-    ['link', { rel: 'icon', href: '/assets/favicon/logo.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes:'180x180', href: '/favicon/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', type:"image/png", sizes:"32x32",  href: '/favicon/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type:"image/png", sizes:"16x16",  href: '/favicon/favicon-16x16.png' }],
+    ['link', { rel: 'manifest',  href: '/favicon/manifest.json' }],
+    ['link', { rel: 'mask-icon',  href: '/favicon/safari-pinned-tab.svg' }],
+    ['meta', { name: 'msapplication-config',  content: '/favicon/browserconfig.xml' }],
     ['link', { rel: 'stylesheet', href: 'https://developer.okta.com/sites/all/themes/developer/css/master.css' }],
     ['meta', { 'http-equiv': 'XA-UA-Compatible', content: 'IE=edge'}],
 
