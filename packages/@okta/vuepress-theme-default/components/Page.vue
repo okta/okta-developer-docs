543d297a067ef2ea9c4c5cf56f0702ee1c1ada82
Brian Retterer <brian.retterer@okta.com>
2019-03-20 23:24:19 -0400

    Rewrites some of the table of contents to remove complexity

--- packages/@okta/vuepress-theme-default/components/Page.vue
+++ packages/@okta/vuepress-theme-default/components/Page.vue
@@ -9,27 +9,18 @@
     <!-- End Content -->
 
     <!-- Begin Table Of Contents -->
-    <TableOfContents v-if="showToc" class="TableOfContents" :items="toc"></TableOfContents>
+    <TableOfContents v-if="showToc" class="TableOfContents"></TableOfContents>
     <!-- End Table Of Contents -->
   </section>
 </template>
 
 <script>
-import { resolveHeaders } from '../util/index'
 export default {
   components: {
     TableOfContents: () => import('./TableOfContents.vue'),
     Sidebar: () => import('./Sidebar.vue')
   },
 
-  data() {
-    return {
-      toc: []
-    }
-  },
-  mounted() {
-    window.addEventListener("load", this.getAllHeaders)
-  },
   computed: {
     showToc () {
       if (this.$page.frontmatter.showToc === false) {
@@ -42,23 +33,6 @@ export default {
 
       return true;
     }
-  },
-  methods: {
-    getAllHeaders(event) {
-      let headers = document.querySelectorAll('.PageContent-main>h1, .PageContent-main>h2, .PageContent-main>h3, .PageContent-main>h4.api.api-operation, .PageContent-main>h4:not(.api)')
-      this.toc = Array.from(headers).map(header => {
-        return  {
-          level: header.localName.split('h')[1] == '1' ? '2': header.localName.split('h')[1],
-          title: header.innerText,
-          href: '#'+header.id,
-          node: header,
-          display: ''
-        }
-      })
-    }
-  },
-  beforeDestroy() {
-    window.removeEventListener('load', this.getAllHeaders);
   }
 }
 </script>
