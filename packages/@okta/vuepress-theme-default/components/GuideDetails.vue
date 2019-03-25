3b0e9431e8de67ee84f00b535add2d8d9d4858c2
susanharper-okta <susan.harper@okta.com>
2019-03-25 16:30:39 -0600

    file structure and content for SPA

--- packages/@okta/vuepress-theme-default/components/GuideDetails.vue
+++ packages/@okta/vuepress-theme-default/components/GuideDetails.vue
@@ -1,19 +1,11 @@
 <template>
   <div class="guide-details">
-<<<<<<< HEAD
-    <InGuideNavigation
-=======
     <GuidesNavigation
->>>>>>> def39456ae42c7cf1d384dd04896a232fe4e4f39
       :sections="sections"
       :section="section"
       :lang="lang"
       :guide="guide" 
-<<<<<<< HEAD
-    />   
-=======
     />
->>>>>>> def39456ae42c7cf1d384dd04896a232fe4e4f39
     <Content :pageKey="componentKey" id="guide_content" />
   </div>
 </template>
@@ -23,11 +15,7 @@
     name: 'GuideDetails',
     props: [ 'componentKey', 'sections', 'guide', 'lang', 'section' ],
     components: {
-<<<<<<< HEAD
-      InGuideNavigation: () => import('../components/InGuideNavigation.vue'),
-=======
       GuidesNavigation: () => import('../components/GuidesNavigation.vue'),
->>>>>>> def39456ae42c7cf1d384dd04896a232fe4e4f39
     },
   }
 </script>
--- packages/@okta/vuepress-theme-default/components/GuidesNavigation.vue
+++ packages/@okta/vuepress-theme-default/components/GuidesNavigation.vue
@@ -1,11 +1,5 @@
 <template>
   <aside class="guides-navigation">
-<<<<<<< HEAD
-    Navigation amongst all guides
-    <ul>
-      <li v-for="guide in guides">
-        <a :href="guide.link">{{guide.title}}</a>
-=======
     <ul class="guides">
       <li v-for="someGuide in guides" :class="{ active: someGuide.name === guide}">
         <a :href="someGuide.link" class="guide">{{someGuide.title}}</a>
@@ -14,7 +8,6 @@
             <div class="highlight"><a :href="sec.makeLink(lang)">{{sec.title}}</a></div>
           </li>
         </ol>
->>>>>>> def39456ae42c7cf1d384dd04896a232fe4e4f39
       </li>
     </ul>
   </aside>
@@ -24,10 +17,7 @@
   import { findGuides } from '../util/guides';
   export default {
     name: 'GuidesNavigation',
-<<<<<<< HEAD
-=======
     props: [ 'sections', 'lang', 'section', 'guide' ],
->>>>>>> def39456ae42c7cf1d384dd04896a232fe4e4f39
     computed: { 
       guides() { 
         return findGuides({ pages: this.$site.pages });
