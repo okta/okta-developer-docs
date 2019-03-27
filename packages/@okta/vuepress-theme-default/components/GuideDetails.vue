88be02d8d2a3752111e5c1323bccc3e5f1d0bb41
susanharper-okta <susan.harper@okta.com>
2019-03-27 09:29:10 -0600

    removed spaces and removed InGuideNavigation.vue

--- packages/@okta/vuepress-theme-default/components/GuideDetails.vue
+++ packages/@okta/vuepress-theme-default/components/GuideDetails.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="guide-details">
-
     <GuidesNavigation
-
       :sections="sections"
       :section="section"
       :lang="lang"
       :guide="guide" 
-
     />
-
     <Content :pageKey="componentKey" id="guide_content" />
   </div>
 </template>
@@ -19,9 +15,7 @@
     name: 'GuideDetails',
     props: [ 'componentKey', 'sections', 'guide', 'lang', 'section' ],
     components: {
-
       GuidesNavigation: () => import('../components/GuidesNavigation.vue'),
-
     },
   }
 </script>
