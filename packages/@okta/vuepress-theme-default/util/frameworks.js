9b9bf1dbefe3e44d8e2684c19f29cc1e648233c5
Brett Ritter <45160689+brettritter-okta@users.noreply.github.com>
2019-04-03 11:14:06 -0700

    fix(guides): Updates framework icon options

--- packages/@okta/vuepress-theme-default/util/frameworks.js
+++ packages/@okta/vuepress-theme-default/util/frameworks.js
@@ -1,25 +1,33 @@
 const FRAMEWORK_TO_COMMON_NAME = { 
   js: 'javascript',
   reactjs: 'react',
-  nodejs: 'node',
+  // nodejs: 'node',
+  node: 'nodejs',
   vuejs: 'vue',
   '.net': 'dotnet',
+  net: 'dotnet',
 };
 
 const COMMON_NAME_TO_FANCY_NAME = { 
   javascript: 'JS',
   angular: 'Angular',
   react: 'React',
+  preact: 'Preact',
   vue: 'Vue.js',
   java: 'Java',
-  node: 'Node.js',
+  nodejs: 'Node.js',
   android: 'Android',
   ios: 'iOS',
+  osx: 'OSX',
   go: 'Go',
+  spring: 'Spring',
   dotnet: '.NET',
+  netcore: '.NET Core',
   php: 'PHP',
+  python: 'Python',
   rest: 'REST',
   reactnative: 'React Native',
+  xamarin: 'Xamarin',
 };
 
 const COMMON_NAME_TO_ICON_NAME = { 
