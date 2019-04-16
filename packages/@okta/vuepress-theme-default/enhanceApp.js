4ef13ffe396d97b6398aef280ecd09c34414c1b0
Brett Ritter <45160689+brettritter-okta@users.noreply.github.com>
2019-04-16 12:01:14 -0700

    fix(guides): Unblocks build from running; Shifts global components to be such

--- packages/@okta/vuepress-theme-default/enhanceApp.js
+++
@@ -1,7 +0,0 @@
-import StackSelector from './components/StackSelector';
-import NextSectionLink from './components/NextSectionLink';
-
-export default ({ Vue }) => {
-  Vue.component('StackSelector', StackSelector); // Force global register
-  Vue.component('NextSectionLink', NextSectionLink); // Force global register
-};
