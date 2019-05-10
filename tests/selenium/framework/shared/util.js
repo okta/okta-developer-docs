0f24d25a14814d63a990357133201b3de76d67ce
Wayne Earl <wayne.earl@okta.com>
2019-05-10 14:11:11 -0600

    add hash link scroll test

--- tests/selenium/framework/shared/util.js
+++ tests/selenium/framework/shared/util.js
@@ -69,3 +69,20 @@ util.fixUrl = function(baseUrl) {
   }
   return fixedUrl;
 };
+
+util.isInViewport = async function (elem) {
+  return await elem.getDriver().executeScript(
+    "var elem = arguments[0],                 " +
+    "  box = elem.getBoundingClientRect(),    " +
+    "  cx = box.left + box.width / 2,         " +
+    "  cy = box.top + box.height / 2,         " +
+    "  e = document.elementFromPoint(cx, cy); " +
+    "for (; e; e = e.parentElement) {         " +
+    "  if (e === elem)                        " +
+    "    return true;                         " +
+    "}                                        " +
+    "return false;                            "
+    , elem).then(isInViewport => {
+    return isInViewport;
+  });
+};
