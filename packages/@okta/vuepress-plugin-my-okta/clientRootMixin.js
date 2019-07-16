cecbb4268e3dbb6cd20333f5747f54bba1e54763
Brett Ritter <swiftone@swiftone.org>
2019-07-16 16:02:10 -0700

    Initial pass - still has clipping

--- packages/@okta/vuepress-plugin-my-okta/clientRootMixin.js
+++ packages/@okta/vuepress-plugin-my-okta/clientRootMixin.js
@@ -1,56 +1,96 @@
+const UNREPLACED_DOMAIN_MSG = `Replace this with your Okta domain. If you sign in to Okta, we’ll try to show your domain here automatically.`;
+const REPLACED_DOMAIN_MSG = `Your Okta domain is shown here because you recently signed in. Each customer sees their own domain.`;
+const DISPLAY_DOMAIN = 'https://{yourOktaDomain}';
+
+const counter = (() => { 
+  let count = 0;
+  return () => count++;
+})();
+
 export default {
   mounted() {
-    let iframeSrc = 'https://login.okta.com'
+    let iframeSrc = 'https://login.okta.com';
 
     if(window.location.hostname == 'localhost') {
-      iframeSrc = 'https://login.okta.io:8081'
+      iframeSrc = 'https://login.okta.io:8081';
     }
 
-    const iframe = document.createElement('iframe')
-    iframe.id = 'myOktaIFrame'
-    iframe.src = iframeSrc
-    iframe.style = 'display:none'
-    document.body.appendChild(iframe)
+    const iframe = document.createElement('iframe');
+    iframe.id = 'myOktaIFrame';
+    iframe.src = iframeSrc;
+    iframe.style = 'display:none';
+    document.body.appendChild(iframe);
 
-
-    window.addEventListener('load', () => {
-      this.getMyOktaAccounts()
-    })
-
-
-    window.addEventListener('message', this.receiveMessage, false)
+    window.addEventListener('load', this.getMyOktaAccounts.bind(this));
+    window.addEventListener('message', this.receiveMessage.bind(this));
   },
 
   methods: {
 
-
-    getMyOktaAccounts: () => {
-      document.querySelector('#myOktaIFrame').contentWindow.postMessage({messageType: 'get_accounts_json'}, document.querySelector('#myOktaIFrame').getAttribute('src'))
+    getMyOktaAccounts: function() {
+      document.querySelector('#myOktaIFrame').contentWindow.postMessage({messageType: 'get_accounts_json'}, document.querySelector('#myOktaIFrame').getAttribute('src'));
     },
 
-    receiveMessage: (event) => {
-      if (event.origin !== document.querySelector('#myOktaIFrame').getAttribute('src') || !event.data) {
-        return
-      }
+    wrapDomain: function({ domain, message }) { 
+      // const replacement = `<span class="has-tooltip"><abbr class="okta-domain">${domain}</abbr><span class="tooltip is-tooltip-top okta-domain-message" role="tooltip">${message}</span></span>`;
+      const fences = document.querySelectorAll('code:not(.wrapped-okta-domain)');
 
-      const accountsExist = event.data.length
-      if (!accountsExist) {
-        return;
-      }
+      fences.forEach( fence => {
+        const id = `domain-tooltip-${counter()}`;
+        const replacement = `
+          <span class="has-tooltip">
+            <abbr class="okta-domain" aria-describedby="${id}">${domain}</abbr>
+            <span id="${id}" class="tooltip is-tooltip-top okta-domain-message" role="tooltip">${message}</span>
+          </span>
+        `.replace(/>[$\s]*/mg, '>').trim();
+        fence.innerHTML = fence.innerHTML.replace(/https?:\/\/{yourOktaDomain}/gi, replacement);
+        fence.classList.add('wrapped-okta-domain'); // only add html to a given code block once
+      });
+    },
 
-      const domain = event.data[0].origin
+    replaceDomain: function ({ domain, message }) { 
+      const fences = document.querySelectorAll('code.wrapped-okta-domain');
+
+      fences.forEach( fence => {
+        fence.querySelectorAll('.okta-domain').forEach( abbr => {
+          abbr.innerText = domain;
+        });
+        fence.querySelectorAll('.okta-domain-message').forEach( tooltip => { 
+          tooltip.innerText = message;
+        });
+      });
+    },
+
+    receiveMessage: function (event) {
+      const fromMyOkta = event.origin === document.querySelector('#myOktaIFrame').getAttribute('src');
+      const hasData = !!event.data;
+      const accountsExist = hasData && event.data.length;
+      const hasDomain = fromMyOkta && hasData && accountsExist;
+
+      const message = hasDomain ? REPLACED_DOMAIN_MSG : UNREPLACED_DOMAIN_MSG;
+      const domain = hasDomain ? event.data[0].origin : DISPLAY_DOMAIN;
+      // const domain = 'http://mydomain.com';
+
       const myOktaAccountFound = new CustomEvent('myOktaAccountFound', {
         detail: {
           domain: domain
         }
       });
+
+      const reactOnceContentExists = ({ domain, message }) => { 
+        setTimeout( () => { 
+          const contentExists = document.querySelector('code');
+          if(contentExists) { 
+            this.replaceDomain({ domain, message }); // replace wrapped first to avoid double hit on first pass
+            this.wrapDomain({ domain, message });
+          } else {
+            reactOnceContentExists({ domain, message});
+          }
+        }, 100);
+      };
       window.dispatchEvent(myOktaAccountFound)
 
-      const fences = document.querySelectorAll("code")
-      fences.forEach((fence, index) => {
-        fences[index].innerHTML = fence.innerHTML.replace(/https?:\/\/{yourOktaDomain}/gi, domain)
-      })
+      reactOnceContentExists({ domain, message});
     }
-
   }
 }
