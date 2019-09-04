2d659854f6bbd4263c8b9699bce389cd1560f215
Moti Kagan <moti.kagan@okta.com>
2019-09-04 19:32:02 -0400

    Release 2019.09.0 (#655)
    * Ratelimit for OAuth2 public metadata (#560)
    
    * Ratelimit for OAuth2 public metadata
    
    * Changing to note formatting, and updating reference text
    
    * update text
    
    * Okta-242057: features api (#629)
    
    * Adding new features API page
    
    * OKTA-242057: features-api docs
    
    * Reformatting page
    
    * Continuing reformmating
    
    * some refactors
    
    * add postman collection
    
    * add postman collection to postman table
    
    * more refactors
    
    * rename lifecycle to status
    
    * add link obj
    
    * refactor
    
    * Adding concepts page, along with nav bar entry and diagram
    
    * More work
    
    * mock features and their description
    
    * More updates
    
    * Fixing grammar
    
    * Fixing typos
    
    * Acting on feedback
    
    * update lifecycle to status
    
    * Acting on feedback
    
    * Updating diagram
    
    * Fixing image comment
    
    * remove the deprecated error response
    
    * Creating release notes entry. (#650)
    
    * Creating release notes entry.
    
    * Correcting typo.
    
    * Incorporating Susan's edits.
    
    * Incorporating Susan's edits.
    
    * Incorporating Susan's edits.
    
    * Incorporating Jakub edit.
    
    * Fixing missed anchor link mistake. (#653)
    
    * Fixing missed anchor link mistake.
    
    * Correcting bugs pluralization.

--- packages/@okta/vuepress-site/.vuepress/nav/main.js
+++ packages/@okta/vuepress-site/.vuepress/nav/main.js
@@ -9,12 +9,13 @@ module.exports = [
       { title: 'Event Hooks', link: '/docs/concepts/event-hooks/'},
       { title: 'Events API Migration', link: '/docs/concepts/events-api-migration/'},
       { title: 'External Identity Providers', link: '/docs/concepts/identity-providers/'},
+      { title: 'Feature Lifecycle Management', link: '/docs/concepts/feature-lifecycle-management/'},
       { title: 'Inline Hooks', link: '/docs/concepts/inline-hooks/'},
       { title: 'Key Rotation', link: '/docs/concepts/key-rotation/'},
       { title: 'OAuth 2.0 & OIDC', link: '/docs/concepts/auth-overview/'},
       { title: 'Okta-Hosted Flows', link: '/docs/concepts/okta-hosted-flows/'},
       { title: 'Okta Organizations', link: '/docs/concepts/okta-organizations/'},
-      { title: 'Social Login', link: '/docs/concepts/social-login/'}   
+      { title: 'Social Login', link: '/docs/concepts/social-login/'}
     ]
   },
   {
