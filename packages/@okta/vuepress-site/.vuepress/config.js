23b03b2cd68b4ce7b9b7643da69afd92c1a53c8d
Hamza Essaoui <hamza.essaoui@okta.com>
2019-12-21 22:56:12 -0800

    Add banner through GTM and move Heap to GTM

--- packages/@okta/vuepress-site/.vuepress/config.js
+++ packages/@okta/vuepress-site/.vuepress/config.js
@@ -18,7 +18,7 @@ module.exports = {
     ['meta', { 'http-equiv': 'XA-UA-Compatible', content: 'IE=edge'}],
 
     /**
-     * Header scripts for typekit, GA, GTM, and Heap Analytics (WIP)
+     * Header scripts for typekit, GA, GTM (WIP)
      */
     ['script', {}, `
       // TypeKit
@@ -34,26 +34,14 @@ module.exports = {
       var isProduction = window.location.hostname === 'developer.okta.com';
       if (isProduction) {
 
-        // Google analytics
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-        ga('create', 'UA-15777010-3', 'auto');
-        ga('send', 'pageview');
-
         // START Google Tag Manager
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-TJ45R6');
+        })(window,document,'script','dataLayer','GTM-KXMLV58');
         // END Google Tag Manager
 
-        // Start Heap Analytics
-        window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
-        heap.load("3356162945");
-        // End Heap Analytics
       }
     `]
   ],
