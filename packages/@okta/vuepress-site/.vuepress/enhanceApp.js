75497fb73264dff69bf54a630d06119193d5c260
Brian Retterer <bretterer@gmail.com>
2020-01-05 20:50:07 -0500

    Adds Guides and Scss

--- packages/@okta/vuepress-site/.vuepress/enhanceApp.js
+++ packages/@okta/vuepress-site/.vuepress/enhanceApp.js
@@ -1,5 +1,10 @@
+import Guides from '@okta/vuepress-theme-prose/layouts/Guides.vue';
+
 export default ({ router, Vue }) => {
   router.addRoutes([
-    
+    {
+      path: '/docs/guides/:guide?/:framework?/:section?',
+      component: Guides,
+    },
   ]);
 }
