76e78f5bbc6900962996ba6ee8e3a166d99e9685
Brett Ritter <swiftone@swiftone.org>
2020-01-16 09:25:47 -0800

    removes dead code

--- packages/@okta/vuepress-site/.vuepress/scripts/convert-replacement-strings.js
+++ packages/@okta/vuepress-site/.vuepress/scripts/convert-replacement-strings.js
@@ -1,19 +1,3 @@
-// const thingsToReplace = { 
-//   /* KEYS HERE GET WRAPPED IN '-=OKTA_REPLACE_WITH_XXX=-'
-//    *
-//    * Changes WILL require restarting `yarn dev` :(
-//    */
-
-// /* VALUES BELOW THIS LINE INTENDED FOR EASY EDITING */
-//   WIDGET_VERSION: 3, // Leave as major version
-//   TEST_JUNK: 'this is a test replacement', // Leave for testing
-// /* VALUES ABOVE THIS LINE INTENDED FOR EASY EDITING */
-// };
-
-// // Magic to convert widget major version into specific latest version for that major version
-// const findLatestWidgetVersion = require('./findLatestWidgetVersion');
-// thingsToReplace.WIDGET_VERSION = findLatestWidgetVersion(thingsToReplace.WIDGET_VERSION || 'no widget major version given');
-
 const convertReplacementStrings = (thingsToReplace) => Object.entries(thingsToReplace)
   .map( ([searchFor, replaceWith]) => { 
     return {
