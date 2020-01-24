083c857f739facdf42b8180f92335bd6c03a1a89
Brian Retterer <bretterer@gmail.com>
2020-01-24 15:59:01 -0500

    Makes sidebar nav expandable when using file

--- packages/@okta/vuepress-theme-prose/components/Sidebar.vue
+++ packages/@okta/vuepress-theme-prose/components/Sidebar.vue
@@ -2,34 +2,34 @@
   <aside class="landing-navigation" :class="{active: sidebarActive}">
     <ul class="landing">
       <li :class="{overview: true}" v-if="!usingFile">
-          <router-link :to="overview.path">{{overview.title}}</router-link>
-        </li>
-        <li v-for="link in navigation" :key="link.title" :class="{subnav: showSublinks(link), open: $page.path.includes(link.path)}">
-          <div class="link-wrap">
-            <svg viewBox="0 0 320 512" v-if="showSublinks(link) && !$page.path.includes(link.path)">
-              <path d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"/>
-            </svg>
+        <router-link :to="overview.path">{{overview.title}}</router-link>
+      </li>
+      <li v-for="link in navigation" :key="link.title" :class="{subnav: showSublinks(link), open: $page.path.includes(link.path)}" @click.prevent="setSubnavOpen(link)">
+        <div class="link-wrap">
+          <svg viewBox="0 0 320 512" v-if="showSublinks(link) && !$page.path.includes(link.path)">
+            <path d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"/>
+          </svg>
 
-            <svg viewBox="0 0 320 512" v-if="showSublinks(link) && $page.path.includes(link.path)" class="open">
-              <path d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"/>
-            </svg>
+          <svg viewBox="0 0 320 512" v-if="showSublinks(link) && $page.path.includes(link.path)" class="open">
+            <path d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"/>
+          </svg>
 
-            <router-link v-if="link.path" :to="link.path">{{link.title}}</router-link>
-            <div v-else>{{link.title}}</div>
-          </div>
-          
-          <ol v-if="usingFile"  class="sections">
-            <li v-for="subLink in link.subLinks" :key="subLink.title">
-              <router-link :to="subLink.path" :class="{'router-link-active': isActive(subLink)}">{{subLink.title}}</router-link>
-            </li>
-          </ol>
+          <router-link v-if="link.path" :to="link.path">{{link.title}}</router-link>
+          <div class="is-link" v-else>{{link.title}}</div>
+        </div>
+        
+        <ol v-if="usingFile && (showSublinks(link) || subnavOpen == link)"  class="sections" >
+          <li v-for="subLink in link.subLinks" :key="subLink.title">
+            <router-link :to="subLink.path" :class="{'router-link-active': isActive(subLink), 'router-link-exact-active': isActive(subLink)}">{{subLink.title}}</router-link>
+          </li>
+        </ol>
 
-          <ol v-if="subLinks(link) && $page.path.includes(link.path) && !usingFile"  class="sections">
-            <li v-for="subLink in subLinks(link)" :key="subLink.title">
-              <router-link :to="subLink.path" :class="{active: isActive(subLink)}">{{subLink.title}}</router-link>
-            </li>
-          </ol>
-        </li>
+        <ol v-if="subLinks(link) && $page.path.includes(link.path) && !usingFile"  class="sections">
+          <li v-for="subLink in subLinks(link)" :key="subLink.title">
+            <router-link :to="subLink.path" :class="{active: isActive(subLink)}">{{subLink.title}}</router-link>
+          </li>
+        </ol>
+      </li>
     </ul>
   </aside>
   
@@ -47,6 +47,7 @@
     },
     data() {
       return {
+        subnavOpen: null,
         sidebarActive: false,
         usingFile: false,
         overview: {
@@ -160,10 +161,22 @@
         }
         if(link.subLinks) {
           return link.subLinks.find((subLink) => {
-            return this.$page.regularPath.includes(subLink.link)
+            console.log(subLink.path);
+            return this.$page.regularPath.includes(subLink.link) || this.$page.regularPath.includes(subLink.activeCheck)
           })
         }
         return false
+      },
+      setSubnavOpen: function (link) {
+        if(this.subnavOpen == null || this.subnavOpen != link) {
+          this.subnavOpen = link
+          return
+        }
+
+        if(this.subnavOpen == link) {
+          this.subnavOpen = null
+          return
+        }
       },
       expandSubNav: function (event) {
         const parent = event.target.parentElement;
