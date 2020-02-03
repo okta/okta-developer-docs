32434c4d4c5623de8779d15834baa538db35044e
Brian Retterer <bretterer@gmail.com>
2020-02-02 22:09:50 -0500

    Update sidebar to be more robust

--- packages/@okta/vuepress-site/.vuepress/nav/reference.js
+++ packages/@okta/vuepress-site/.vuepress/nav/reference.js
@@ -1,59 +1,44 @@
 // the navigation that is shown across the reference pages
 
 module.exports = [
-    {
-        title: "Overview",
-        link: "/docs/reference/"
+    { title: 'API Overview', link: '/docs/reference/api-overview/' },
+    { title: 'Sign in Your Users', link: '/docs/reference/api/oidc/', subLinksId: 'Sidebar_References', subLinks: [
+      { title: 'OpenID Connect & OAuth 2.0 API', link: '/docs/reference/api/oidc/'},
+      { title: 'Authentication', link: '/docs/reference/api/authn/'}
+    ]
     },
-    {
-        title: "Authentication API",
-        link: "/docs/reference/api/authn/"
+    { title: 'Manage Okta Resources', link: '/docs/reference/api/roles/',  subLinksId: 'Sidebar_Resources',  subLinks: [
+        { title: 'Administrator Roles', link: '/docs/reference/api/roles/'},
+        { title: 'Apps', link: '/docs/reference/api/apps/'},
+        { title: 'Authorization Servers', link: '/docs/reference/api/authorization-servers/'},
+        { title: 'Dynamic Client Registration', link: '/docs/reference/api/oauth-clients/'},
+        { title: 'Event Hooks', link: '/docs/reference/api/event-hooks/'},
+        { title: 'Factors', link: '/docs/reference/api/factors/'},
+        { title: 'Features', link: '/docs/reference/api/features/'},
+        { title: 'Groups', link: '/docs/reference/api/groups/'},
+        { title: 'Identity Providers', link: '/docs/reference/api/idps/'},
+        { title: 'Inline Hooks', link: '/docs/reference/api/inline-hooks/'},
+        { title: 'Linked Objects', link: '/docs/reference/api/linked-objects/'},
+        { title: 'Policy', link: '/docs/reference/api/policy/'},
+        { title: 'Schemas', link: '/docs/reference/api/schemas/'},
+        { title: 'Sessions', link: '/docs/reference/api/sessions/'},
+        { title: 'System Log', link: '/docs/reference/api/system-log/'},
+        { title: 'Templates', link: '/docs/reference/api/templates/'},
+        { title: 'Trusted Origins', link: '/docs/reference/api/trusted-origins/'},
+        { title: 'User Types', link: '/docs/reference/api/user-types/'},
+        { title: 'Users', link: '/docs/reference/api/users/'},
+        { title: 'Zones', link: '/docs/reference/api/zones/'}
+      ]
     },
-    {
-        title: "Design Principles",
-        link: "/docs/reference/api-overview/"
-    },
-    {
-        title: "Error Codes",
-        link: "/docs/reference/error-codes/"
-    },
-    {
-        title: "Management APIs",
-        links: [
-            { title: 'Administrator Roles', link: '/docs/reference/api/roles/'},
-            { title: 'Apps', link: '/docs/reference/api/apps/'},
-            { title: 'Authorization Servers', link: '/docs/reference/api/authorization-servers/'},
-            { title: 'Dynamic Client Registration', link: '/docs/reference/api/oauth-clients/'},
-            { title: 'Event Hooks', link: '/docs/reference/api/event-hooks/'},
-            { title: 'Factors', link: '/docs/reference/api/factors/'},
-            { title: 'Groups', link: '/docs/reference/api/groups/'},
-            { title: 'Identity Providers', link: '/docs/reference/api/idps/'},
-            { title: 'Inline Hooks', link: '/docs/reference/api/inline-hooks/'},
-            { title: 'Linked Objects', link: '/docs/reference/api/linked-objects/'},
-            { title: 'Policy', link: '/docs/reference/api/policy/'},
-            { title: 'Schemas', link: '/docs/reference/api/schemas/'},
-            { title: 'Sessions', link: '/docs/reference/api/sessions/'},
-            { title: 'System Log', link: '/docs/reference/api/system-log/'},
-            { title: 'Templates', link: '/docs/reference/api/templates/'},
-            { title: 'Trusted Origins', link: '/docs/reference/api/trusted-origins/'},
-            { title: 'Users', link: '/docs/reference/api/users/'},
-            { title: 'Zones', link: '/docs/reference/api/zones/'}
-          ]
-    },
-    {
-        title: "Okta Expression Language",
-        link: "/docs/reference/okta-expression-language/"
-    },
-    {
-        title: "OpenID Connect & OAuth 2.0 API",
-        link: "/docs/reference/api/oidc/"
-    },
-    {
-        title: "Postman Collections",
-        link: "/docs/reference/postman-collections/"
-    },
-    {
-        title: "Rate Limits",
-        link: "/docs/reference/rate-limits/"
-    }
-]
+    { title: 'Error Codes', link: '/docs/reference/error-codes/'},
+    { title: 'Import Hook', link: '/docs/reference/import-hook/'},
+    { title: 'SAML Hook', link: '/docs/reference/saml-hook/'},
+    { title: 'Registration Hook', link: '/docs/reference/registration-hook/'},
+    { title: 'Token Hook', link: '/docs/reference/token-hook/'},
+    { title: 'Okta Expression Language', link: '/docs/reference/okta-expression-language/'},
+    { title: 'Postman Collections', link: '/docs/reference/postman-collections/'},
+    { title: 'Rate Limits', link: '/docs/reference/rate-limits/'},
+    { title: 'Release Life Cycle', link: '/docs/reference/releases-at-okta/'},
+    { title: 'SCIM Protocol', link: '/docs/reference/scim/'},
+    { title: 'WebFinger', link: '/docs/reference/webfinger/'}
+  ]
---
+++ packages/@okta/vuepress-theme-prose/components/SidebarItem.vue
@@ -0,0 +1,53 @@
+<template>
+    <li :class="{'subnav': link.subLinks}">
+        <div class="link-wrap">
+            <svg viewBox="0 0 320 512" v-if="link.subLinks && !(imActive || iHaveChildrenActive)">
+                <path d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"/>
+            </svg>
+
+            <svg viewBox="0 0 320 512" v-if="link.subLinks && (imActive || iHaveChildrenActive)">
+                <path d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"/>
+            </svg>
+
+            <router-link :to="link.link" @click="setData">{{link.title}}</router-link>
+        </div>
+
+        <ul v-if="link.subLinks" class="sections" v-show="iHaveChildrenActive || imActive">
+            <SidebarItem v-for="(sublink, key) in link.subLinks" :key="key" :link="sublink" />
+        </ul>
+      </li>
+</template>
+
+<script>
+export default {
+    name: 'SidebarItem',
+    props: ['link'],
+    components: {
+      SidebarItem: () => import('../components/SidebarItem.vue'),
+    },
+    data() {
+        return {
+            imActive: false,
+            iHaveChildrenActive: false
+        }
+    },
+    mounted() {
+        this.setData();
+    },
+    watch: {
+        '$route': function() {
+            this.$nextTick(() => {
+                this.setData();
+            });
+        }
+    },
+    methods: {
+        setData: function() {
+            this.imActive = this.$page.regularPath == this.link.link;
+            this.iHaveChildrenActive = !!this.$children.find((child) => {
+                return child.imActive
+            });
+        }
+    }
+}
+</script>
