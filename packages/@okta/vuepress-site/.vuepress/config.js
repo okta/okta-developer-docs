ea611317ae403b941ec1070d9bd22033cf43bd4e
Brian Retterer <bretterer@gmail.com>
2020-02-20 10:35:37 -0500

    Adds browser-update.org code

--- packages/@okta/vuepress-site/.vuepress/config.js
+++ packages/@okta/vuepress-site/.vuepress/config.js
@@ -27,7 +27,16 @@ module.exports = {
      * Header scripts for typekit, GA, GTM (WIP)
      */
     ['script', {}, `
-      
+
+      var $buoop = {required:{e:-4,f:-3,o:-3,s:-1,c:-3},insecure:true,api:2020.02 };
+      function $buo_f(){
+        var e = document.createElement("script");
+        e.src = "//browser-update.org/update.min.js";
+        document.body.appendChild(e);
+        };
+        try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
+        catch(e){window.attachEvent("onload", $buo_f)
+      }
 
       var isProduction = window.location.hostname === 'developer.okta.com';
       if (isProduction) {
@@ -253,5 +262,5 @@ module.exports = {
       }
     }
   },
-  
+
 }
