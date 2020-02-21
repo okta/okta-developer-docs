5d4896ff70bc50ece565f615394369c5b3ea6f79
Laura Rodriguez <rdz.maria.laura@gmail.com>
2020-02-21 11:37:51 -0500

    Add missing values in prose frameworks.js. Fix #983.

--- packages/@okta/vuepress-theme-prose/util/frameworks.js
+++ packages/@okta/vuepress-theme-prose/util/frameworks.js
@@ -9,6 +9,7 @@ const FRAMEWORK_TO_COMMON_NAME = {
 };
 
 const COMMON_NAME_TO_FANCY_NAME = {
+  azure: 'Azure AD',
   javascript: 'JS',
   angular: 'Angular',
   react: 'React',
@@ -25,7 +26,8 @@ const COMMON_NAME_TO_FANCY_NAME = {
   dotnet: '.NET',
   netcore: '.NET Core',
   aspnet: 'ASP.NET',
-  aspnetcore: 'ASP.NET Core',
+  aspnetcore: 'ASP.NET Core 2.x',
+  aspnetcore3: 'ASP.NET Core 3.x',
   php: 'PHP',
   python: 'Python',
   rest: 'REST',
@@ -46,6 +48,7 @@ const COMMON_NAME_TO_ICON_NAME = {
   netcore: 'dotnet',
   aspnet: 'dotnet',
   aspnetcore: 'dotnet',
+  aspnetcore3: 'dotnet',
   springboot: 'spring',
   nodeexpress: 'nodejs'
 };
