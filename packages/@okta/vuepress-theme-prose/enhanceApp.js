2becba0363130dd8cf753997b8c5d4a23e060a0b
Brian Retterer <bretterer@gmail.com>
2020-06-25 11:20:50 -0400

    Updates

--- packages/@okta/vuepress-theme-prose/enhanceApp.js
+++ packages/@okta/vuepress-theme-prose/enhanceApp.js
@@ -17,7 +17,6 @@ export default ({
   Vue.use(VueBrowserUpdate, {
     options: {
       required: {e:-4,f:-3,o:-3,s:-1,c:-3},
-      container: document.body,
       text: "Your web browser ({brow_name}) is not supported. For the best site experience, we recommend updating your browser. <br> <a{up_but}>Update browser</a> <a{ignore_but}>Ignore</a>"
     },
   });
