fd853a5a80cdbb5c3abd83dd8ec63e3eb10cc9f4
Shuo Wu <wushuo2010@gmail.com>
2020-07-22 13:24:09 -0400

    refactor: remove activeCheck in nav/codePages config

--- packages/@okta/vuepress-site/.vuepress/nav/codePages.js
+++ packages/@okta/vuepress-site/.vuepress/nav/codePages.js
@@ -4,30 +4,30 @@ module.exports = [
   {
     title: "Mobile",
     subLinks: [
-      { title: 'Android', path: '/code/android/', activeCheck: '/code/android/'},
-      { title: 'iOS', path: '/code/ios/', activeCheck: '/code/ios/'},
-      { title: 'React Native', path: '/code/react-native/', activeCheck: '/code/react-native/'}
+      { title: 'Android', path: '/code/android/' },
+      { title: 'iOS', path: '/code/ios/' },
+      { title: 'React Native', path: '/code/react-native/' }
     ]
   },
   {
     title: "Front End",
     subLinks: [
-      { title: 'Angular', path: '/code/angular/', activeCheck: '/code/angular/'},
-      { title: 'Javascript', path: '/code/javascript/', activeCheck: '/code/javascript/'},
-      { title: 'React', path: '/code/react/', activeCheck: '/code/react/'},
-      { title: 'Vue', path: '/code/vue/', activeCheck: '/code/vue/'}
+      { title: 'Angular', path: '/code/angular/' },
+      { title: 'Javascript', path: '/code/javascript/' },
+      { title: 'React', path: '/code/react/' },
+      { title: 'Vue', path: '/code/vue/' }
     ]
   },
   {
     title: "Back End",
     subLinks: [
-      { title: '.Net', path: '/code/dotnet/aspnetcore/', activeCheck: '/code/dotnet/'},
-      { title: 'Go', path: '/code/go/', activeCheck: '/code/go/'},
-      { title: 'Java', path: '/code/java/', activeCheck: '/code/java/'},
-      { title: 'Node.js', path: '/code/nodejs/', activeCheck: '/code/nodejs/'},
-      { title: 'PHP', path: '/code/php/', activeCheck: '/code/php/'},
-      { title: 'Python', path: '/code/python/', activeCheck: '/code/python/'},
-      { title: 'REST', path: '/code/rest/', activeCheck: '/code/rest/'},
+      { title: '.Net', path: '/code/dotnet/aspnetcore/' },
+      { title: 'Go', path: '/code/go/' },
+      { title: 'Java', path: '/code/java/' },
+      { title: 'Node.js', path: '/code/nodejs/' },
+      { title: 'PHP', path: '/code/php/' },
+      { title: 'Python', path: '/code/python/' },
+      { title: 'REST', path: '/code/rest/' },
     ]
   }
 ]
