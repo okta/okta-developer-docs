134ee52bfd247d446913fec8cafa8b7338f16c57
Brian Demers <bdemers@apache.org>
2020-08-19 15:37:25 -0400

    Create vue component to mark page as needing the "classic ui"
    To use this tag add `<RequireClassicUI/>` at the top of the page.

---
+++ packages/@okta/vuepress-theme-prose/global-components/RequireClassicUI.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <blockquote>
+      <details class="custom-block details">
+        <summary>The <strong>Classic UI</strong> is required for this guide.</summary>
+        <p>
+          The top left of your Okta Admin Console should say <strong>Classic UI</strong>, if it says <strong>Developer Console</strong> click the dropdown to change it to <strong>Classic UI</strong>.
+        </p>
+      </details>
+    </blockquote>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'RequireClassicUI',
+  };
+</script>
