fb7336d5b88309acceb76d1c69fb4e4d9432ff1f
Brian Retterer <bretterer@gmail.com>
2020-09-15 14:22:12 -0400

    Removes old code and updates build script to reflect changes

--- packages/@okta/vuepress-theme-prose/enhanceApp.js
+++ packages/@okta/vuepress-theme-prose/enhanceApp.js
@@ -13,13 +13,4 @@ export default ({
   for (const [name, component] of Object.entries(pageComponents)) {
     Vue.component(name, component)
   }
-
-  Vue.use(VueBrowserUpdate, {
-    options: {
-      required: {e:-4,f:-3,o:-3,s:-1,c:-3},
-      container: document.body,
-      text: "Your web browser ({brow_name}) is not supported. For the best site experience, we recommend updating your browser. <br> <a{up_but}>Update browser</a> <a{ignore_but}>Ignore</a>"
-    },
-  });
-
 }
