9a78cbd51e483fd57495a9e89d590d00731efea8
Moti Kagan <moti.kagan@okta.com>
2020-09-21 15:14:37 -0400

    Changing name of guide to Quickstart: Signing in your first user.

--- packages/@okta/vuepress-site/.vuepress/nav/guides.js
+++ packages/@okta/vuepress-site/.vuepress/nav/guides.js
@@ -5,7 +5,7 @@ module.exports = [
   { 
     title: 'Basics', 
     subLinks: [
-      { title: 'QuickStart', guideName: 'quickstart' },
+      { title: 'Quickstart: Signing in your first user', guideName: 'quickstart' },
       { title: 'Create an API token', guideName: 'create-an-api-token' },
       { title: 'Enable CORS', guideName: 'enable-cors' },
       { title: 'Find your Okta domain', guideName: 'find-your-domain' },
