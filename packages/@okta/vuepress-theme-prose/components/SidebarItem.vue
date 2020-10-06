2d67130137229bb025245c9136ebd19c85003441
Oleksandr Pravosudko <oleksandr.pravosudko@okta.com>
2020-10-06 17:15:49 +0300

    fix hairy merge

--- packages/@okta/vuepress-theme-prose/components/SidebarItem.vue
+++ packages/@okta/vuepress-theme-prose/components/SidebarItem.vue
@@ -6,7 +6,7 @@
             </div>
 
             <div v-else>
-                <div class="is-link item-collapsable" @click="toggle">
+                <div :class="{'is-link':true, 'item-collapsable': true, 'router-link-active': iHaveChildrenActive}" @click="toggle">
                     <svg viewBox="0 0 320 512" v-if="link.subLinks && !iHaveChildrenActive">
                         <path d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"/>
                     </svg>
