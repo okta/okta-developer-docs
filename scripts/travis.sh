4f2466f4fb6867f1097093c8c88cc0fcd03293f1
Robert Damphousse <robert.damphousse.devex@okta.com>
2020-10-11 13:52:04 -0700

    Iterate
    with NFDEPLOY

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,40 +1,30 @@
 #!/bin/bash
 set -e
 
-# source "${0%/*}/helpers.sh"
+source "${0%/*}/helpers.sh"
 
-# export CHROME_HEADLESS=true
+export CHROME_HEADLESS=true
 
-# # Run the yarn install to pull in test dependencies
-# fold yarn_install yarn install
+# Run the yarn install to pull in test dependencies
+fold yarn_install yarn install
 
-# # Lint the markdown
-# #fold yarn_markdown_check yarn markdown-lint
+# Lint the markdown
+#fold yarn_markdown_check yarn markdown-lint
 
-# # Check the links
-# fold yarn_check_links yarn check-links
+# Check the links
+fold yarn_check_links yarn check-links
 
-# # Build site and Run tests
-# fold yarn_test yarn test
+# Build site and Run tests
+fold yarn_test yarn test
 
-
-echo "=======NETLIFY_SITE_ID"
-echo "$NETLIFY_SITE_ID"
-echo "=======TRAVIS_COMMIT_MESSAGE"
-echo "$TRAVIS_COMMIT_MESSAGE"
-echo "=======TRAVIS_COMMIT"
-echo "$TRAVIS_COMMIT"
-echo "=======TRAVIS_PULL_REQUEST_SHA"
-echo "$TRAVIS_PULL_REQUEST_SHA"
-git log -n1 --pretty=format:"%B" $TRAVIS_PULL_REQUEST_SHA
-echo "=======TRAVIS_PULL_REQUEST_BRANCH"
-echo "$TRAVIS_PULL_REQUEST_BRANCH"
-echo "======="
-
+# Will run the netlify deploy if:
+# - The site ID is present (will not be true on forks)
+# - The latest commit message on the PR includes the text NFDEPLOY
+#
+# Note: netflicy-cli expects that NETLIFY_AUTH_TOKEN is present
 if [ -n $NETLIFY_SITE_ID ] && git log -n1 --pretty=format:"%B" $TRAVIS_PULL_REQUEST_SHA | grep -q NFDEPLOY; then
-    echo "WILL DEPLOY"
-    # fold netlify_setup npm install -g netlify-cli
-    # fold netlify netlify deploy
+    fold netlify_setup npm install -g netlify-cli
+    fold netlify netlify deploy
 else
     echo "Will not use Netlify deploy"
 fi	
