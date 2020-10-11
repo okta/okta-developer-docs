d70c208487a1733057c3a656935bd17eb6ebe9e3
Robert Damphousse <robert.damphousse.devex@okta.com>
2020-10-11 12:54:05 -0700

    Another test
    with NFDEPLOY

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 set -e
 
-source "${0%/*}/helpers.sh"
+# source "${0%/*}/helpers.sh"
 
-export CHROME_HEADLESS=true
+# export CHROME_HEADLESS=true
 
-# Run the yarn install to pull in test dependencies
-fold yarn_install yarn install
+# # Run the yarn install to pull in test dependencies
+# fold yarn_install yarn install
 
-# Lint the markdown
-#fold yarn_markdown_check yarn markdown-lint
+# # Lint the markdown
+# #fold yarn_markdown_check yarn markdown-lint
 
-# Check the links
-fold yarn_check_links yarn check-links
+# # Check the links
+# fold yarn_check_links yarn check-links
 
-# Build site and Run tests
-fold yarn_test yarn test
+# # Build site and Run tests
+# fold yarn_test yarn test
+
+
+echo "======="
+echo "$NETLIFY_SITE_ID"
+echo "======="
+echo "$TRAVIS_COMMIT_MESSAGE"
+echo "======="
 
 if [ -n $NETLIFY_SITE_ID ] && echo $TRAVIS_COMMIT_MESSAGE | grep -q NFDEPLOY; then
     fold netlify_setup npm install -g netlify-cli
