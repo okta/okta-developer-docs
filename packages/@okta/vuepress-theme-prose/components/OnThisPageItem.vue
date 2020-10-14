721a8759899c812eded6f4a6332b685e5eb640f6
Oleksandr Pravosudko <oleksandr.pravosudko@okta.com>
2020-10-14 12:47:02 +0300

    add scroll correction for non-100% screen scale

--- packages/@okta/vuepress-theme-prose/components/OnThisPageItem.vue
+++ packages/@okta/vuepress-theme-prose/components/OnThisPageItem.vue
@@ -51,11 +51,18 @@ export default {
       if(e.target.tagName.toLowerCase() === 'a') {
         hash = e.target.hash
       }
-    
+
       if(hash) {
         const node = document.querySelector(hash);
         if(node) { // node is sometimes null - perhaps content hasn't loaded?
-          window.scrollTo(0, node.offsetTop - document.querySelector('.fixed-header').clientHeight - 45);
+          const scrollToPosition = node.offsetTop - document.querySelector('.fixed-header').clientHeight - 45;
+          window.scrollTo(0, scrollToPosition);
+          // Chrome & Safari: when zoomed in/out, window.scrollTo does not always perform scroll strictly equal to passed parameter
+          // https://bugs.chromium.org/p/chromium/issues/detail?id=890345
+          if(window.scrollY < scrollToPosition) {
+            const scrollAlignment = 2;
+            window.scrollBy(0, scrollAlignment);
+          }
         }
       }
     }
