1309b50852d59c2fa1de508c70238032fd433dac
Brian Duffield - Okta <70648001+brianduffield-okta@users.noreply.github.com>
2020-12-15 10:22:10 -0500

    Update packages/@okta/vuepress-site/.vuepress/nav/guides.js
    Co-authored-by: Jakub Vul <jakub.vul@okta.com>

--- packages/@okta/vuepress-site/.vuepress/nav/guides.js
+++ packages/@okta/vuepress-site/.vuepress/nav/guides.js
@@ -103,7 +103,7 @@ module.exports = [
   {
     title: 'Hook Implementations',
     subLinks: [
-      { title: 'Overview and Considerations', guideName: 'overview-and-considerations'},
+      { title: 'Overview and considerations', guideName: 'overview-and-considerations'},
       { title: 'Event Hook', guideName: 'event-hook-implementation' },
       { title: 'Password Import Inline Hook', guideName: 'password-import-inline-hook' },
       { title: 'Registration Inline Hook', guideName: 'registration-inline-hook' }
