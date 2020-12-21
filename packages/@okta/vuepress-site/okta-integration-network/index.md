8191020eecc09df75f91cc62182bb6f4f8d91660
Shaw Yu <shaw.yu@okta.com>
2020-12-21 15:35:53 -0500

    add oin component, css files

---
+++ packages/@okta/vuepress-site/okta-integration-network/index.md
@@ -0,0 +1,9 @@
+---
+# This file controls the links and content shown on the /okta-integration-network landing page.
+
+component: OktaIntegrationNetwork
+title: Developer - Integrate with Okta
+
+showBreadcrumb: False
+
+---
---
+++ packages/@okta/vuepress-theme-prose/components/OktaIntegrationNetwork.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="page-content">
+    <section class="oin">
+      <div class="oin--container">
+        <div class="oin--leader">
+          <h1 class="oin--title">Integrate with Okta</h1>
+          <p class="oin--subtitle">
+            Publish your application to the <a href="https://www.okta.com/integrations">Okta Integration Network (OIN) catalog</a>, enabling millions of Okta users to discover your application and access it using single sign-on (SSO).
+          </p>
+          <p class="oin--subtitle">
+            Are you ready to join the OIN catalog?
+          </p>
+          <p class="oin--subtitle">
+            <a href="/signup" class="Button--red">Create Free Account</a>
+          </p>
+        </div>
+      </div>
+      <div class="oin--background-curve">
+        <img src="/img/curve-doubleLeft.png" />
+      </div>
+      <div class="oin-alt-bg">
+        <div class="oin--container">
+        </div>
+      </div>
+      <div class="oin--background-curve">
+        <img src="/img/curve-singleRightLight.png" />
+      </div>
+      <div class="oin--container">
+      </div>
+    </section>
+  </div>
+</template>
