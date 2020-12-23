62921b511db225b9b74ddafe3437f8e53a61d287
Shaw Yu <shaw.yu@okta.com>
2020-12-23 10:59:56 -0500

    add and style integration guides section

--- packages/@okta/vuepress-site/okta-integration-network/index.md
+++ packages/@okta/vuepress-site/okta-integration-network/index.md
@@ -6,4 +6,22 @@ title: Developer - Integrate with Okta
 
 showBreadcrumb: False
 
+guides:
+  - name: Enable Single Sign-On
+    icon: /img/icons/icon--saml.png
+    description: Federate using SAML or OIDC
+    links:
+      - text: What is SAML?
+        url: /docs/concepts/saml
+      - text: What is OIDC?
+        url: https://developer.okta.com/blog/2017/06/21/what-the-heck-is-oauth
+    docsUrl: /docs/guides/build-sso-integration/saml2/overview/
+  - name: Automate Provisioning
+    icon: /img/icons/icon--lock.svg
+    description: Synchronize user identities using SCIM
+    links:
+      - text: What is SCIM?
+        url: /docs/concepts/scim
+    docsUrl: /docs/guides/oin-build-provisioning-integration/overview/
+
 ---
--- packages/@okta/vuepress-theme-prose/components/OktaIntegrationNetwork.vue
+++ packages/@okta/vuepress-theme-prose/components/OktaIntegrationNetwork.vue
@@ -5,7 +5,7 @@
         <div class="oin--leader">
           <h1 class="oin--title">Integrate with Okta</h1>
           <p class="oin--subtitle">
-            Publish your application to the <a href="https://www.okta.com/integrations">Okta Integration Network (OIN) catalog</a>, enabling millions of Okta users to discover your application and access it using single sign-on (SSO).
+            Publish your application to the <a class="magentaLink" href="https://www.okta.com/integrations">Okta Integration Network (OIN) catalog</a>, enabling millions of Okta users to discover your application and access it using single sign-on (SSO).
           </p>
           <p class="oin--subtitle">
             Are you ready to join the OIN catalog?
@@ -20,12 +20,40 @@
       </div>
       <div class="oin-alt-bg">
         <div class="oin--container">
+          <div class="oin--integration">
+            <div class="oin--integration-header">
+              <h1>Integration Guides</h1>
+              <p>Use our step-by-step guides to build your integration.</p>
+            </div>
+            <div class="oin--integration-content">
+              <div v-for="guide in $page.frontmatter.guides" class="oin-guide">
+                <img :src="guide.icon" />
+                <h5>{{guide.name}}</h5>
+                <p>{{guide.description}}</p>
+                <p>
+                  <template v-for="(link, index) in guide.links">
+                    <span v-if="index > 0"> | </span>
+                    <a class="magentaLink" :href="link.url">{{link.text}}</a>
+                  </template>
+                </p>
+                <p>
+                  <a :href="guide.docsUrl" class="Button--redOutline">View Docs</a>
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div class="oin--background-curve">
         <img src="/img/curve-singleRightLight.png" />
       </div>
       <div class="oin--container">
+        <div class="oin--develop">
+          <h1>Are you ready to submit your integration for review?</h1>
+          <p class="oin--subtitle">
+            <a href="/docs/guides/submit-app/overview/" class="Button--red">Get Started</a>
+          </p>
+        </div>
       </div>
     </section>
   </div>
