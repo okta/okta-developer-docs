6651edd51dd682ec5145d054868b92c400c5da84
Volodymyr Makarchuk <vmakarchuk@LVL2009010.local>
2020-12-24 18:10:34 +0200

    NFDEPLOY

--- packages/@okta/vuepress-theme-prose/components/SidebarItem.vue
+++ packages/@okta/vuepress-theme-prose/components/SidebarItem.vue
@@ -1,75 +1,101 @@
 <template>
-    <li :class="{'subnav': link.subLinks, 'hidden':hidden}">
-        <div class="link-wrap">
-            <div v-if="link.path">
-                <router-link :to="link.path" exact @click="setData" class="tree-nav-link">{{link.title}}</router-link>
-            </div>
+  <li :class="{ subnav: link.subLinks, hidden: hidden }">
+    <div class="link-wrap">
+      <div v-if="link.path">
+        <router-link
+          :to="link.path"
+          exact
+          @click="setData"
+          class="tree-nav-link"
+          >{{ link.title }}</router-link
+        >
+      </div>
 
-            <div v-else>
-                <div :class="{'is-link':true, 'item-collapsable': true, 'router-link-active': iHaveChildrenActive}" @click="toggle">
-                    <svg viewBox="0 0 320 512" v-if="link.subLinks && !iHaveChildrenActive">
-                        <path d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"/>
-                    </svg>
+      <div v-else>
+        <div
+          :class="{
+            'is-link': true,
+            'item-collapsable': true,
+            'router-link-active': iHaveChildrenActive
+          }"
+          @click="toggle"
+        >
+          <svg
+            viewBox="0 0 320 512"
+            v-if="link.subLinks && !iHaveChildrenActive"
+          >
+            <path
+              d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"
+            />
+          </svg>
 
-                    <svg viewBox="0 0 320 512" v-if="link.subLinks && iHaveChildrenActive">
-                        <path d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"/>
-                    </svg>
-                    {{link.title}}
-                </div>
-            </div>
+          <svg
+            viewBox="0 0 320 512"
+            v-if="link.subLinks && iHaveChildrenActive"
+          >
+            <path
+              d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+            />
+          </svg>
+          {{ link.title }}
         </div>
+      </div>
+    </div>
 
-
-        <ul v-if="link.subLinks" class="sections" v-show="iHaveChildrenActive">
-            <SidebarItem v-for="sublink in link.subLinks" :key="sublink.title" :link="sublink" />
-        </ul>
-      </li>
+    <ul v-if="link.subLinks" class="sections" v-show="iHaveChildrenActive">
+      <SidebarItem
+        v-for="sublink in link.subLinks"
+        :key="sublink.title"
+        :link="sublink"
+      />
+    </ul>
+  </li>
 </template>
 
 <script>
 export default {
-    name: 'SidebarItem',
-    props: ['link'],
-    inject: ['appContext'],
-    components: {
-      SidebarItem: () => import('../components/SidebarItem.vue'),
+  name: "SidebarItem",
+  props: ["link"],
+  inject: ["appContext"],
+  components: {
+    SidebarItem: () => import("../components/SidebarItem.vue")
+  },
+  data() {
+    return {
+      iHaveChildrenActive: false,
+      hidden: !!this.link.hidden
+    };
+  },
+  mounted() {
+    this.setData();
+  },
+  watch: {
+    link() {
+      this.setData();
     },
-    data() {
-        return {
-            iHaveChildrenActive: false,
-            hidden: !!this.link.hidden
+    iHaveChildrenActive(isActivated, _) {
+      if (isActivated) {
+        // element.scrollIntoViewIfNeeded is not supported by Firefox
+        if (this.$el.scrollIntoViewIfNeeded) {
+          this.$el.scrollIntoViewIfNeeded();
+        } else {
+          this.$el.scrollIntoView({ block: "nearest" });
         }
+      }
     },
-    mounted() {
-        this.setData();
-    },
-    watch: {
-        'link'() {
-            this.setData();
-        },
-        'iHaveChildrenActive' (isActivated, _) {
-            if (isActivated) {
-                // element.scrollIntoViewIfNeeded is not supported by Firefox
-                if (this.$el.scrollIntoViewIfNeeded) {
-                    this.$el.scrollIntoViewIfNeeded();
-                } else {
-                    this.$el.scrollIntoView({block: 'nearest'});
-                }
-            }
-        },
-        'appContext.isTreeNavMobileOpen'(isOpen, _) {
-            if (isOpen && this.link.imActive && this.link.path) {
-                this.$el.scrollIntoView({block: 'center'});
-            }
-        }
-    },
-    methods: {
-        toggle() {
-            this.iHaveChildrenActive = !this.iHaveChildrenActive
-        },
-        setData: function() {
-            this.iHaveChildrenActive = Boolean(this.link.imActive);
-        }
+    "appContext.isTreeNavMobileOpen"(isOpen, _) {
+      if (isOpen && this.link.imActive && this.link.path) {
+        this.$el.scrollIntoView({ block: "center" });
+      }
     }
-}
+  },
+  methods: {
+    toggle() {
+      this.iHaveChildrenActive = !this.iHaveChildrenActive;
+    },
+    setData: function() {
+      this.iHaveChildrenActive = Boolean(this.link.imActive);
+    }
+  }
+};
 </script>
