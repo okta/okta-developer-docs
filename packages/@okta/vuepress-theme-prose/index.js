586f9a667a1104437e7a7a266aaca3d26dbf919d
Volodymyr Makarchuk <vmakarchuk@LVL2009010.local>
2020-12-28 20:46:22 +0200

    add accidentally removed index.js

---
+++ packages/@okta/vuepress-theme-prose/index.js
@@ -0,0 +1,53 @@
+module.exports = (options, ctx) => {
+  const { themeConfig, siteConfig } = ctx
+  return {
+    plugins: [
+      ['@vuepress/last-updated', {
+        transformer: (timestamp, lang) => {
+          const moment = require('moment')
+          moment.locale(lang)
+          return moment(timestamp).format('LL')
+        }
+      }]
+    ],
+    extendPageData ($page) {
+      $page.fullHeaders = resolveHeaders($page);
+
+      // Set to true to enable the redesign in development
+      const redesign = process.env.REDESIGN || false;
+      // Respect the redesign flag unless this is a production build
+      $page.redesign = true //!ctx.isProd && redesign;
+    }
+  }
+}
+
+function resolveHeaders (page) {
+  const headers = groupHeaders(page.headers || [])
+  return [{
+    type: 'group',
+    collapsable: false,
+    title: page.title,
+    path: null,
+    children: headers.map(h => ({
+      type: 'auto',
+      title: h.title,
+      basePath: page.path,
+      path: page.path + '#' + h.slug,
+      children: h.children || []
+    }))
+  }]
+}
+
+function groupHeaders (headers) {
+  // group h3s under h2
+  headers = headers.map(h => Object.assign({}, h))
+  let lastH2
+  headers.forEach(h => {
+    if (h.level === 2) {
+      lastH2 = h
+    } else if (lastH2) {
+      (lastH2.children || (lastH2.children = [])).push(h)
+    }
+  })
+  return headers.filter(h => h.level === 2)
+}
