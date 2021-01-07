4905fa497120a12c6a6b91e878815367a2166fd0
Shaw Yu <shaw.yu@okta.com>
2021-01-07 13:34:02 -0500

    whitespace and style nits for frontpagewidget component

--- packages/@okta/vuepress-theme-prose/components/FrontPageWidget.vue
+++ packages/@okta/vuepress-theme-prose/components/FrontPageWidget.vue
@@ -3,72 +3,72 @@
 </template>
 
 <script>
-import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css'
-  export default {
-    name: 'FrontPageWidget',
-    mounted: function () {
-    import('@okta/okta-signin-widget').then(
-      module =>{
-            this.$nextTick(function () {
-                    const OktaSignIn = module.default
-                    this.widget = new OktaSignIn({
-                    baseUrl: 'https://{yourOktaDomain}',
-                    logo: '/img/homepage/alliance.png',
-                    username: 'leia@rebelalliance.io',
-                    processCreds: (creds, callback) => {
-                      console.log('PROCES CRED')
-                      if (creds.username === 'leia@rebelalliance.io' && creds.password === 'secret') {
-                        this.$emit('authLeia')
-                      } else {
-                        callback();
-                      }
-                    }, 
-                    helpLinks: {
-                          help: 'https://developer.okta.com/code/javascript/okta_sign-in_widget'
-                    },
-                    i18n: {
-                        en: {
-                          "primaryauth.title": "Alliance Authentication",
-                          "primaryauth.submit": "Sign In"
-                        },
-                    },
-                  })
-                  this.widget.on('afterRender', () => {
-                            if (!this.rendered) {
-                              this.widget.hide();
+import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
 
-                              // Prefill password input
-                              const password = document.getElementById('okta-signin-password');
-                              if (password) {
-                                password.setAttribute('value', 'secret');
-                                password.dispatchEvent(new Event('input', { 'bubbles': true } ));
-                              }
+export default {
+  name: 'FrontPageWidget',
+  mounted: function() {
+    import('@okta/okta-signin-widget').then(module => {
+      this.$nextTick(function() {
+        const OktaSignIn = module.default;
 
-                              setTimeout(() => {
-                                this.widget.show();
-                                this.rendered = true;
-                              }, 0);
-                            } else {
-                              // Last focused element to return to
-                              const elementToFocus = (document.activeElement);
-                              setTimeout(() => {
-                                const activeElement = (document.activeElement);
-                                if (activeElement.id === 'okta-signin-password') {
-                                  activeElement.blur();
-                                  elementToFocus.focus();
-                                }
-                              }, 100);
-                            }
-                          });
-                  this.widget.renderEl(
-                    { el: '#okta-sign-in' },
-                  )
-                })
-      }
-    )
+        this.widget = new OktaSignIn({
+          baseUrl: 'https://{yourOktaDomain}',
+          logo: '/img/homepage/alliance.png',
+          username: 'leia@rebelalliance.io',
+          processCreds: (creds, callback) => {
+            if (creds.username === 'leia@rebelalliance.io' && creds.password === 'secret') {
+              this.$emit('authLeia');
+            } else {
+              callback();
+            }
+          }, 
+          helpLinks: {
+            help: 'https://developer.okta.com/code/javascript/okta_sign-in_widget',
+          },
+          i18n: {
+            en: {
+              "primaryauth.title": "Alliance Authentication",
+              "primaryauth.submit": "Sign In"
+            },
+          },
+        });
+
+        this.widget.on('afterRender', () => {
+          if (!this.rendered) {
+            this.widget.hide();
+
+            // Prefill password input
+            const password = document.getElementById('okta-signin-password');
+            if (password) {
+              password.setAttribute('value', 'secret');
+              password.dispatchEvent(new Event('input', { 'bubbles': true } ));
+            }
+
+            setTimeout(() => {
+              this.widget.show();
+              this.rendered = true;
+            }, 0);
+          } else {
+            // Last focused element to return to
+            const elementToFocus = (document.activeElement);
+            setTimeout(() => {
+              const activeElement = (document.activeElement);
+
+              if (activeElement.id === 'okta-signin-password') {
+                activeElement.blur();
+                elementToFocus.focus();
+              }
+            }, 0);
+          }
+        });
+
+        this.widget.renderEl({ el: '#okta-sign-in' });
+      });
+    });
   },
-    destroyed () {
-     this.widget ? this.widget.remove() : null
-    }
-  }
+  destroyed () {
+   this.widget ? this.widget.remove() : null;
+  },
+};
 </script>
