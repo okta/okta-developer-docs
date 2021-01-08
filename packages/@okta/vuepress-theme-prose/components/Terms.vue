3c9dac6c09bfbdfdbd65d5c9faec26feff56f072
Shaw Yu <shaw.yu@okta.com>
2021-01-08 11:15:41 -0500

    add terms page and content

---
+++ packages/@okta/vuepress-theme-prose/components/Terms.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="page-content">
+    <section class="terms">
+      <div class="terms--container">
+        <div class="terms--leader">
+          <h1 class="terms--title">Terms</h1>
+        </div>
+      </div>
+      <div class="terms--container">
+        <div class="terms--text">
+          <section v-for="(section, sectionIdx) in $page.frontmatter.terms" v-bind:key="sectionIdx">
+            <h2 class="terms--section-title" v-if="section.heading">
+              {{section.heading}}
+            </h2>
+            <p class="terms--section-paragraph" v-for="(paragraph, paragraphIdx) in section.paragraphs" v-bind:key="paragraphIdx">
+              {{paragraph}}
+            </p>
+          </section>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
