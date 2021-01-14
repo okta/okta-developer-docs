4e2dc4f765a474424b6d752109292b350e5cc80c
Shaw Yu <shaw.yu@okta.com>
2021-01-14 11:28:37 -0500

    typo

--- packages/@okta/vuepress-theme-prose/global-components/SignUp.vue
+++ packages/@okta/vuepress-theme-prose/global-components/SignUp.vue
@@ -238,7 +238,7 @@ export default {
   },
   data() {
     return {
-      state: { lable: "", list: [] },
+      state: { label: "", list: [] },
       displayConsent: false,
       displayAgree: false,
       form: {
