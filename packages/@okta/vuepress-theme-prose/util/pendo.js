742bf3890c2bae338cb005ee7fec08bb681ebf0d
Andrii Dovhopolov <andrii.dovhopolov@okta.com>
2021-01-18 18:30:11 +0200

    delete redundant conf

--- packages/@okta/vuepress-theme-prose/util/pendo.js
+++ packages/@okta/vuepress-theme-prose/util/pendo.js
@@ -4,31 +4,8 @@ export default (function(apiKey){
       o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
       y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
       z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
-
-      // Call this whenever information about your visitors becomes available
-      // Please use Strings, Numbers, or Bools for value types.
       pendo.initialize({
-          visitor: {
-              // id:              'VISITOR-UNIQUE-ID'   // Required if user is logged in
-              // email:        // Recommended if using Pendo Feedback, or NPS Email
-              // full_name:    // Recommended if using Pendo Feedback
-              // role:         // Optional
-
-              // You can add any additional visitor level key-values here,
-              // as long as it's not one of the above reserved names.
-          },
-
-          account: {
-              // id:           'ACCOUNT-UNIQUE-ID' // Highly recommended
-              // name:         // Optional
-              // is_paying:    // Recommended if using Pendo Feedback
-              // monthly_value:// Recommended if using Pendo Feedback
-              // planLevel:    // Optional
-              // planPrice:    // Optional
-              // creationDate: // Optional
-
-              // You can add any additional account level key-values here,
-              // as long as it's not one of the above reserved names.
-          }
+          visitor: {},
+          account: {}
       });
 })('f8bd2822-002a-478f-66a9-0178efd7ee1f');
