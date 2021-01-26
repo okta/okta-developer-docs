298f4b1ab80a6e5665d99957bd1cd2e5277eef5f
Shaw Yu <shaw.yu@okta.com>
2021-01-26 13:40:34 -0500

    Adds /cla page (#1849)
    * adds cla page
    
    * NFDEPLOY
    
    * update md comment
    
    * formatting
    
    * move cla pdf into static dir and update links

--- packages/@okta/vuepress-theme-prose/components/Terms.vue
+++ packages/@okta/vuepress-theme-prose/components/Terms.vue
@@ -3,20 +3,35 @@
     <section class="terms">
       <div class="terms--container">
         <div class="terms--leader">
-          <h1 class="terms--title">Terms</h1>
+          <h1 class="terms--title">
+            {{$page.frontmatter.heading}}
+          </h1>
         </div>
       </div>
       <div class="terms--container">
         <div class="terms--text">
           <section v-for="(section, sectionIdx) in $page.frontmatter.terms" v-bind:key="sectionIdx">
-            <h2 class="terms--section-title" v-if="section.heading">
+            <component
+              :is="section.tag || 'h2'"
+              class="terms--section-title"
+              v-if="section.heading">
               {{section.heading}}
-            </h2>
-            <p class="terms--section-paragraph"
-              v-for="(paragraph, paragraphIdx) in section.paragraphs"
-              v-bind:key="paragraphIdx"
-              v-html="paragraph">
-            </p>
+            </component>
+            <template v-for="(paragraph, paragraphIdx) in section.paragraphs">
+              <ul
+                v-if="Array.isArray(paragraph)"
+                v-bind:key="paragraphIdx">
+                <li v-for="(bullet, bulletIdx) in paragraph"
+                    v-bind:key="bulletIdx"
+                    v-html="bullet">
+                </li>
+              </ul>
+              <p class="terms--section-paragraph"
+                v-bind:key="paragraphIdx"
+                v-html="paragraph"
+                v-else>
+              </p>
+            </template>
           </section>
         </div>
       </div>
