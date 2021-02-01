d2687f593e3bf7a8b2638246dde902f3417e86b7
Shaw Yu <shaw.yu@okta.com>
2021-02-01 16:42:37 -0500

    Catch unmatched paths in router and display custom 404 (#1877)

--- packages/@okta/vuepress-theme-prose/enhanceApp.js
+++ packages/@okta/vuepress-theme-prose/enhanceApp.js
@@ -11,5 +11,15 @@ export default ({
 }) => {
   Vue.component('v-select', VueSelect);
 
+  router.beforeEach((to, from, next) => {
+    /**
+     * Catch `404` in router and redirect to custom 404 page
+     */
+    if (to.matched.length > 0 && to.matched[0].path === "*") {
+      next("/errors/404.html");
+    } else {
+      next();
+    }
+  });
 
 }
