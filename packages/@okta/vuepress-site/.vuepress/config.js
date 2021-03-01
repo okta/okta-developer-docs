b492ea18a8053bb00180a28db6962d02ef305954
Shaw Yu <shaw.yu@okta.com>
2021-03-01 14:40:11 -0500

    add developer.okta gtm container

--- packages/@okta/vuepress-site/.vuepress/config.js
+++ packages/@okta/vuepress-site/.vuepress/config.js
@@ -40,12 +40,20 @@ module.exports = {
       var isProduction = window.location.hostname === 'developer.okta.com';
       if (isProduction) {
 
-        // START Google Tag Manager
+        // START Google Tag Manager - main container
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-KXMLV58');
+        // END Google Tag Manager
+
+        // START Google Tag Manager - developer.okta.com container
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WCRV9ZC');
         // END Google Tag Manager
 
       }
