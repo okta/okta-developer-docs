a9b0092912ce9a564bf6fa435b7a7409ed1752d2
Volodymyr Makarchuk <volodymyr.makarchuk@okta.com>
2021-03-30 16:42:04 +0300

    add improvements

--- packages/@okta/vuepress-theme-prose/components/OnThisPage.vue
+++ packages/@okta/vuepress-theme-prose/components/OnThisPage.vue
@@ -4,10 +4,20 @@
       <div class="title">On This Page</div>
       <div>
         <ul class="links" v-if="items">
-          <OnThisPageItem v-for="(link, index) in items" :link="link" :key="index" :activeAnchor=activeAnchor />
-          </ul>
+          <OnThisPageItem
+            v-for="(link, index) in items"
+            :link="link"
+            :key="index"
+            :activeAnchor="activeAnchor"
+          />
+        </ul>
         <ul class="links" v-else>
-          <OnThisPageItem v-for="(link, index) in $page.fullHeaders[0].children" :link="link" :key="index" :activeAnchor=activeAnchor />
+          <OnThisPageItem
+            v-for="(link, index) in $page.fullHeaders[0].children"
+            :link="link"
+            :key="index"
+            :activeAnchor="activeAnchor"
+          />
         </ul>
       </div>
     </div>
@@ -15,85 +25,136 @@
 </template>
 
 <script>
-  import { LAYOUT_CONSTANTS } from '../layouts/Layout.vue';
-  import AnchorHistory from '../mixins/AnchorHistory.vue';
-  import _ from 'lodash';
-  export default {
-    name: 'OnThisPage',
-    mixins: [AnchorHistory],
-    inject: ['appContext'],
-    components: {
-      OnThisPageItem: () => import('../components/OnThisPageItem.vue'),
-    },
-    props: ['items'],
-    data() {
-      return {
-        activeAnchor: null,
-        anchors: [],
-        anchorOffsetPairs: [],
-        paddedHeaderHeight: 0,
+import { LAYOUT_CONSTANTS } from "../layouts/Layout.vue";
+import AnchorHistory from "../mixins/AnchorHistory.vue";
+import _ from "lodash";
+export default {
+  name: "OnThisPage",
+  mixins: [AnchorHistory],
+  inject: ["appContext"],
+  components: {
+    OnThisPageItem: () => import("../components/OnThisPageItem.vue")
+  },
+  props: ["items"],
+  data() {
+    return {
+      activeAnchor: null,
+      anchors: [],
+      sidebarAnchors: [],
+      anchorOffsetPairs: [],
+      sidebarAnchorsOffsetPairs: [],
+      paddedHeaderHeight: 0
+    };
+  },
+  computed: {
+    showOnthisPage: function() {
+      return this.items ||
+        (this.$page.fullHeaders[0].children &&
+          this.$page.fullHeaders[0].children.length > 0)
+        ? true
+        : false;
+    }
+  },
+  mounted() {
+    this.paddedHeaderHeight =
+      document.querySelector(".fixed-header").clientHeight +
+      LAYOUT_CONSTANTS.HEADER_TO_CONTENT_GAP;
+    this.$nextTick(() => {
+      this.captureAnchors();
+      this.handleScroll();
+      this.setActiveHash();
+    });
+    window.addEventListener("scroll", this.handleScroll);
+    window.addEventListener("scroll", this.setActiveHash);
+  },
+  updated() {
+    if (!this.appContext.isInMobileViewport) {
+      this.captureAnchors();
+    }
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.handleScroll);
+    window.removeEventListener("scroll", this.setActiveHash);
+  },
+  methods: {
+    handleScroll: _.debounce(function(event) {
+      let maxHeight =
+        document.querySelector(".on-this-page").clientHeight - window.scrollY;
+      if (maxHeight > window.innerHeight) {
+        maxHeight =
+          window.innerHeight -
+          document.querySelector(".fixed-header").clientHeight -
+          60;
       }
-    },
-    computed: {
-      showOnthisPage: function(){
-         return this.items ||
-                (this.$page.fullHeaders[0].children && this.$page.fullHeaders[0].children.length > 0)
-                ? true : false;
-      }
-    },
-    mounted() {
-      this.paddedHeaderHeight = document.querySelector('.fixed-header').clientHeight + LAYOUT_CONSTANTS.HEADER_TO_CONTENT_GAP;
-      this.$nextTick(() => {
-        this.captureAnchors();
-        this.handleScroll();
-        this.setActiveHash();
-      });
-      window.addEventListener('scroll', this.handleScroll);
-      window.addEventListener('scroll', this.setActiveHash);
-    },
-    updated() {
-      if(!this.appContext.isInMobileViewport) {
-        this.captureAnchors();
-      }
-    },
-    beforeDestroy() {
-      window.removeEventListener('scroll', this.handleScroll);
-      window.removeEventListener('scroll', this.setActiveHash);
-    },
-    methods: {
-      handleScroll: _.debounce(function (event) {
-        let maxHeight = document.querySelector('.on-this-page').clientHeight - window.scrollY
-        if(maxHeight > window.innerHeight) {
-          maxHeight = window.innerHeight - document.querySelector('.fixed-header').clientHeight - 60;
-        }
-        document.querySelector('.on-this-page-navigation').style.height = maxHeight + 'px';
-      }, 200),
+      document.querySelector(".on-this-page-navigation").style.height =
+        maxHeight + "px";
+    }, 200),
 
-      captureAnchors: function () {
-        const sidebarLinks = [].slice.call(document.querySelectorAll('.on-this-page-link'));
-        this.anchors = [].slice.call(document.querySelectorAll('.header-anchor'))
-          .filter(anchor => sidebarLinks.some(sidebarLink => sidebarLink.hash === anchor.hash));
-        const anchorOffsets = this.anchors.map(anchor => anchor.parentElement.offsetTop);
-        this.anchorOffsetPairs = anchorOffsets.map((anchorOffset, index, anchorOffsets) => [anchorOffset, anchorOffsets[index + 1]]);
-      },
+    captureAnchors: function() {
+      const sidebarLinks = [].slice.call(
+        document.querySelectorAll(".on-this-page-link")
+      );
+      this.anchors = [].slice.call(document.querySelectorAll(".header-anchor"));
+      this.sidebarAnchors = this.anchors.filter(anchor =>
+        sidebarLinks.some(sidebarLink => sidebarLink.hash === anchor.hash)
+      );
+      const anchorOffsets = this.anchors.map(
+        anchor => anchor.parentElement.offsetTop
+      );
+      const sidebarAnchorOffsets = this.sidebarAnchors.map(
+        anchor => anchor.parentElement.offsetTop
+      );
+      this.sidebarAnchorsOffsetPairs = sidebarAnchorOffsets.map(
+        (anchorOffset, index, anchorOffsets) => [
+          anchorOffset,
+          anchorOffsets[index + 1]
+        ]
+      );
+      this.anchorOffsetPairs = anchorOffsets.map(
+        (anchorOffset, index, anchorOffsets) => [
+          anchorOffset,
+          anchorOffsets[index + 1]
+        ]
+      );
+    },
 
-      setActiveHash: _.debounce(function (event) {
+    setActiveHash: _.debounce(
+      function(event) {
         const scrollTop = Math.max(
           window.pageYOffset,
           document.documentElement.scrollTop,
           document.body.scrollTop
-        )
+        );
 
-        const matchingPair = this.anchorOffsetPairs.find(pair =>
-          (scrollTop >= pair[0] - this.paddedHeaderHeight) && (!pair[1] || scrollTop < pair[1] - this.paddedHeaderHeight), this);
+        const matchingPair = this.anchorOffsetPairs.find(
+          pair =>
+            scrollTop >= pair[0] - this.paddedHeaderHeight &&
+            (!pair[1] || scrollTop < pair[1] - this.paddedHeaderHeight),
+          this
+        );
+        const sidebarMatchingPair = this.sidebarAnchorsOffsetPairs.find(
+          pair =>
+            scrollTop >= pair[0] - this.paddedHeaderHeight &&
+            (!pair[1] || scrollTop < pair[1] - this.paddedHeaderHeight),
+          this
+        );
 
-        const activeAnchor = matchingPair ? this.anchors[this.anchorOffsetPairs.indexOf(matchingPair)] : this.anchors[0];
+        const activeAnchor = matchingPair
+          ? this.anchors[this.anchorOffsetPairs.indexOf(matchingPair)]
+          : this.anchors[0];
+        const sidebarActiveAnchor = matchingPair
+          ? this.sidebarAnchors[this.sidebarAnchorsOffsetPairs.indexOf(sidebarMatchingPair)]
+          : this.sidebarAnchors[0];
         if (activeAnchor) {
-          this.activeAnchor = activeAnchor.hash;
-          this.historyReplaceAnchor(activeAnchor.hash)
-
+          this.historyReplaceAnchor(activeAnchor.hash);
         }
-      }, 200, {leading: true})
-    }
+        if (sidebarActiveAnchor) {
+          this.activeAnchor = sidebarActiveAnchor.hash;
+        }
+      },
+      200,
+      { leading: true }
+    )
   }
+};
 </script>
