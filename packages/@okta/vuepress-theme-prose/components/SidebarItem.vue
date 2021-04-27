0bac2c3b860e1583e68d9f034edd7a0cf34c52f0
volodymyrmakarchuk-okta <71866734+volodymyrmakarchuk-okta@users.noreply.github.com>
2021-04-27 16:17:32 +0300

    On this page route fix (#2026)
    * Merge branch 'op-active-anchor-on-scroll' into vm-OKTA-338593-onthispage-route-fix
    
    * edditional changes
    
    * add improvements
    
    * NFDEPLOY
    
    * fix bugs: active sidebar item
    on page load, scroll to active sidebar item if it's not in viewport
    
    * NFDEPLOY
    
    * NFDEPLOY
    
    * move some logic outside onthispage component
    
    * make some  refactoring
    
    * NFDEPLOY
    
    * add minor changes
    
    * remove not needed code from index.js
    
    * resolve comments
    
    * add some  minor changes
    
    * remove temporary code
    
    * resolviing comments
    
    * extend router link for tree nav
    to be able to display active item with dymanically hash changing
    
    * NFDEPLOY
    
    * NFDEPLOY : fix selenium tests
    
    * NFDEPLOY : add some changes on page load
    
    * NFDEPLOY : add minor changes
    
    * add improvements based on pr comments
    
    * add selenium test for onthispage component
    
    * minor fix
    
    * minor changes

--- packages/@okta/vuepress-theme-prose/components/SidebarItem.vue
+++ packages/@okta/vuepress-theme-prose/components/SidebarItem.vue
@@ -4,10 +4,16 @@
       <div v-if="link.path">
         <router-link
           :to="link.path"
-          exact
+          v-slot="{ route, href, navigate }"
           class="tree-nav-link"
+        >
+          <a
+            :href="href"
+            @click="navigate"
+            :class="route.path === $route.path ? 'router-link-active' : ''"
+            :aria-current="route.path === $route.path && 'page'"
+            ><slot>{{ link.title }}</slot></a
           >
-          {{ link.title }}
         </router-link>
       </div>
 
@@ -20,19 +26,13 @@
           }"
           @click="toggleExpanded"
         >
-          <svg
-            viewBox="0 0 320 512"
-            v-if="link.subLinks && !sublinksExpanded"
-          >
+          <svg viewBox="0 0 320 512" v-if="link.subLinks && !sublinksExpanded">
             <path
               d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"
             />
           </svg>
 
-          <svg
-            viewBox="0 0 320 512"
-            v-if="link.subLinks && sublinksExpanded"
-          >
+          <svg viewBox="0 0 320 512" v-if="link.subLinks && sublinksExpanded">
             <path
               d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
             />
@@ -69,7 +69,7 @@ export default {
   mounted() {
     this.setData();
   },
-  
+
   watch: {
     link() {
       this.setData();
@@ -93,7 +93,7 @@ export default {
   
   methods: {
     toggleExpanded() {
-      this.sublinksExpanded = !this.sublinksExpanded
+      this.sublinksExpanded = !this.sublinksExpanded;
     },
     setData: function() {
       this.sublinksExpanded = Boolean(this.link.iHaveChildrenActive);
