51bf47ff257ffab5b01db28fc980364d3ed5e538
Shaw Yu <shaw.yu@okta.com>
2021-05-07 10:58:53 -0400

    remove ga container

--- packages/@okta/vuepress-site/.vuepress/config.js
+++ packages/@okta/vuepress-site/.vuepress/config.js
@@ -48,14 +48,6 @@ module.exports = {
         })(window,document,'script','dataLayer','GTM-KXMLV58');
         // END Google Tag Manager
 
-        // START Google Tag Manager - developer.okta.com container
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-WCRV9ZC');
-        // END Google Tag Manager
-
       }
     `],
     ['script', {}, `
