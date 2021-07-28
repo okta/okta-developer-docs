851b9636ee7da90bd34a5892ad2232725a2a869d
Shaw Yu <shaw.yu@okta.com>
2021-07-28 16:32:01 -0400

    trying something out

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -6,17 +6,16 @@ source "${0%/*}/helpers.sh"
 export CHROME_HEADLESS=true
 
 # Run the yarn install to pull in test dependencies
-fold yarn_install yarn install
+#fold yarn_install yarn install
 
 # Lint the markdown
 # fold yarn_markdown_check yarn markdown-lint
 
 # Check the links
-fold yarn_check_links yarn check-links
+#fold yarn_check_links yarn check-links
 
 # Build site and Run tests
 fold yarn_test yarn test
-
 
 # Will run the netlify deploy if:
 # - NETLIFY_SITE_ID and NETLIFY_AUTH_TOKEN is present (will not be true on forks)
