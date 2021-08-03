b2c508a322ef5cf0c4f1012b58c9a54d60bcc94a
Ulya Sh <ulyana.sh@ramotion.agency>
2021-08-03 18:02:46 +0300

    breadcrumb links

--- packages/@okta/vuepress-theme-prose/components/Breadcrumb.vue
+++ packages/@okta/vuepress-theme-prose/components/Breadcrumb.vue
@@ -3,7 +3,8 @@
   <div class="breadcrumb--container">
     <ol>
       <li v-for="(crumb, index) in crumbItems" :key="index">
-        <span>{{crumb.title}}</span>
+        <router-link v-if="crumb.link" :to="crumb.link">{{crumb.title}}</router-link>
+        <span v-else>{{crumb.title}}</span>
         <svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M1 9L5 5L1 1" stroke="#1D1D21"/>
         </svg>
@@ -44,7 +45,7 @@
       crumbItems() {
         this.crumbs = [];
         this.crumbParents = [];
-        var crumbs = this.getCrumbs(this.getNavigation());
+        let crumbs = this.getCrumbs(this.getNavigation());
         return crumbs;
       }
     },
@@ -63,7 +64,7 @@
             menuItem.parents = _.union([parent], parent.parents)
           }
 
-          if(menuItem.path != undefined && this.$page.path == menuItem.path) {
+          if(menuItem.path != undefined && this.$page.path === menuItem.path) {
 
             //add parent crumbs           
             menuItem.parents.reverse().map((parentCrumb) => {
@@ -74,11 +75,32 @@
           }
 
           if (menuItem.subLinks != undefined) {
+            menuItem = this.addMainLinks(menuItem);
             this.getCrumbs(menuItem.subLinks, menuItem);
           }
         }
 
         return this.crumbs;
+      },
+
+      addMainLinks: function(menuItem) {      
+        if(menuItem.title === 'Guides' && this.$page.path.startsWith('/docs/guides/')) {
+          menuItem.path = '/docs/guides/';
+        }       
+        if(menuItem.title === 'Concepts' && this.$page.path.startsWith('/docs/concepts/')) {
+          menuItem.path = '/docs/concepts/';
+        }     
+        if(menuItem.title === 'Reference' && this.$page.path.startsWith('/docs/reference/')) {
+          menuItem.path = '/docs/reference/';
+        }
+        if(menuItem.title === 'Languages & SDKs' && this.$page.path.startsWith('/code/')) {
+          menuItem.path = '/code/';
+        }
+        if(menuItem.title === 'Release Notes' && this.$page.path.startsWith('/docs/release-notes/')) {
+          menuItem.path = '/docs/release-notes/';
+        }
+
+        return menuItem;
       },
 
       crumbItemExists: function (checkCrumb) {
