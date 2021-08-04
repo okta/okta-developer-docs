2248e3582243ccae34b43cff237e1e99a9acfb7e
christine choy <christine.choy@okta.com>
2021-08-04 13:31:30 -0700

    updated

--- packages/@okta/vuepress-theme-prose/const/navbar.const.js
+++ packages/@okta/vuepress-theme-prose/const/navbar.const.js
@@ -228,10 +228,6 @@ export const guides = [
             guideName: "implement-auth-code-pkce"
           },
           {
-            title: "Create an Authorization Server",
-            guideName: "customize-authz-server"
-          },
-          {
             title: "Configure SSO for Native apps",
             guideName: "configure-native-sso"
           },
