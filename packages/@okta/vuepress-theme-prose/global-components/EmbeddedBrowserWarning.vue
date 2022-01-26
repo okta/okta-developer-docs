9efe333a4d5d403d164ec64faeae623e048a58f5
Chris Mills <87396614+chrisdmills-okta@users.noreply.github.com>
2022-01-26 14:13:02 +0000

    Update packages/@okta/vuepress-theme-prose/global-components/EmbeddedBrowserWarning.vue
    Co-authored-by: Susan <susan.harper@okta.com>

--- packages/@okta/vuepress-theme-prose/global-components/EmbeddedBrowserWarning.vue
+++ packages/@okta/vuepress-theme-prose/global-components/EmbeddedBrowserWarning.vue
@@ -1,6 +1,6 @@
 <template>
   <blockquote>
-    <p><strong>Note</strong>: We strongly advise against using WebViews for authentication on mobile apps because of major security implications. See <a href="https://datatracker.ietf.org/doc/html/draft-ietf-oauth-native-apps">OAuth 2.0 for Native Apps</a> for more information. You should use our native SDKs instead.</p>
+    <p><strong>Note</strong>: We strongly advise against using WebViews for authentication on mobile apps because of major security implications. See <a href="https://datatracker.ietf.org/doc/html/draft-ietf-oauth-native-apps">OAuth 2.0 for Native Apps</a>. Use our native SDKs instead.</p>
   </blockquote>
 </template>
 
