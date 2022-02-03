e0a6a2ec53970d2942ac5c9c65d902d69459dd70
Ulya Sh <ulyana.sh@ramotion.agency>
2022-02-03 17:28:36 +0300

    set dark theme for body instaed of layout

--- packages/@okta/vuepress-theme-prose/components/ThemeModeToggle.vue
+++ packages/@okta/vuepress-theme-prose/components/ThemeModeToggle.vue
@@ -11,17 +11,23 @@ import { deleteCookie, getCookie, setCookie } from "../util/attribution/cookies"
 const themeModeCookieName = 'is_dark_mode';
 
 export default {
-  inject: ['appContext'],
   data() {
     return {
-      isDarkMode: getCookie(themeModeCookieName, this.appContext.isDarkMode),
+      isDarkMode: getCookie(themeModeCookieName, false),
     };
+  },
+  created() {
+    this.addHtmlClass();
   },
   methods: {
     toggleDarkMode: function(value) {
-      this.isDarkMode = this.appContext.isDarkMode = !!value;
+      this.isDarkMode = !!value;
+      this.addHtmlClass();
       setCookie(themeModeCookieName, this.isDarkMode, { expires: 365 });
     },
+    addHtmlClass: function() {
+      this.isDarkMode ? document.body.classList.add('dark-theme') : document.body.classList.remove('dark-theme');
+    }
   }
 };
 </script>
