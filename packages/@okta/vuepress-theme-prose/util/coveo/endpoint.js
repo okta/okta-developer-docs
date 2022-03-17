8ee333229459c998e7babfc4b2b3a80842de72de
François Lehoux <francois.lehoux@okta.com>
2022-03-17 09:30:46 -0400

    Add localStorage detection to prevent errors

--- packages/@okta/vuepress-theme-prose/util/coveo/endpoint.js
+++ packages/@okta/vuepress-theme-prose/util/coveo/endpoint.js
@@ -2,13 +2,15 @@ import axios from "axios";
 import jwt_decode from "jwt-decode";
 import moment from "moment";
 
+import storage from '../localStorage'
+
 const TOKEN_ENDPOINT =
   "https://wt1ugse0be.execute-api.us-west-2.amazonaws.com/prod/token?site=developer";
 const COVEO_PIPELINE = "oktaproduction9ounvcxa";
 const COVEO_ENDPOINT = "https://platform.cloud.coveo.com/rest/search";
 
 const _getToken = () => {
-  let token = localStorage.getItem("coveo_token");
+  let token = storage.getItem("coveo_token");
   if (!token) return;
 
   // Strip " if present (breaks coveo)
@@ -19,7 +21,7 @@ const _getToken = () => {
   const now = moment();
   const expWithBuffer = moment.unix(decoded.exp).subtract(5, "minutes");
   if (now.isAfter(expWithBuffer)) {
-    localStorage.removeItem("coveo_token");
+    storage.removeItem("coveo_token");
     return;
   } else {
     return token;
@@ -28,7 +30,7 @@ const _getToken = () => {
 
 const _renewToken = () => {
   return axios.get(TOKEN_ENDPOINT).then(({ data: { token } }) => {
-    localStorage.setItem("coveo_token", token);
+    storage.setItem("coveo_token", token);
     return token;
   });
 };
