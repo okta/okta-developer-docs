ecc3f02085da0048588ffd948fdb0963d826ab3f
Andrey Bo <andrew.bo@ramotion.agency>
2022-07-25 18:00:21 +0300

    fix: update SidebarItem animation

--- packages/@okta/vuepress-theme-prose/components/SidebarItem.vue
+++ packages/@okta/vuepress-theme-prose/components/SidebarItem.vue
@@ -198,8 +198,7 @@ export default {
 .slide-fade-leave-active {
   transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
 }
-.slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active до версии 2.1.8 */ {
+.slide-fade-enter, .slide-fade-leave-to {
   transform: translateX(10px);
   opacity: 0;
 }
