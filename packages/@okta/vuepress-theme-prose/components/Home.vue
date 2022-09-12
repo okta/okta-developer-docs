af690951c0b87ed1e446758e3c4c82c2489d75d5
chrisdmills-okta <chris.d.mills@okta.com>
2022-09-12 20:59:17 +0100

    add integrator survey banner

--- packages/@okta/vuepress-theme-prose/components/Home.vue
+++ packages/@okta/vuepress-theme-prose/components/Home.vue
@@ -3,6 +3,11 @@
     <div class="content--container">
       <Sidebar />
       <div class="width-adjuster">
+      <AnnouncementBanner>
+          <template v-slot:description>
+            Want to leverage Okta’s reach and get more people to use your app? <a href="https://surveys.okta.com/jfe/form/SV_1FFAqyUKBLAtUr4?jfefe=new" target="_blank" rel="noopener noreferrer">Take this survey to qualify for our integrator pilot program!</a>
+          </template>
+        </AnnouncementBanner>
         <div class="justify-content-center">
           <div class="homepage--elevated">
             <div class="homepage--top-section homepage--section-margins">
