65d9d005d8b50e3b00f25dd81f462aa7037b989f
Harutyun Karapetyan <harutyun.k@ramotion.agency>
2022-09-14 11:00:53 +0300

    Update Card (not complete yet)

--- packages/@okta/vuepress-theme-prose/global-components/Card.vue
+++ packages/@okta/vuepress-theme-prose/global-components/Card.vue
@@ -1,37 +1,63 @@
 <template>
-    <SmartLink :item="{ link: href }" classes="card is-card-action is-card-clickable">
-        <header class="card--header">
-            <figure class="card--header-icon" v-if="headerIcon">
-                <i class="icon-75" :class="headerIcon"></i>
-            </figure>
-            <figure class="card--header-icon" v-else-if="headerImage">
-                <div>
-                    <img class="card--header-img" :src="headerImage" />
-                </div>
-            </figure>
-            <section class="card--header-main-own">
-                <h2 class="card--title" v-if="cardTitle">
-                    {{ cardTitle }}
-                </h2>
-                <section class="card--meta" v-if="cardMeta">
-                    {{ cardMeta }}
-                </section>
-            </section>
-        </header>
-        <section class="card--main">
-            <p>
-                <slot
-                    >Commodo deserunt nulla Lorem veniam do minim consectetur ea
-                    in.</slot
-                >
-            </p>
+  <SmartLink
+    :item="{ link: href }"
+    :class="type === 'secondary' ? 'card_type_secondary' : ''"
+    classes="card is-card-action is-card-clickable"
+  >
+    <header class="card--header">
+      <figure
+        v-if="headerIcon"
+        class="card--header-icon"
+      >
+        <i
+          class="icon-75"
+          :class="headerIcon"
+        ></i>
+      </figure>
+      <figure
+        v-else-if="headerImage"
+        class="card--header-icon"
+      >
+        <div>
+          <img
+            class="card--header-img"
+            :src="headerImage"
+          />
+        </div>
+      </figure>
+      <section class="card--header-main-own">
+        <h2
+          v-if="cardTitle"
+          class="card--title"
+        >
+          {{ cardTitle }}
+        </h2>
+        <section
+          v-if="cardMeta"
+          class="card--meta"
+        >
+          {{ cardMeta }}
         </section>
-        <footer class="card--footer" v-if="showFooter">
-            <section class="card--actions">
-                <span class="card--link">{{ cardLinkText }}</span>
-            </section>
-        </footer>
-    </SmartLink>
+      </section>
+    </header>
+    <section class="card--main">
+      <p>
+        <slot>
+          Commodo deserunt nulla Lorem veniam do minim consectetur ea in.
+        </slot>
+      </p>
+    </section>
+    <footer
+      v-if="showFooter"
+      class="card--footer"
+    >
+      <section class="card--actions">
+        <span class="card--link">
+          {{ cardLinkText }}
+        </span>
+      </section>
+    </footer>
+  </SmartLink>
 </template>
 
 <script>
@@ -40,6 +66,10 @@ export default {
         SmartLink: () => import("../components/SmartLink.vue")
     },
     props: {
+        type: {
+          default: null,
+          type: String,
+        },
         href: {
             default: "#",
             type: String,
