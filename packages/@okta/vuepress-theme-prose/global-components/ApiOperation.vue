d2dda98e151eb8be7a31ece61c5d036b4d31230e
konstantin.sh <wotkad@gmail.com>
2022-10-18 19:35:03 +0400

    fix ApiOperation

--- packages/@okta/vuepress-theme-prose/global-components/ApiOperation.vue
+++ packages/@okta/vuepress-theme-prose/global-components/ApiOperation.vue
@@ -1,18 +1,31 @@
 <template>
   <p>
     <span
-      :class="('api-uri-template api-uri-' + method.toLowerCase() )">
-       <span class="api-label">
+      :class="('api-uri-template api-uri-' + method.toLowerCase() )"
+    >
+      <span class="api-label">
         {{ method | uppercase }}
       </span>
-       <span class="api-url" v-html="formattedUrl"></span>
-     </span>
+      <span 
+        class="api-url" 
+        v-html="formattedUrl"
+      >
+      </span>
+    </span>
   </p>
 </template>
 
 <script>
   export default {
     name: 'ApiOperation',
+    filters: {
+      uppercase: function (value) {
+        return value.toUpperCase()
+      },
+      formatUrl: function (value) {
+        return value.replace(/\${([^ ]*)}/gm, (match, prop) => `<strong>"${prop}"</strong>`);
+      }
+    },
     props: {
       method: {
         type: String,
@@ -28,14 +41,5 @@
         return this.url.replace(/\${([^ ]*)}/gm, (match, prop) => '<strong>${'+prop+'}</strong>');
       }
     },
-    filters: {
-      uppercase: function (value) {
-        return value.toUpperCase()
-      },
-
-      formatUrl: function (value) {
-        return value.replace(/\${([^ ]*)}/gm, (match, prop) => `<strong>"${prop}"</strong>`);
-      }
-    }
   }
 </script>
