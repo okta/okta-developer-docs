4cacaa991f7da2fea13c02e2aaebea4d3bf27636
konstantin.sh <wotkad@gmail.com>
2022-10-18 19:34:24 +0400

    fix CategoryLinks

--- packages/@okta/vuepress-theme-prose/global-components/CategoryLinks.vue
+++ packages/@okta/vuepress-theme-prose/global-components/CategoryLinks.vue
@@ -1,9 +1,20 @@
 <template>
   <ul class="category--links">
-    <li v-for="link in links" :key="link.path" :class="{'is-active': $page.path === link.path}">
-      <router-link :to="link.path">{{ link.title }}</router-link>
+    <li 
+      :class="{'is-active': $page.path === link.path}"
+      v-for="link in links" 
+      :key="link.path" 
+    >
+      <router-link :to="link.path">
+        {{ link.title }}
+      </router-link>
       <br v-if="link.frontmatter.excerpt && showExcerpt">
-      <span class="description" v-if="link.frontmatter.excerpt && showExcerpt">{{ link.frontmatter.excerpt }}</span>
+      <span 
+        class="description" 
+        v-if="link.frontmatter.excerpt && showExcerpt"
+      >
+        {{ link.frontmatter.excerpt }}
+      </span>
     </li>
   </ul>
 </template>
@@ -18,7 +29,7 @@
       "linkPrefix": {
         type: String
       },
-      "where_exp": {
+      "whereExp": {
         type: String
       },
       "sort": {
@@ -39,9 +50,9 @@
           links = this.$site.pages.filter((el) => el.path.includes(this.linkPrefix))
         }
 
-        if ( this.where_exp != undefined ) {
-           let toRemove = links.filter((el) => el.frontmatter[this.where_exp] && el.frontmatter[this.where_exp] == true);
-           links = links.filter((el) => toRemove.indexOf(el) < 0);
+        if ( this.whereExp != undefined ) {
+          let toRemove = links.filter((el) => el.frontmatter[this.whereExp] && el.frontmatter[this.whereExp] == true);
+          links = links.filter((el) => toRemove.indexOf(el) < 0);
         }
 
         if ( this.sort ) {
