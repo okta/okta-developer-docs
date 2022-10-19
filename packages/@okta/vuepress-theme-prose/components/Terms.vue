ac5295d8d0dddf50a5caa97d6343017836089e74
konstantin.sh <wotkad@gmail.com>
2022-10-19 17:27:09 +0400

    fix Terms

--- packages/@okta/vuepress-theme-prose/components/Terms.vue
+++ packages/@okta/vuepress-theme-prose/components/Terms.vue
@@ -4,32 +4,41 @@
       <div class="terms--container">
         <div class="terms--leader">
           <h1 class="terms--title">
-            {{$page.frontmatter.heading}}
+            {{ $page.frontmatter.heading }}
           </h1>
         </div>
       </div>
       <div class="terms--container">
         <div class="terms--text">
-          <section v-for="(section, sectionIdx) in $page.frontmatter.terms" v-bind:key="sectionIdx">
+          <section 
+            v-for="(section, sectionIdx) in $page.frontmatter.terms" 
+            :key="sectionIdx"
+          >
             <component
-              :is="section.tag || 'h2'"
               class="terms--section-title"
-              v-if="section.heading">
-              {{section.heading}}
+              :is="section.tag || 'h2'"
+              v-if="section.heading"
+            >
+              {{ section.heading }}
             </component>
             <template v-for="(paragraph, paragraphIdx) in section.paragraphs">
               <ul
                 v-if="Array.isArray(paragraph)"
-                v-bind:key="paragraphIdx">
-                <li v-for="(bullet, bulletIdx) in paragraph"
-                    v-bind:key="bulletIdx"
-                    v-html="bullet">
+                :key="paragraphIdx"
+              >
+                <li 
+                  v-for="(bullet, bulletIdx) in paragraph"
+                  :key="bulletIdx"
+                  v-html="bullet"
+                >
                 </li>
               </ul>
-              <p class="terms--section-paragraph"
-                v-bind:key="paragraphIdx"
+              <p 
+                class="terms--section-paragraph"
+                v-else
+                :key="paragraphIdx"
                 v-html="paragraph"
-                v-else>
+              >
               </p>
             </template>
           </section>
