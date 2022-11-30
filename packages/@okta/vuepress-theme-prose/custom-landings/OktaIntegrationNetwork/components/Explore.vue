2b3a333730d09ff9cd6110b3180484eab1fd284f
Van Ngo <van.ngo@okta.com>
2022-11-29 19:10:29 -0500

    Transfer updated OIN landing page content to new branch

--- packages/@okta/vuepress-theme-prose/custom-landings/OktaIntegrationNetwork/components/Explore.vue
+++ packages/@okta/vuepress-theme-prose/custom-landings/OktaIntegrationNetwork/components/Explore.vue
@@ -2,7 +2,7 @@
   <section class="customLanding-white customLanding-waves customLanding-waves-bottom customLanding-waves-top">
     <div class="customLanding-wrapper">
         <div class="customLanding-explore">
-            <h2>Explore the integration process from end-to-end</h2>
+            <h2>Explore the integration process</h2>
             <p class="customLanding-explore__desc">Our step-by-step guides walk you through every phase of building your integration.</p>
             <div class="customLanding-explore__list">
                 <div class="customLanding-explore__item" v-for="({ id, caption, text, link, path }) in list" :key="caption">
@@ -31,28 +31,30 @@
                 {
                     id: 1,
                     caption: 'Learn',
-                    text: 'Easy and secure access is a base requirement for modern apps. Understand what enterprises need, their common use cases, and the technologies involved.',
-                    link: 'View concepts',
-                    path: '/docs/concepts/'
+                    text: 'Understand how Okta works and how to implement solutions for your use cases. Get started by signing up for a free Okta developer org and trying the quickstart examples.',
+                    link: 'Try quickstart examples',
+                    path: '/docs/guides/quickstart/'
                 },
                 {
                     id: 2,
                     caption: 'Design & build',
-                    text: 'OIN is a catalog and a support system. Our resources help you to design, build, and test your integration before you submit it for verification.',
-                    link: 'View guides',
-                    path: '/docs/guides/#publish-an-integration'
+                    text: 'OIN is a catalog and a support system. View SSO, lifecycle, and service app integration guides to help you to design, build, and test your integration before you submit it for verification. Get support from the Okta Developer Forum during your build journey.',
+                    link: 'Start with SSO',
+                    path: '/docs/guides/oin-sso-overview/'
                 },
                 {
                     id: 3,
                     caption: 'Get Okta verified',
-                    text: 'From testing to running diagnostics and validation, our team works with you to get your integration reviewed and ready to publish to the OIN.',
-                    link: 'Learn more',
+                    text: 'Prepare submission requirements and use the OIN Manager portal to submit your integration for verification. The OIN team runs diagnostics and works with you to validate your integration for publication.',
+                    link: 'Publish your app',
                     path: '/docs/guides/submit-app'
                 },
                 {
                     id: 4,
                     caption: 'Launch your integration',
-                    text: 'Congratulations! Your integration is Okta verified and trusted by thousands of customers who use the OIN. Now, it’s time to spread the word.'
+                    text: 'Congratulations! Your integration is Okta verified and trusted by thousands of customers who use the OIN. Now, it’s time to spread the word.',
+                    link: 'View catalog',
+                    path: 'https://okta.com/integrations/'
                 }
             ]
         }
