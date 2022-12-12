f0c1f4c2d4384b6b50b698bfecb9c466f9f9e365
francoislehoux-okta <francois.lehoux@okta.com>
2022-12-12 15:44:08 -0500

    Remove commented code from yarn-checks.sh (#3818)

--- scripts/yarn-checks.sh
+++ scripts/yarn-checks.sh
@@ -12,13 +12,3 @@ group yarn_check_links yarn broken-link-checker:internal
 
 # Build site and Run tests
 group yarn_test yarn test
-
-# Will run the netlify deploy if:
-# - NETLIFY_SITE_ID and NETLIFY_AUTH_TOKEN is present (will not be true on forks)
-# - The latest commit message on the PR includes the text NFDEPLOY
-# if [ -n $NETLIFY_SITE_ID ] && git log -n1 --pretty=format:"%B" $TRAVIS_PULL_REQUEST_SHA | grep -q NFDEPLOY; then
-#     group netlify_setup npm install -g netlify-cli@^12
-#     group netlify netlify deploy
-# else
-#     echo "Will not use Netlify deploy"
-# fi
