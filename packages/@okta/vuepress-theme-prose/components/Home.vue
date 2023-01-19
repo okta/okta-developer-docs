c8bbd00c70f7a4a52d2cda8cd1557b2c66a4a71f
Harutyun K <110822129+harutyun-k@users.noreply.github.com>
2023-01-19 16:29:39 +0300

    Add workforce

--- packages/@okta/vuepress-theme-prose/components/Home.vue
+++ packages/@okta/vuepress-theme-prose/components/Home.vue
@@ -11,6 +11,9 @@
             Our developer portal enables you to deploy auth that protects your users, apps, APIs, and infrastructure.
           </p>
         </section>
+        <WorkForce
+          :items="workforce"
+        />
         <GetSupport />
       </div>
     </div>
@@ -22,12 +25,118 @@ export default {
   name: "Home",
   components: {
     Sidebar: () => import("../components/Sidebar"),
+    WorkForce: () => import("../components/Workforce"),
     GetSupport: () => import("../components/GetSupport"),
   },
-
   data() {
     return {
-      pseudoAuthorizedCodeBlock: false
+      workforce: [
+        {
+          title: "Learn",
+          block: [
+            {
+              title: "Understand the basics of identity",
+              text: "Learn the key concepts you need for creating identity and access management (IAM) solutions for WIC",
+              list: [
+                {
+                  url: "/docs/concepts/iam-overview",
+                  text: "Understand IAM",
+                },
+                {
+                  url: "/docs/concepts/how-okta-works",
+                  text: "How WIC works",
+                },
+                {
+                  url: "/docs/concepts/oauth-openid",
+                  text: "Choose an authentication protocol",
+                },
+                {
+                  url: "/signup/",
+                  text: "Get a developer org",
+                },
+              ]
+            }
+          ]
+        },
+        {
+          title: "Build",
+          block: [
+            {
+              title: "Connect with APIs and SDKs",
+              text: "Build apps and services that interact directly with WIC for a completely integrated experience",
+              list: [
+                {
+                  url: "/docs/reference",
+                  text: "Explore reference APIs",
+                },
+                {
+                  url: "/code",
+                  text: "Explore our SDKs ",
+                },
+                {
+                  url: "/docs/guides/embedded-authentication",
+                  text: "Explore embedded authentication use cases",
+                },
+              ]
+            },
+            {
+              title: "Authenticate",
+              text: "Define how your applications and APIs verify the identity of a user or device",
+              list: [
+                {
+                  url: "/docs/guides/redirect-authentication",
+                  text: "Start with redirect authentication",
+                },
+                {
+                  url: "/docs/guides/authenticators-overview/main",
+                  text: "Set up multifactor authentication",
+                },
+                {
+                  url: "/docs/guides/identity-providers",
+                  text: "Use an external IdP"
+                }
+              ]
+            },
+            {
+              title: "Brand and customize",
+              text: "Tailor your IAM tools with your organization’s brand and give users a consistent, familiar experience",
+              list: [
+                {
+                  url: "/docs/guides/custom-url-domain/main",
+                  text: "Add a custom domain",
+                },
+                {
+                  url: "/docs/guides/custom-email/main",
+                  text: "Style email notifications",
+                },
+                {
+                  url: "/docs/guides/custom-widget/main",
+                  text: "Customize the sign-in page"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          title: "Publish",
+          block: [
+            {
+              title: "Join the Okta Integration Network (OIN)",
+              text: "Promote your OIDC, SAML, SCIM, or API service integration to thousands of customers and grow your business with the Okta Integration Network (OIN)",
+              list: [
+                {
+                  url: "/docs/guides/okta-integration-network/",
+                  text: "Learn about the OIN",
+                },
+                {
+                  url: "/integrations",
+                  text: "Explore the OIN catalog"
+                },
+              ]
+            }
+          ]
+        },
+      ]
     };
   },
 };
---
+++ packages/@okta/vuepress-theme-prose/components/Workforce.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="workforce">
+    <h2 class="workforce__title">
+      Start your Workforce Identity journey
+    </h2>
+    <p class="workforce__text">
+      Start with <strong>Learn</strong> if you’re new to Workforce Identity Cloud (WIC). Otherwise find the step you need and follow a link.
+    </p>
+    <article
+      class="workforce__article"
+      v-for="(item, index) in items"
+      :key="index"
+    >
+      <h3 class="workforce__article-title">
+        {{ item.title }}
+      </h3>
+      <hr class="workforce__article-line">
+      <ul class="workforce__block">
+        <li
+          class="workforce__block-item"
+          v-for="(block, index) in item.block"
+          :key="index"
+        >
+          <h4 class="workforce__block-title">
+            {{ block.title }}
+          </h4>
+          <p class="workforce__block-text">
+            {{ block.text }}
+          </p>
+          <ul class="workforce__list">
+            <li
+              class="workforce__list-item"
+              v-for="(list, index) in block.list"
+              :key="index"
+            >
+              <a
+                class="workforce__list-link"
+                :href="list.url"
+              >
+                {{ list.text }}
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </article>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: "Workforce",
+    props: {
+      items: {
+        type: Array,
+        required: true,
+      }
+    },
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
