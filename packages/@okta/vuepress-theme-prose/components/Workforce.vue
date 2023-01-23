856696d1cc290a2349a7fa42e50e550393328d63
Harutyun K <110822129+harutyun-k@users.noreply.github.com>
2023-01-23 15:39:14 +0300

    Update workforce

--- packages/@okta/vuepress-theme-prose/components/Workforce.vue
+++ packages/@okta/vuepress-theme-prose/components/Workforce.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="workforce">
-    <h2 class="workforce__title">
+    <h2 class="workforce__title dont-break-out">
       Start your Workforce Identity journey
     </h2>
-    <p class="workforce__text">
+    <p class="workforce__text dont-break-out">
       Start with <strong>Learn</strong> if you’re new to Workforce Identity Cloud (WIC). Otherwise find the step you need and follow a link.
     </p>
     <article
@@ -11,22 +11,27 @@
       v-for="(item, index) in items"
       :key="index"
     >
-      <h3 class="workforce__article-title">
+      <h3 class="workforce__article-title dont-break-out">
         {{ item.title }}
       </h3>
-      <hr class="workforce__article-line">
+      <hr
+        class="workforce__article-line"
+        aria-hidden="true"
+      >
       <ul class="workforce__block">
         <li
           class="workforce__block-item"
           v-for="(block, index) in item.block"
           :key="index"
         >
-          <h4 class="workforce__block-title">
-            {{ block.title }}
-          </h4>
-          <p class="workforce__block-text">
-            {{ block.text }}
-          </p>
+          <div class="workforce__block-wrapper">
+            <h4 class="workforce__block-title dont-break-out">
+              {{ block.title }}
+            </h4>
+            <p class="workforce__block-text dont-break-out">
+              {{ block.text }}
+            </p>
+          </div>
           <ul class="workforce__list">
             <li
               class="workforce__list-item"
@@ -34,7 +39,7 @@
               :key="index"
             >
               <a
-                class="workforce__list-link"
+                class="workforce__list-link dont-break-out"
                 :href="list.url"
               >
                 {{ list.text }}
