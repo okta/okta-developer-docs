c61ca669b9dede6842d4dcca6d5b1fc9a0348175
Harutyun K <110822129+harutyun-k@users.noreply.github.com>
2023-01-23 16:00:13 +0300

    Update news

--- packages/@okta/vuepress-theme-prose/components/News.vue
+++ packages/@okta/vuepress-theme-prose/components/News.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="news">
-    <h2 class="news__title">
+    <h2 class="news__title dont-break-out">
       What’s new in Okta Developer
     </h2>
     <article class="news__article news__article_size_large">
@@ -11,14 +11,14 @@
         alt=""
       >
       <div class="news__wrapper">
-        <p class="news__text">
+        <p class="news__text dont-break-out dont-break-out">
           Blog
         </p>
-        <h3 class="news__article-title">
+        <h3 class="news__article-title dont-break-out">
           How to build a secure React and Fastify API App
         </h3>
         <a
-          class="news__article-link"
+          class="news__article-link dont-break-out"
           href="/blog/2022/12/06/react-fastify-postgres"
         >
           Read more
@@ -26,56 +26,56 @@
       </div>
     </article>
     <article class="news__article">
-      <p class="news__text">
+      <p class="news__text dont-break-out">
         Blog
       </p>
-      <h3 class="news__article-title">
+      <h3 class="news__article-title dont-break-out">
         Secure secrets with Spring Cloud Config and Vault
       </h3>
       <a
-        class="news__article-link"
+        class="news__article-link dont-break-out"
         href="/blog/2022/10/20/spring-vault"
       >
         Read more
       </a>
     </article>
     <article class="news__article">
-      <p class="news__text">
+      <p class="news__text dont-break-out">
         Blog
       </p>
-      <h3 class="news__article-title">
+      <h3 class="news__article-title dont-break-out">
         Quick JavaScript authentication with OktaDev schematics
       </h3>
       <a
-        class="news__article-link"
+        class="news__article-link dont-break-out"
         href="/blog/2022/10/14/quick-javascript-authentication"
       >
         Read more
       </a>
     </article>
     <article class="news__article">
-      <p class="news__text">
+      <p class="news__text dont-break-out">
         Blog
       </p>
-      <h3 class="news__article-title">
+      <h3 class="news__article-title dont-break-out">
         Integrate React Native and Spring Boot securely
       </h3>
       <a
-        class="news__article-link"
+        class="news__article-link dont-break-out"
         href="/blog/2022/10/12/integrate-react-native-and-spring-boot-securely"
       >
         Read more
       </a>
     </article>
     <article class="news__article">
-      <p class="news__text">
+      <p class="news__text dont-break-out">
         Blog
       </p>
-      <h3 class="news__article-title">
+      <h3 class="news__article-title dont-break-out">
         Practical uses of dependency injection in Angular
       </h3>
       <a
-        class="news__article-link"
+        class="news__article-link dont-break-out"
         href="/blog/2022/10/11/angular-dependency-injection"
       >
         Read more
