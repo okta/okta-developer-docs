0430145ea4a2d558b7004a52a67456355d39826a
Harutyun K <110822129+harutyun-k@users.noreply.github.com>
2023-01-24 13:55:59 +0300

    Update workforce

--- packages/@okta/vuepress-theme-prose/components/Workforce.vue
+++ packages/@okta/vuepress-theme-prose/components/Workforce.vue
@@ -1,54 +1,56 @@
 <template>
   <section class="workforce">
-    <h2 class="workforce__title dont-break-out">
-      Start your Workforce Identity journey
-    </h2>
-    <p class="workforce__text dont-break-out">
-      Start with <strong>Learn</strong> if you’re new to Workforce Identity Cloud (WIC). Otherwise find the step you need and follow a link.
-    </p>
-    <article
-      v-for="(item, index) in items"
-      :key="index"
-      class="workforce__article"
-    >
-      <h3 class="workforce__article-title dont-break-out">
-        {{ item.title }}
-      </h3>
-      <hr
-        class="workforce__article-line"
-        aria-hidden="true"
+    <div class="wrapper">
+      <h2 class="workforce__title dont-break-out">
+        Start your Workforce Identity journey
+      </h2>
+      <p class="workforce__text dont-break-out">
+        Start with <strong>Learn</strong> if you’re new to Workforce Identity Cloud (WIC). Otherwise find the step you need and follow a link.
+      </p>
+      <article
+        v-for="(item, index) in items"
+        :key="index"
+        class="workforce__article"
       >
-      <ul class="workforce__block">
-        <li
-          v-for="(block, index) in item.block"
-          :key="index"
-          class="workforce__block-item"
+        <h3 class="workforce__article-title dont-break-out">
+          {{ item.title }}
+        </h3>
+        <hr
+          class="workforce__article-line"
+          aria-hidden="true"
         >
-          <div class="workforce__block-wrapper">
-            <h4 class="workforce__block-title dont-break-out">
-              {{ block.title }}
-            </h4>
-            <p class="workforce__block-text dont-break-out">
-              {{ block.text }}
-            </p>
-          </div>
-          <ul class="workforce__list">
-            <li
-              v-for="(list, index) in block.list"
-              :key="index"
-              class="workforce__list-item"
-            >
-              <a
-                class="workforce__list-link dont-break-out"
-                :href="list.url"
+        <ul class="workforce__block">
+          <li
+            v-for="(block, index) in item.block"
+            :key="index"
+            class="workforce__block-item"
+          >
+            <div class="workforce__block-wrapper">
+              <h4 class="workforce__block-title dont-break-out">
+                {{ block.title }}
+              </h4>
+              <p class="workforce__block-text dont-break-out">
+                {{ block.text }}
+              </p>
+            </div>
+            <ul class="workforce__list">
+              <li
+                v-for="(list, index) in block.list"
+                :key="index"
+                class="workforce__list-item"
               >
-                {{ list.text }}
-              </a>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </article>
+                <a
+                  class="workforce__list-link dont-break-out"
+                  :href="list.url"
+                >
+                  {{ list.text }}
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </article>
+    </div>
   </section>
 </template>
 
