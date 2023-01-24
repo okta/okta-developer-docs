5d38596181ee31bf358d6db82e6855b841f845bd
Harutyun K <110822129+harutyun-k@users.noreply.github.com>
2023-01-24 13:43:31 +0300

    Update hero

--- packages/@okta/vuepress-theme-prose/components/Hero.vue
+++ packages/@okta/vuepress-theme-prose/components/Hero.vue
@@ -1,10 +1,12 @@
 <template>
   <section class="hero">
-    <h1 class="hero__title dont-break-out">
-      Okta developer
-    </h1>
-    <p class="hero__paragraph dont-break-out">
-      Our developer portal enables you to deploy auth that protects your users, apps, APIs, and infrastructure.
-    </p>
+    <div class="hero__wrapper wrapper">
+      <h1 class="hero__title dont-break-out">
+        Okta developer
+      </h1>
+      <p class="hero__paragraph dont-break-out">
+        Our developer portal enables you to deploy auth that protects your users, apps, APIs, and infrastructure.
+      </p>
+    </div>
   </section>
 </template>
