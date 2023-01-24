fd13b0107db31b34e13570169af1f335234b238f
Harutyun K <110822129+harutyun-k@users.noreply.github.com>
2023-01-24 14:12:50 +0300

    Update resources

--- packages/@okta/vuepress-theme-prose/components/Resources.vue
+++ packages/@okta/vuepress-theme-prose/components/Resources.vue
@@ -1,126 +1,128 @@
 <template>
   <section class="resources">
-    <h2 class="resources__title dont-break-out">
-      External Resources
-    </h2>
-    <p class="resources__subtitle dont-break-out">
-      Automation and Orchestration
-    </p>
-    <span class="resources__status dont-break-out">
-      Advanced
-    </span>
-    <p class="resources__paragraph dont-break-out">
-      Automate your Workforce Identity Cloud provisioning with Terraform. Okta articles and partner resources are available to help you get up and running, and as always, the WIC community forum is a great space to ask questions and find peer support.
-    </p>
-    <a
-      class="resources__link dont-break-out"
-      href="/search?q=terraform"
-    >
-      View Terraform discussions on the forum
-      <svg
-        width="13"
-        height="13"
-        viewBox="0 0 13 13"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        aria-hidden="true"
-        focusable="false"
+    <div class="resources__wrapper wrapper">
+      <h2 class="resources__title dont-break-out">
+        External Resources
+      </h2>
+      <p class="resources__subtitle dont-break-out">
+        Automation and Orchestration
+      </p>
+      <span class="resources__status dont-break-out">
+        Advanced
+      </span>
+      <p class="resources__paragraph dont-break-out">
+        Automate your Workforce Identity Cloud provisioning with Terraform. Okta articles and partner resources are available to help you get up and running, and as always, the WIC community forum is a great space to ask questions and find peer support.
+      </p>
+      <a
+        class="resources__link dont-break-out"
+        href="/search?q=terraform"
       >
-        <path
-          d="M9.552 4.064L9.536 7.968H11.056V1.472H4.56V2.992L8.448 2.976L4 7.408L5.12 8.528L9.552 4.064Z"
-          fill="#191919"
-        />
-        <path
-          d="M0 0.472H12.056H0ZM12.056 14.028H2C0.0670034 14.028 -1.5 12.461 -1.5 10.528H1.5C1.5 10.8041 1.72386 11.028 2 11.028H12.056V14.028ZM2 14.028C0.0670034 14.028 -1.5 12.461 -1.5 10.528V0.472H1.5V10.528C1.5 10.8041 1.72386 11.028 2 11.028V14.028ZM12.056 0.472V12.528V0.472Z"
-          fill="#191919"
-          mask="url(#path-1-inside-1_1471_16106)"
-        />
-      </svg>
-    </a>
-    <ul class="resources__list">
-      <li class="resources__list-item">
-        <h3 class="resources__list-title dont-break-out">
-          Blog
-        </h3>
-        <p class="resources__list-text dont-break-out">
-          Explore official Okta blog posts and Help Center articles to learn how to orchestrate Workforce Identity with Terraform.
-        </p>
-        <div class="resources__links">
-          <a
-            class="dont-break-out"
-            href="/blog/2022/04/14/where-okta-meets-devops"
-          >
-            How can DevOps engineers use Okta?
-          </a>
-          <a
-            class="dont-break-out"
-            href="/blog/2021/11/08/k8s-api-server-oidc"
-          >
-            Secure your Kubernetes cluster for Okta OIDC and Terraform
-          </a>
-        </div>
-      </li>
-      <li class="resources__list-item">
-        <h3 class="resources__list-title dont-break-out">
-          Technical documentation
-        </h3>
-        <p class="resources__list-text dont-break-out">
-          Discover official Terraform partner resources to automate provisioning and management for Workforce Identity.
-        </p>
-        <div class="resources__links">
-          <a
-            class="dont-break-out"
-            href="/help/s/article/Prepare-the-Okta-Terraform-Provider-for-the-OIE-upgrade?language=en_US"
-          >
-            Prepare the Okta Terraform Provider for OIE Upgrade
-            <svg
-              width="13"
-              height="13"
-              viewBox="0 0 13 13"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-              focusable="false"
+        View Terraform discussions on the forum
+        <svg
+          width="13"
+          height="13"
+          viewBox="0 0 13 13"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          focusable="false"
+        >
+          <path
+            d="M9.552 4.064L9.536 7.968H11.056V1.472H4.56V2.992L8.448 2.976L4 7.408L5.12 8.528L9.552 4.064Z"
+            fill="#191919"
+          />
+          <path
+            d="M0 0.472H12.056H0ZM12.056 14.028H2C0.0670034 14.028 -1.5 12.461 -1.5 10.528H1.5C1.5 10.8041 1.72386 11.028 2 11.028H12.056V14.028ZM2 14.028C0.0670034 14.028 -1.5 12.461 -1.5 10.528V0.472H1.5V10.528C1.5 10.8041 1.72386 11.028 2 11.028V14.028ZM12.056 0.472V12.528V0.472Z"
+            fill="#191919"
+            mask="url(#path-1-inside-1_1471_16106)"
+          />
+        </svg>
+      </a>
+      <ul class="resources__list">
+        <li class="resources__list-item">
+          <h3 class="resources__list-title dont-break-out">
+            Blog
+          </h3>
+          <p class="resources__list-text dont-break-out">
+            Explore official Okta blog posts and Help Center articles to learn how to orchestrate Workforce Identity with Terraform.
+          </p>
+          <div class="resources__links">
+            <a
+              class="resources__list-link dont-break-out"
+              href="/blog/2022/04/14/where-okta-meets-devops"
             >
-              <path
-                d="M9.552 4.064L9.536 7.968H11.056V1.472H4.56V2.992L8.448 2.976L4 7.408L5.12 8.528L9.552 4.064Z"
-                fill="#3F59E4"
-              />
-              <path
-                d="M0 0.472H12.056H0ZM12.056 14.028H2C0.0670034 14.028 -1.5 12.461 -1.5 10.528H1.5C1.5 10.8041 1.72386 11.028 2 11.028H12.056V14.028ZM2 14.028C0.0670034 14.028 -1.5 12.461 -1.5 10.528V0.472H1.5V10.528C1.5 10.8041 1.72386 11.028 2 11.028V14.028ZM12.056 0.472V12.528V0.472Z"
-                fill="#3F59E4"
-                mask="url(#path-1-inside-1_1471_16106)"
-              />
-            </svg>
-          </a>
-          <a
-            class="dont-break-out"
-            href="/okta/terraform-provider-okta"
-          >
-            View Terraform provider on Github
-            <svg
-              width="13"
-              height="13"
-              viewBox="0 0 13 13"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-              focusable="false"
+              How can DevOps engineers use Okta?
+            </a>
+            <a
+              class="resources__list-link dont-break-out"
+              href="/blog/2021/11/08/k8s-api-server-oidc"
             >
-              <path
-                d="M9.552 4.064L9.536 7.968H11.056V1.472H4.56V2.992L8.448 2.976L4 7.408L5.12 8.528L9.552 4.064Z"
-                fill="#3F59E4"
-              />
-              <path
-                d="M0 0.472H12.056H0ZM12.056 14.028H2C0.0670034 14.028 -1.5 12.461 -1.5 10.528H1.5C1.5 10.8041 1.72386 11.028 2 11.028H12.056V14.028ZM2 14.028C0.0670034 14.028 -1.5 12.461 -1.5 10.528V0.472H1.5V10.528C1.5 10.8041 1.72386 11.028 2 11.028V14.028ZM12.056 0.472V12.528V0.472Z"
-                fill="#3F59E4"
-                mask="url(#path-1-inside-1_1471_16106)"
-              />
-            </svg>
-          </a>
-        </div>
-      </li>
-    </ul>
+              Secure your Kubernetes cluster for Okta OIDC and Terraform
+            </a>
+          </div>
+        </li>
+        <li class="resources__list-item">
+          <h3 class="resources__list-title dont-break-out">
+            Technical documentation
+          </h3>
+          <p class="resources__list-text dont-break-out">
+            Discover official Terraform partner resources to automate provisioning and management for Workforce Identity.
+          </p>
+          <div class="resources__links">
+            <a
+              class="resources__list-link dont-break-out"
+              href="/help/s/article/Prepare-the-Okta-Terraform-Provider-for-the-OIE-upgrade?language=en_US"
+            >
+              Prepare the Okta Terraform Provider for OIE Upgrade
+              <svg
+                width="13"
+                height="13"
+                viewBox="0 0 13 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                focusable="false"
+              >
+                <path
+                  d="M9.552 4.064L9.536 7.968H11.056V1.472H4.56V2.992L8.448 2.976L4 7.408L5.12 8.528L9.552 4.064Z"
+                  fill="#3F59E4"
+                />
+                <path
+                  d="M0 0.472H12.056H0ZM12.056 14.028H2C0.0670034 14.028 -1.5 12.461 -1.5 10.528H1.5C1.5 10.8041 1.72386 11.028 2 11.028H12.056V14.028ZM2 14.028C0.0670034 14.028 -1.5 12.461 -1.5 10.528V0.472H1.5V10.528C1.5 10.8041 1.72386 11.028 2 11.028V14.028ZM12.056 0.472V12.528V0.472Z"
+                  fill="#3F59E4"
+                  mask="url(#path-1-inside-1_1471_16106)"
+                />
+              </svg>
+            </a>
+            <a
+              class="resources__list-link dont-break-out"
+              href="/okta/terraform-provider-okta"
+            >
+              View Terraform provider on Github
+              <svg
+                width="13"
+                height="13"
+                viewBox="0 0 13 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                focusable="false"
+              >
+                <path
+                  d="M9.552 4.064L9.536 7.968H11.056V1.472H4.56V2.992L8.448 2.976L4 7.408L5.12 8.528L9.552 4.064Z"
+                  fill="#3F59E4"
+                />
+                <path
+                  d="M0 0.472H12.056H0ZM12.056 14.028H2C0.0670034 14.028 -1.5 12.461 -1.5 10.528H1.5C1.5 10.8041 1.72386 11.028 2 11.028H12.056V14.028ZM2 14.028C0.0670034 14.028 -1.5 12.461 -1.5 10.528V0.472H1.5V10.528C1.5 10.8041 1.72386 11.028 2 11.028V14.028ZM12.056 0.472V12.528V0.472Z"
+                  fill="#3F59E4"
+                  mask="url(#path-1-inside-1_1471_16106)"
+                />
+              </svg>
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
   </section>
 </template>
 
