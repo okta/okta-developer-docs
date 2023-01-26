a81c3625dd59b3c86a9b684719c96f2b986f7ed8
Harutyun K <harutyun.k@ramotion.agency>
2023-01-26 18:39:58 +0300

    Add comment

--- packages/@okta/vuepress-theme-prose/components/News.vue
+++ packages/@okta/vuepress-theme-prose/components/News.vue
@@ -5,7 +5,9 @@
         What’s new in Okta Developer
       </h2>
       <article class="news__article news__article_size_large">
-        <div class="news__article-image-wrapper"></div>
+        <div class="news__article-image-wrapper">
+          <!-- image -->
+        </div>
         <div class="news__wrapper">
           <p class="news__text dont-break-out dont-break-out">
             Blog
