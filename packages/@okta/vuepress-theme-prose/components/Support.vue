79ca1c8c4a54a40fe8fb5c176c6a98d715aa9d22
Harutyun K <harutyun.k@ramotion.agency>
2023-02-01 14:04:06 +0300

    Update support

--- packages/@okta/vuepress-theme-prose/components/Support.vue
+++ packages/@okta/vuepress-theme-prose/components/Support.vue
@@ -1,42 +1,49 @@
 <template>
   <section class="support">
     <div class="homepage__support support__wrapper wrapper">
-      <h2 class="support__title dont-break-out">
-        Get support
-      </h2>
-      <p class="support__paragraph dont-break-out">
-        Search or post a question in the WIC Developer Forum.
-      </p>
-      <a
-        class="support__link dont-break-out"
-        href="https://devforum.okta.com"
-      >
-        Search the forum
-      </a>
-      <ul class="support__list">
-        <li class="support__list-item">
-          <h3 class="support__paragraph support__paragraph_size_small dont-break-out">
-            Looking for WIC SDKs?
-          </h3>
-          <a
-            class="support__list-link dont-break-out"
-            href="https://github.com/okta"
-          >
-            Visit the Okta GitHub repos
-          </a>
-        </li>
-        <li class="support__list-item">
-          <h3 class="support__paragraph support__paragraph_size_small dont-break-out">
-            Looking for WIC product docs?
-          </h3>
-          <a
-            class="support__list-link dont-break-out"
-            href="https://support.okta.com/help/s/?language=en_US"
-          >
-            Visit Okta Help Center
-          </a>
-        </li>
-      </ul>
+      <div class="support__box">
+        <h2 class="support__title dont-break-out">
+          Get the support you need, when you need it
+        </h2>
+        <p class="support__paragraph dont-break-out">
+          Search or post a question in the Okta Developer Forum.
+        </p>
+        <a
+          class="support__link dont-break-out"
+          href="https://devforum.okta.com"
+        >
+          Go to Okta Developer Forum <span aria-hidden="true">→</span>
+        </a>
+      </div>
+      <aside class="support__sidebar">
+        <h3 class="support__sidebar-title">
+          Additional support
+        </h3>
+        <ul class="support__list">
+          <li class="support__list-item">
+            <h3 class="support__paragraph support__paragraph_size_small dont-break-out">
+              Interested in Workforce Identity Cloud SDKs?
+            </h3>
+            <a
+              class="support__list-link dont-break-out"
+              href="https://github.com/okta"
+            >
+              Visit the Okta GitHub repos
+            </a>
+          </li>
+          <li class="support__list-item">
+            <h3 class="support__paragraph support__paragraph_size_small dont-break-out">
+              Looking for Workforce Identity Cloud product documentation?
+            </h3>
+            <a
+              class="support__list-link dont-break-out"
+              href="https://support.okta.com/help/s/?language=en_US"
+            >
+              Try our Okta Help Center
+            </a>
+          </li>
+        </ul>
+      </aside>
     </div>
   </section>
 </template>
