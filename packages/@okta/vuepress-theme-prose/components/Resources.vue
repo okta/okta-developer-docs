e30de2fe190f58c3d6725cec6c0e12c24e374b1c
Harutyun K <harutyun.k@ramotion.agency>
2023-02-01 16:00:36 +0300

    Update resources

--- packages/@okta/vuepress-theme-prose/components/Resources.vue
+++ packages/@okta/vuepress-theme-prose/components/Resources.vue
@@ -1,16 +1,16 @@
 <template>
   <section class="resources">
     <div class="resources__wrapper wrapper">
+      <h2 class="resources__title dont-break-out">
+        External Resources
+      </h2>
       <div class="resources__block">
-        <h2 class="resources__title dont-break-out">
-          External Resources
-        </h2>
-        <p class="resources__subtitle dont-break-out">
-          Automation and Orchestration
-        </p>
         <span class="resources__status dont-break-out">
           Advanced
         </span>
+        <p class="resources__subtitle dont-break-out">
+          Automation and Orchestration
+        </p>
         <p class="resources__paragraph dont-break-out">
           Automate your Workforce Identity Cloud provisioning with Terraform. Okta articles and partner resources are available to help you get up and running, and as always, the WIC community forum is a great space to ask questions and find peer support.
         </p>
@@ -19,25 +19,6 @@
           href="https://devforum.okta.com/search?q=terraform"
         >
           View Terraform discussions on the forum
-          <svg
-            width="13"
-            height="13"
-            viewBox="0 0 13 13"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            focusable="false"
-          >
-            <path
-              d="M9.552 4.064L9.536 7.968H11.056V1.472H4.56V2.992L8.448 2.976L4 7.408L5.12 8.528L9.552 4.064Z"
-              fill="currentColor"
-            />
-            <path
-              d="M0 0.472H12.056H0ZM12.056 14.028H2C0.0670034 14.028 -1.5 12.461 -1.5 10.528H1.5C1.5 10.8041 1.72386 11.028 2 11.028H12.056V14.028ZM2 14.028C0.0670034 14.028 -1.5 12.461 -1.5 10.528V0.472H1.5V10.528C1.5 10.8041 1.72386 11.028 2 11.028V14.028ZM12.056 0.472V12.528V0.472Z"
-              fill="currentColor"
-              mask="url(#path-1-inside-1_1471_16106)"
-            />
-          </svg>
         </a>
       </div>
       <ul class="resources__list">
@@ -54,12 +35,50 @@
               href="https://developer.okta.com/blog/2022/04/14/where-okta-meets-devops"
             >
               How can DevOps engineers use Okta?
+              <svg
+                width="13"
+                height="13"
+                viewBox="0 0 13 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                focusable="false"
+              >
+                <path
+                  d="M9.552 4.064L9.536 7.968H11.056V1.472H4.56V2.992L8.448 2.976L4 7.408L5.12 8.528L9.552 4.064Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M0 0.472H12.056H0ZM12.056 14.028H2C0.0670034 14.028 -1.5 12.461 -1.5 10.528H1.5C1.5 10.8041 1.72386 11.028 2 11.028H12.056V14.028ZM2 14.028C0.0670034 14.028 -1.5 12.461 -1.5 10.528V0.472H1.5V10.528C1.5 10.8041 1.72386 11.028 2 11.028V14.028ZM12.056 0.472V12.528V0.472Z"
+                  fill="currentColor"
+                  mask="url(#path-1-inside-1_1471_16106)"
+                />
+              </svg>
             </a>
             <a
               class="resources__list-link dont-break-out"
               href="https://developer.okta.com/blog/2021/11/08/k8s-api-server-oidc"
             >
               Secure your Kubernetes cluster for Okta OIDC and Terraform
+              <svg
+                width="13"
+                height="13"
+                viewBox="0 0 13 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                focusable="false"
+              >
+                <path
+                  d="M9.552 4.064L9.536 7.968H11.056V1.472H4.56V2.992L8.448 2.976L4 7.408L5.12 8.528L9.552 4.064Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M0 0.472H12.056H0ZM12.056 14.028H2C0.0670034 14.028 -1.5 12.461 -1.5 10.528H1.5C1.5 10.8041 1.72386 11.028 2 11.028H12.056V14.028ZM2 14.028C0.0670034 14.028 -1.5 12.461 -1.5 10.528V0.472H1.5V10.528C1.5 10.8041 1.72386 11.028 2 11.028V14.028ZM12.056 0.472V12.528V0.472Z"
+                  fill="currentColor"
+                  mask="url(#path-1-inside-1_1471_16106)"
+                />
+              </svg>
             </a>
           </div>
         </li>
