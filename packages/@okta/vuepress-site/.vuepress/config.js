40e16b596f95c3c3a0696e683fe474fab4518e4e
Harutyun K <harutyun.k@ramotion.agency>
2023-02-06 20:17:44 +0400

    Fix minor bugs

--- packages/@okta/vuepress-site/.vuepress/config.js
+++ packages/@okta/vuepress-site/.vuepress/config.js
@@ -203,8 +203,8 @@ module.exports = ctx => ({
         items: [
           { text: 'Contact our team', link: 'https://www.okta.com/contact/', target: '_self' },
           { text: 'Contact sales', link: 'https://www.okta.com/contact-sales/', target: '_self' },
-          { text: 'Developer Service Terms', link: '/terms/' },
-          { text: 'Site Terms', link: 'https://www.okta.com/terms-of-service/' },
+          { text: 'Developer Service terms', link: '/terms/' },
+          { text: 'Site terms', link: 'https://www.okta.com/terms-of-service/' },
 			 { text: 'Privacy policy', link: 'https://www.okta.com/privacy-policy/', target: '_self' },
 			 { text: 'Copyright & trademarks', link: '/copyright/' },
         ]
