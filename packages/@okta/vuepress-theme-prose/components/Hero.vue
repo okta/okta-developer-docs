1fc390161c628ae5fbc76232b75811f95eb85318
Susan Harper <susan.harper@okta.com>
2023-05-02 16:51:46 -0600

    removing the empty line

--- packages/@okta/vuepress-theme-prose/components/Hero.vue
+++ packages/@okta/vuepress-theme-prose/components/Hero.vue
@@ -7,7 +7,6 @@
       <p class="hero__paragraph dont-break-out">
         Our developer portal enables you to deploy auth that protects your users, apps, APIs, and infrastructure.
       </p>
-      <p></p>
       <h3 class="support__paragraph support__paragraph_size_small dont-break-out"><strong>Learn what’s new with Okta at Devday23 on May 16 and 17.</strong>
         <a class="CoveoResultLink"
         href="https://developerday.com/"><strong>REGISTER NOW</strong></a>
