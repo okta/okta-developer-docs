b6c61f719a6abf85c0d25f4a72df7b2f3d7a9767
Susan Harper <susan.harper@okta.com>
2023-05-03 14:36:18 -0600

    fixing errors

--- packages/@okta/vuepress-theme-prose/components/Hero.vue
+++ packages/@okta/vuepress-theme-prose/components/Hero.vue
@@ -7,9 +7,10 @@
       <p class="hero__paragraph dont-break-out">
         Our developer portal enables you to deploy auth that protects your users, apps, APIs, and infrastructure.
       </p>
-      <p style="color: white; font-size: x-large; font-weight: bold">Learn what’s new with Okta at Devday23 on May 16 and 17
-      <br>
-        <a id="hoverlink" href="https://developerday.com/">REGISTER NOW</a>
+      <p style="color: white; font-size: x-large; font-weight: bold">
+        Learn what’s new with Okta at Devday23 on May 16 and 17<br>
+        <a id="hoverlink"
+        href="https://developerday.com/">REGISTER NOW</a>
       </p>
     </div>
   </section>
