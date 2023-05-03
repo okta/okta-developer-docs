88cc310d296825254101538aa9124563b03066eb
Susan Harper <susan.harper@okta.com>
2023-05-03 14:24:58 -0600

    hover part deus

--- packages/@okta/vuepress-theme-prose/components/Hero.vue
+++ packages/@okta/vuepress-theme-prose/components/Hero.vue
@@ -7,9 +7,19 @@
       <p class="hero__paragraph dont-break-out">
         Our developer portal enables you to deploy auth that protects your users, apps, APIs, and infrastructure.
       </p>
-      <p style="color: white; font-size: x-large; font-weight: bold">Learn what’s new with Okta at Devday23 on May 16 and 17.
-      <a style="color: white; font-size: x-large; font-weight: bold; hover: black; href=https://developerday.com/ ">REGISTER NOW</a>
+      <p style="color: white; font-size: x-large; font-weight: bold">Learn what’s new with Okta at Devday23 on May 16 and 17
+      <br>
+        <a id="hoverlink" href="https://developerday.com/">REGISTER NOW</a>
       </p>
     </div>
   </section>
 </template>
+
+<style>
+#hoverlink {
+  color: purple !important;
+}
+#hoverlink:hover {
+  color: white !important;
+}
+</style>
