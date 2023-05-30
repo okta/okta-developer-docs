6b84cea2d0e647d3511ff6fa1ab5806f2ffd956f
Harutyun Karapetyan <harutyun.k@ramotion.agency>
2023-05-30 23:14:03 +0400

    Add broken links

--- linkinator.js
+++ linkinator.js
@@ -4,11 +4,14 @@ const EXTERNAL_LINKS = 'packages/**/*.md';
 const LINK_EXTERNAL_REGEXP = /^(https?|http):\/\/[^\s$.?#].[^\s]*$/gm;
 
 const checker = new LinkChecker();
+const brokenLinks = [];
 
 checker.on('link', (link) => {
-  if (link.status !== 200) {
-    if (link.url.match(LINK_EXTERNAL_REGEXP)) {
-      console.log(link);
+  if (link.url.match(LINK_EXTERNAL_REGEXP)) {
+    if (link.status === 404) {
+      brokenLinks.push({
+        url: link.url,
+      })
     }
   }
 });
@@ -17,3 +20,9 @@ await checker.check({
   path: EXTERNAL_LINKS,
   recurse: true,
 });
+
+if (brokenLinks.length) {
+  for (const brokenLink of brokenLinks) {
+    console.log(brokenLink.url);
+  }
+}
