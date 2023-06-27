4021f19e72627bc23827dce297fbcfaf86de706c
Paul Wallace <paul.wallace@okta.com>
2023-06-26 21:34:02 -0700

    Fixing linting issues

--- packages/@okta/vuepress-theme-prose/components/Feedback.vue
+++ packages/@okta/vuepress-theme-prose/components/Feedback.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="feedback-tab">
-    <a id="feedback-link"
-       href="#"
-       onclick="feedback.openSurvey();  return false"
-       target="_blank"
-       title="Submit feedback">
+    <a
+      id="feedback-link"
+      href="#"
+      onclick="feedback.openSurvey();  return false"
+      target="_blank"
+      title="Submit feedback"
+    >
       <div id="feedback-container">
         <p id="feedback-text">Feedback</p>
       </div>
