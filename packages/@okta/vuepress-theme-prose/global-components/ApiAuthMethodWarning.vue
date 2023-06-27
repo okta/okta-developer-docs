ec751a43c2d35dfd97c23c9d147e265cc6cbd620
vanngo-okta <80703015+vanngo-okta@users.noreply.github.com>
2023-06-27 19:19:45 -0400

    Update packages/@okta/vuepress-theme-prose/global-components/ApiAuthMethodWarning.vue
    Co-authored-by: Brian Duffield - Okta <70648001+brianduffield-okta@users.noreply.github.com>

--- packages/@okta/vuepress-theme-prose/global-components/ApiAuthMethodWarning.vue
+++ packages/@okta/vuepress-theme-prose/global-components/ApiAuthMethodWarning.vue
@@ -1,6 +1,6 @@
 <template>
   <blockquote>
-    <p><strong>Note</strong>: Some of the curl code examples on this page include SSWS API token authentication. However, Okta recommends using scoped OAuth 2.0 and OIDC access tokens to authenticate with Okta management APIs. OAuth 2.0 and OIDC access tokens provide fine-grain control over the bearer's actions on specific endpoints compared to SSWS tokens. See Okta API <a href="/docs/reference/core-okta-api/#authentication">authentication</a> methods.</p>
+    <p><strong>Note</strong>: Some of the curl code examples on this page include SSWS API token authentication. However, Okta recommends using scoped OAuth 2.0 and OIDC access tokens to authenticate with Okta management APIs. OAuth 2.0 and OIDC access tokens provide fine-grain control over the bearer's actions on specific endpoints. See Okta API <a href="/docs/reference/core-okta-api/#authentication">authentication methods</a>.</p>
   </blockquote>
 </template>
 
