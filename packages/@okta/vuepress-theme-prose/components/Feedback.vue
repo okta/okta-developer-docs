ad57644a83c6c51ce6a31c3cfc8910df38be5632
Paul Wallace <paul.wallace@okta.com>
2023-07-05 13:54:52 -0700

    Adding feedback compontent

---
+++ packages/@okta/vuepress-theme-prose/components/Feedback.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="feedback-tab">
+    <a
+      id="feedback-link"
+      href="#"
+      onclick="feedback.openSurvey();  return false"
+      target="_blank"
+      title="Submit feedback"
+    >
+      <div id="feedback-container">
+        <p id="feedback-text">Feedback</p>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+  feedback = (function(window) {
+    function openSurvey() {
+      window.open('https://surveys.okta.com/jfe/form/SV_e4L0iW8a3tz8Yol?source=' + encodeURIComponent(document.location.href), '_blank');
+    }
+    return {
+      openSurvey: openSurvey
+    };
+  })(window);
+</script>
