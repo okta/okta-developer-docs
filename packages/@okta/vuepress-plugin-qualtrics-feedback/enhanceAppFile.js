b6accf4cb85b2600fa4180cbf750e4078d14e671
Paul Wallace <paul.wallace@okta.com>
2023-07-05 22:25:15 -0700

    Removing feedback JS plugin

--- packages/@okta/vuepress-plugin-qualtrics-feedback/enhanceAppFile.js
+++
@@ -1,8 +0,0 @@
-export default () => {
-    if (process.env.NODE_ENV === 'production' && typeof document !== 'undefined') {
-        (function () {
-            // Making this a no-op
-            // Refactoring the feedback widget as a component
-        }())
-    }
-}
