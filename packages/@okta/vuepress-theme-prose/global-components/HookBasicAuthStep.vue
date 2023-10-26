fc7bafa6452b87c2521e0cd26fcb3abe0630b425
Brian Duffield <brian.duffield@okta.com>
2023-10-26 10:23:17 -0400

    Update as per local review

--- packages/@okta/vuepress-theme-prose/global-components/HookBasicAuthStep.vue
+++ packages/@okta/vuepress-theme-prose/global-components/HookBasicAuthStep.vue
@@ -1,12 +1,10 @@
 <template>
-  <blockquote>
     <p>Add the <strong>Authentication field</strong> and <strong>Authentication secret</strong> values. This example uses HTTP Basic Authentication. Refer to [HTTP header: Basic Authentication](/docs/guides/common-hook-set-up-steps/nodejs/main/#http-header-basic-authentication).</p>
-    <br><ul>
+    <ul>
     <li>><strong>Authentication field</strong> = <code>authorization</code><li>
 
     <li><strong>Authentication secret</strong> = <code>Basic YWRtaW46c3VwZXJzZWNyZXQ=`</code></li>
     </ul>
-  </blockquote>
 </template>
 
 <script>
