6e341a534a0d45c1e2e09c2a8823c89b44419a7f
Brian Duffield <brian.duffield@okta.com>
2023-10-26 11:11:29 -0400

    Adding new component for oauth note

--- packages/@okta/vuepress-theme-prose/global-components/HookBasicAuthStep.vue
+++ packages/@okta/vuepress-theme-prose/global-components/HookBasicAuthStep.vue
@@ -5,9 +5,6 @@
     <li><strong>Authentication secret</strong> = <code>Basic YWRtaW46c3VwZXJzZWNyZXQ=</code></li>
     </ul>
     </p>
-    <blockquote>
-    <p><strong>Note:</strong> If you want to use OAuth 2.0 to secure your inline hooks, see <a href="https://developer.okta.com/docs/guides/common-hook-set-up-steps/nodejs/main/#oauth-2-0-client-secret">OAuth 2.0: Client Secret</a> or <a href="https://developer.okta.com/docs/guides/common-hook-set-up-steps/nodejs/main/#oauth-2-0-private-key">OAuth 2.0: Private Key</a>.</p>
-    </blockquote>
 </template>
 
 <script>
