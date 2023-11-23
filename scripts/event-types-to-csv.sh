87e251db7075aa2f28170037d87d2e60f07365e6
Brian Duffield <brian.duffield@okta.com>
2023-11-23 17:26:08 -0500

    adding working file

--- scripts/event-types-to-csv.sh
+++ scripts/event-types-to-csv.sh
@@ -13,4 +13,14 @@ fi
 #jq -r '.eventTypes | map([.version, .releaseDate] | @csv) | (.[0] | keys_unsorted) as $keys | $keys, .[]' "$json_file" > "$csv_file"
 
 # Use jq to convert "eventTypes" array within "versions" array to CSV, if it exists
-jq -r 'if has("versions") and (.versions | arrays) then .versions[] | select(has("eventTypes") and (.eventTypes | arrays)) | .eventTypes | map([.id, .description] | @csv) | (.[0] | keys_unsorted) as $keys | $keys, .[] else empty end' "$json_file" > "$csv_file"
+#jq -r 'if has("versions") and (.versions | arrays) then .versions[] | select(has("eventTypes") and (.eventTypes | arrays)) | .eventTypes | map([.id, .description] | @csv) | (.[0] | keys_unsorted) as $keys | $keys, .[] else empty end' "$json_file" > "$csv_file"
+
+#jq -r '["id", "category", "description", "info created, "info release], (.versions[].eventTypes[] | select(.id.id) | [.id.category, .id.category, .id.info.created, .id.info.release]) | @csv' "$json_file" > "$csv_file"
+
+# WORKSjq -r '.versions[] | ["version", .version] | @csv' "$json_file" > "$csv_file"
+
+jq -r '.versions[].eventTypes[] | select(.beta != true and .internal !=true) | [ .id // empty, .description // empty, .tags[] // empty, .info.release //empty] | @csv' "$json_file" > "$csv_file"
+
+# jq -r '.versions[].eventTypes[] | tojson | @csv' "$json_file" > "$csv_file"
+
+echo "CSV file generated: $csv_file"
