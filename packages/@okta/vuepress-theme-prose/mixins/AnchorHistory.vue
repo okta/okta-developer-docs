145b22d8329088a56d96561ceed0a0276014273d
emikhasyak-okta <emikhasyak@okta.com>
2023-11-28 13:07:53 -0500

    Updates anchor offsets (#4513)

--- packages/@okta/vuepress-theme-prose/mixins/AnchorHistory.vue
+++ packages/@okta/vuepress-theme-prose/mixins/AnchorHistory.vue
@@ -5,38 +5,50 @@ export default {
   inject: ['appContext'],
   data() {
     return {
-      paddedHeaderHeight: 0
+      paddedHeaderHeight: 0,
+      anchorOffset: []
     };
   },
-  computed: {
-    anchorsOffset() {
+  mounted() {},
+  methods: {
+    getPaddedHeaderHeight: function () {
+      return  document.querySelector(".fixed-header").clientHeight +
+              LAYOUT_CONSTANTS.HEADER_TO_CONTENT_GAP;
+    },
+
+    setAnchors: function (anchors) {
+        this.anchors = anchors;
+        this.getAnchorsOffset();
+    },
+    getAnchorsOffset: function () {
+      if (!this.anchors) {
+        return;
+      }
+
+      this.paddedHeaderHeight = this.getPaddedHeaderHeight()
+
       const anchorOffsets = this.anchors.map(
         anchor => anchor.parentElement.offsetTop
       );
 
-      return anchorOffsets.map((anchorOffset, index, anchorOffsets) => ({
+      this.anchorsOffset = anchorOffsets.map((anchorOffset, index, anchorOffsets) => ({
         start: anchorOffset,
         end: anchorOffsets[index + 1]
       }));
-    }
-  },
-  mounted() {
-    this.paddedHeaderHeight =
-      document.querySelector(".fixed-header").clientHeight +
-      LAYOUT_CONSTANTS.HEADER_TO_CONTENT_GAP;
-  },
-  methods: {
+    },
+
     scrollToAnchor: function(anchorId) {
       const target = document.querySelector(anchorId);
-      if (target) {
-        const scrollToPosition = target.offsetTop - this.paddedHeaderHeight;
-        window.scrollTo(0, scrollToPosition);
-        // Chrome & Safari: when zoomed in/out, window.scrollTo does not always perform scroll strictly equal to passed parameter
-        // https://bugs.chromium.org/p/chromium/issues/detail?id=890345
-        if (window.scrollY < scrollToPosition) {
-          const scrollAlignment = 2;
-          window.scrollBy(0, scrollAlignment);
-        }
+      if (!target) {
+        return;
+      }
+      const scrollToPosition = target.offsetTop - this.getPaddedHeaderHeight();
+      window.scrollTo(0, scrollToPosition);
+      // Chrome & Safari: when zoomed in/out, window.scrollTo does not always perform scroll strictly equal to passed parameter
+      // https://bugs.chromium.org/p/chromium/issues/detail?id=890345
+      if (window.scrollY < scrollToPosition) {
+        const scrollAlignment = 2;
+        window.scrollBy(0, scrollAlignment);
       }
     },
 
