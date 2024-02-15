3db2c078862e8dc9095e71c42cfcd3135bb5901f
ishan-krishna-okta <ishan.krishna@okta.com>
2024-02-15 12:24:28 +0530

    Added home page banner component in dev docs

--- packages/@okta/vuepress-theme-prose/components/Hero.vue
+++ packages/@okta/vuepress-theme-prose/components/Hero.vue
@@ -1,25 +1,74 @@
 <template>
   <!-- Remove style attribute from .hero section when removing promo banner -->
-  <section
-    class="hero"
-    style="padding-top: 28px; padding-bottom: 28px;"
-  >
-    <div class="hero__wrapper wrapper">
-      <h1 class="hero__title dont-break-out">
-        Okta developer
-      </h1>
-      <p class="hero__paragraph dont-break-out">
-        Our developer portal enables you to deploy auth that protects your users, apps, APIs, and infrastructure.
-      </p>
-      <!-- Start workshops promo banner -->
-      <p style="color: white; font-size: 18px; line-height: 1.4; max-width: 500px;">
-        Get your app <a
-          style="color: white; text-decoration: underline;"
-          href="https://regionalevents.okta.com/enterprisereadyworkshops"
-          target="_blank"
-        >enterprise-ready with free virtual workshops</a>!
-      </p>
-      <!-- End workshops promo banner -->
-    </div>
-  </section>
+  <div class="hero-container">
+    <section
+      class="hero"
+      style="padding-top: 28px; padding-bottom: 28px;"
+    >
+      <div class="hero-wrapper-container">
+        <div :class="{ 'hero__wrapper': true, 'wrapper': true, 'full-width-wrapper': !showBanner }">
+          <h1 class="hero__title dont-break-out">
+            Okta developer
+          </h1>
+          <p class="hero__paragraph dont-break-out">
+            Our developer portal enables you to deploy auth that protects your users, apps, APIs, and infrastructure.
+          </p>
+          <!-- Start workshops promo banner -->
+          <p style="color: white; font-size: 18px; line-height: 1.4; max-width: 500px;">
+            Get your app <a
+              style="color: white; text-decoration: underline;"
+              href="https://regionalevents.okta.com/enterprisereadyworkshops"
+              target="_blank"
+            >enterprise-ready with free virtual workshops</a>!
+          </p>
+          <!-- End workshops promo banner -->
+        </div>
+        <!-- 
+          For understanding usage of the banner, refer https://oktawiki.atlassian.net/wiki/spaces/DOC/pages/2920874189/Adding+a+banner+on+homepage+in+dev+docs
+         -->
+        <HomePageBanner
+          v-if="showBanner"
+          primary-cta-text="Primary button"
+          primary-cta-link="/docs/guides/"
+          primary-link-target="_self"
+          secondary-cta-text="Secondary button"
+          secondary-cta-link="https://www.yahoo.com"
+          @onPrimaryCtaClick="handlePrimaryCtaClick()"
+          @onSecondaryCtaClick="handleSecondaryCtaClick()"
+        >
+          <template #heading>
+            Banner heading
+          </template>
+          <template #description>
+            Get your app
+            <a
+              href="https://regionalevents.okta.com/enterprisereadyworkshops"
+              target="_blank"
+            >
+              enterprise-ready with free virtual workshops
+            </a>!
+          </template>
+        </HomePageBanner>
+      </div>
+    </section>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "Hero",
+  data() {
+    return {
+      showBanner: false
+    }
+  },
+  methods: {
+    handlePrimaryCtaClick() {
+      // This callback gets triggered when primary CTA button is clicked in the HomePageBanner
+    },
+    handleSecondaryCtaClick() {
+      // This callback gets triggered when secondary CTA button is clicked in the HomePageBanner
+    },
+  },
+};
+</script>
---
+++ packages/@okta/vuepress-theme-prose/global-components/HomePageBanner.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="home-page-banner">
+    <div :class="{ 'home-page-banner--heading': true, 'home-page-banner--heading-hidden': !$slots.heading }">
+      <slot name="heading" />
+    </div>
+    <div class="home-page-banner--description">
+      <slot name="description" />
+    </div>
+
+    <div
+      v-if="primaryCtaText || secondaryCtaText"
+      class="home-page-banner--cta-container"
+    >
+      <a
+        v-if="primaryCtaLink"  
+        :href="primaryCtaLink"
+        :target="primaryLinkTarget ? primaryLinkTarget : '_blank'"
+      >
+        <button
+          v-if="primaryCtaText"
+          class="home-page-action-button primary-button"
+          @click="handlePrimaryCtaClick()"
+        >
+          {{ primaryCtaText }}
+        </button>
+      </a>
+      <button
+        v-else-if="primaryCtaText"
+        class="home-page-action-button primary-button"
+        @click="handlePrimaryCtaClick()"
+      >
+        {{ primaryCtaText }}
+      </button>
+      <a
+        v-if="secondaryCtaLink" 
+        :href="secondaryCtaLink"
+        :target="secondaryLinkTarget ? secondaryLinkTarget : '_blank'"
+      >
+        <button
+          v-if="secondaryCtaText"
+          class="home-page-action-button secondary-button"
+          @click="handleSecondaryCtaClick()"
+        >
+          {{ secondaryCtaText }}
+        </button>
+      </a>
+      <button
+        v-else-if="secondaryCtaText"
+        class="home-page-action-button secondary-button"
+        @click="handleSecondaryCtaClick()"
+      >
+        {{ secondaryCtaText }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePageBanner",
+  props: ["primaryCtaLink", "primaryCtaText", "primaryLinkTarget", "secondaryCtaLink", "secondaryCtaText", "secondaryLinkTarget"],
+  methods: {
+    handlePrimaryCtaClick() {
+      this.$emit("onPrimaryCtaClick");
+    },
+    handleSecondaryCtaClick() {
+      this.$emit("onSecondaryCtaClick");
+    },
+  },
+};
+</script>
