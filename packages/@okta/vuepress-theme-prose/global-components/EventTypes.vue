8d5ef291718d6bbf72a3f33657977fb8259766cf
Brian Duffield <brian.duffield@okta.com>
2024-03-15 15:38:13 -0400

    Fixing vue script spacing issues. I think. I think. This is it. The solution.

--- packages/@okta/vuepress-theme-prose/global-components/EventTypes.vue
+++ packages/@okta/vuepress-theme-prose/global-components/EventTypes.vue
@@ -65,10 +65,10 @@
       </p>
 
       <div
-       v-if="['user.risk.change', 'analytics.feedback.provide', 'security.events.provider.receive_event', 'device.signals.status.timeout', 
-                  'policy.auth.reevaluate.fail', 'policy.continuous_access.evaluate', 'policy.continuous_access.action', 'user.session.context.changed', 
-                  'policy.entity_risk.evaluate', 'policy.entity_risk.action', 'user.session.end', 'user.session.clear', 'user.authentication.universal_logout', 
-                  'workflows.user.delegatedflow.run'].indexOf(eventType.id) !== -1"
+        v-if="['user.risk.change', 'analytics.feedback.provide', 'security.events.provider.receive_event', 'device.signals.status.timeout', 
+               'policy.auth.reevaluate.fail', 'policy.continuous_access.evaluate', 'policy.continuous_access.action', 'user.session.context.changed', 
+               'policy.entity_risk.evaluate', 'policy.entity_risk.action', 'user.session.end', 'user.session.clear', 'user.authentication.universal_logout', 
+               'workflows.user.delegatedflow.run'].indexOf(eventType.id) !== -1"
       >
         <p class="event-itp-type">
           See also: <a href="http://localhost:8080/docs/reference/api/itp-et">ITP Event Types</a>
