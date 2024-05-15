4ef0abc623473b8eeb2d0dd1e46a0be531514554
ishan-krishna-okta <ishan.krishna@okta.com>
2024-05-15 14:03:35 +0530

    Test preview links

--- scripts/update-infodev-pr-cci.sh
+++ scripts/update-infodev-pr-cci.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-netlify deploy --alias=preview-${CIRCLE_PULL_REQUEST##*/} --filter @okta/vuepress-site
-
-export HTTP_FIND_PR=$(curl -s \
+export PR_DETAILS=$(curl -s \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer ${OKTA_GH_TOKEN}" \
     https://api.github.com/repos/okta/okta-developer-docs/pulls?head="okta:${CIRCLE_BRANCH}")
 
-export PR_NUMBER=$(echo ${HTTP_FIND_PR} | jq -r '.[0].number')
+export PR_NUMBER=$(echo ${PR_DETAILS} | jq -r '.[0].number')
+
+TARGET_NAME=""
 
 if [ "${URL}" != "null" ]; then
     TARGET_NAME="https://preview-${PR_NUMBER}--reverent-murdock-829d24.netlify.app"
@@ -23,5 +23,4 @@ else
     echo There is no PR associated with ${CIRCLE_BRANCH}
 fi
 
-echo
-echo PR: ${TARGET_NAME}
+echo Preview Link: ${TARGET_NAME}
