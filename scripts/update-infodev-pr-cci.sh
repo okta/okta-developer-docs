914d3009db219e46d7567bb4cc628929cdc83f4e
ishan-krishna-okta <ishan.krishna@okta.com>
2024-05-15 14:49:12 +0530

    Test preview links

--- scripts/update-infodev-pr-cci.sh
+++ scripts/update-infodev-pr-cci.sh
@@ -19,13 +19,16 @@ if [ "${URL}" != "null" ]; then
         https://api.github.com/repos/okta/okta-developer-docs/issues/${PR_NUMBER}/comments)
     
     echo ${ISSUE_COMMENTS_BY_BOT}
-    # curl -L -s \
-    #     -X POST \
-    #     -H "Accept: application/vnd.github+json" \
-    #     -H "Authorization: Bearer ${OKTA_GH_TOKEN}" \
-    #     -H "X-GitHub-Api-Version: 2022-11-28" \
-    #     https://api.github.com/repos/okta/okta-developer-docs/issues/${PR_NUMBER}/comments \
-    #     -d '{"body":"Preview URL for the changes: '"${TARGET_NAME}"'"}'
+
+    if [[ ! $(echo "$ISSUE_COMMENTS_BY_BOT" | jq '.[] | select(.user.id == 164419112 and .body | contains("Preview URL for the changes:"))') ]]; then
+        curl -L -s \
+            -X POST \
+            -H "Accept: application/vnd.github+json" \
+            -H "Authorization: Bearer ${OKTA_GH_TOKEN}" \
+            -H "X-GitHub-Api-Version: 2022-11-28" \
+            https://api.github.com/repos/okta/okta-developer-docs/issues/${PR_NUMBER}/comments \
+            -d '{"body":"Preview URL for the changes: '"${TARGET_NAME}"'"}'
+    fi
 else
     echo There is no PR associated with ${CIRCLE_BRANCH}
 fi
