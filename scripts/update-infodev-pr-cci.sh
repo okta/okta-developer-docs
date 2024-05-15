6a711812e282808e380bcaeda670ceffc9a89ca0
ishan-krishna-okta <ishan.krishna@okta.com>
2024-05-15 14:29:04 +0530

    Test preview links

--- scripts/update-infodev-pr-cci.sh
+++ scripts/update-infodev-pr-cci.sh
@@ -12,15 +12,22 @@ TARGET_NAME=""
 if [ "${URL}" != "null" ]; then
     TARGET_NAME="https://preview-${PR_NUMBER}--reverent-murdock-829d24.netlify.app"
     echo Added preview link to PR
-    curl -L -s \
-        -X POST \
+    ISSUE_COMMENTS_BY_BOT=$(curl -L \
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer ${OKTA_GH_TOKEN}" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-        https://api.github.com/repos/okta/okta-developer-docs/issues/${PR_NUMBER}/comments \
-        -d '{"body":"Preview URL for the changes: '"${TARGET_NAME}"'"}'
+        https://api.github.com/repos/okta/okta-developer-docs/issues/${PR_NUMBER}/comments)
+    
+    echo ${ISSUE_COMMENTS_BY_BOT}
+    # curl -L -s \
+    #     -X POST \
+    #     -H "Accept: application/vnd.github+json" \
+    #     -H "Authorization: Bearer ${OKTA_GH_TOKEN}" \
+    #     -H "X-GitHub-Api-Version: 2022-11-28" \
+    #     https://api.github.com/repos/okta/okta-developer-docs/issues/${PR_NUMBER}/comments \
+    #     -d '{"body":"Preview URL for the changes: '"${TARGET_NAME}"'"}'
 else
     echo There is no PR associated with ${CIRCLE_BRANCH}
 fi
 
-echo Preview Link: ${TARGET_NAME}
+# echo Preview Link: ${TARGET_NAME}
