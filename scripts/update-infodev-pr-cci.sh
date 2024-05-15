b737b44985c906638668f0fd1fb379f13b1b8219
ishan-krishna-okta <ishan.krishna@okta.com>
2024-05-15 13:42:04 +0530

    Test preview links

--- scripts/update-infodev-pr-cci.sh
+++ scripts/update-infodev-pr-cci.sh
@@ -2,22 +2,15 @@
 
 netlify deploy --alias=preview-${CIRCLE_PULL_REQUEST##*/} --filter @okta/vuepress-site
 
-TARGET_NAME="https://preview-${CIRCLE_PULL_REQUEST}--reverent-murdock-829d24.netlify.app"
-
-echo ${CIRCLE_BRANCH}
-echo ${TARGET_NAME}
-
 export HTTP_FIND_PR=$(curl -s \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer ${OKTA_GH_TOKEN}" \
     https://api.github.com/repos/okta/okta-developer-docs/pulls?head="okta:${CIRCLE_BRANCH}")
 
-echo ${HTTP_FIND_PR}
-
-export URL=$(echo ${HTTP_FIND_PR} | jq -r '.[0].html_url')
 export PR_NUMBER=$(echo ${HTTP_FIND_PR} | jq -r '.[0].number')
 
 if [ "${URL}" != "null" ]; then
+    TARGET_NAME="https://preview-${PR_NUMBER}--reverent-murdock-829d24.netlify.app"
     echo Added preview link to PR
     curl -L -s \
         -X POST \
@@ -25,7 +18,7 @@ if [ "${URL}" != "null" ]; then
         -H "Authorization: Bearer ${OKTA_GH_TOKEN}" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
         https://api.github.com/repos/okta/okta-developer-docs/issues/${PR_NUMBER}/comments \
-        -d '{"body":"Preview URL: '"${TARGET_NAME}"'"}'
+        -d '{"body":"Preview URL for the changes: '"${TARGET_NAME}"'"}'
 else
     echo There is no PR associated with ${CIRCLE_BRANCH}
 fi
