6f4291e65b2bc6d293f74ccf78e80f1af83dec77
ishan-krishna-okta <ishan.krishna@okta.com>
2024-05-17 17:28:54 +0530

    Added urls as links and added email env variable

--- scripts/send-nightly-email.sh
+++ scripts/send-nightly-email.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-broken_urls=$(comm -23 <(sort urls.txt | uniq) <(sort falsepos.txt | uniq))
+# broken_urls=$(comm -23 <(sort urls.txt | uniq) <(sort falsepos.txt | uniq))
 
-# Use sed to replace spaces with newlines
-broken_urls=$(echo "$broken_urls" | sed 's/ /\n/g')
+# # Use sed to replace spaces with newlines
+# broken_urls=$(echo "$broken_urls" | sed 's/ /\n/g')
 
-# Use awk to format each URL into an HTML anchor tag with line break
-formatted_broken_urls=$(echo "$broken_urls" | awk '{printf "<a href=\"%s\">%s</a><br/>\n", $0, $0}')
+# # Use awk to format each URL into an HTML anchor tag with line break
+# formatted_broken_urls=$(echo "$broken_urls" | awk '{printf "<a href=\"%s\">%s</a><br/>\n", $0, $0}')
 
-if [ -n "$formatted_broken_urls" ]; then
+# if [ -n "$formatted_broken_urls" ]; then
     curl --location --request POST 'https://www.cinotify.cc/api/notify' \
             -d "to=${NOTIFY_EMAIL}&subject=FAILED: Dev Docs Nightly Job&type=text/html&body=<p style='font-family: Arial, sans-serif; font-size: 16px; color: #333; line-height: 1.6; text-align: center; background-color: #ffcccc; padding: 10px;'>
             <span style='font-weight: bold;'>Nightly link checker job failed. See the list of broken URLs below.</span><br><br>
-            <a href='https://app.circleci.com/pipelines/github/okta/okta-developer-docs/<< pipeline.number >>' style='display: inline-block; padding: 5px 10px; background-color: #4caf50; color: white; text-decoration: none;'>Click here</a>
+            <a href='https://app.circleci.com/pipelines/github/okta/okta-developer-docs/${pipeline.number}' style='display: inline-block; padding: 5px 10px; background-color: #4caf50; color: white; text-decoration: none;'>Click here</a>
             to see job details.<br>
         </p><h2 style='font-family: Arial, sans-serif; font-size: 16px; color: #333; line-height: 1.6; text-align: center;'>List of broken URLs:</h2>$formatted_broken_urls"
-fi
+# fi
