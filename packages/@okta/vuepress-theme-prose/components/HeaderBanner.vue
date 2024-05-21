ab59fab40281dc3e5cf3f861b61026b80678c138
madhurishivakumar-okta <madhuri.shivakumar@okta.com>
2024-05-21 13:19:28 +0530

    Add new header banner component

---
+++ packages/@okta/vuepress-theme-prose/components/HeaderBanner.vue
@@ -0,0 +1,113 @@
+<template>
+  <div v-if="!isDismissed" class="header-banner">
+    <div class="header-banner-content">
+      <slot></slot>
+    </div>
+    <button v-if="dismissable" class="dismiss-btn" @click="dismissBanner">
+      <img src="/img/icons/icon--x.svg" alt="close" />
+    </button>
+  </div>
+</template>
+
+<script>
+import storage from "../util/localStorage";
+
+export default {
+  name: "HeaderBanner",
+  props: {
+    dismissable: {
+      type: Boolean,
+      default: false,
+    },
+    bannerId: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      isDismissed: false,
+      darkMode: false,
+    };
+  },
+  mounted() {
+    this.checkDismissal();
+    this.updateHeaderHeight();
+  },
+  methods: {
+    dismissBanner() {
+      this.isDismissed = true;
+      storage.setItem(`banner_dismissed_${this.bannerId}`, "true");
+      this.updateHeaderHeight();
+    },
+    checkDismissal() {
+      if (this.dismissable) {
+        this.isDismissed =
+          storage.getItem(`banner_dismissed_${this.bannerId}`) === "true";
+      }
+    },
+    updateHeaderHeight() {
+      this.$nextTick(() => {
+        this.$emit("updateHeight");
+      });
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.header-banner {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 12px;
+  padding: 10px 50px;
+  position: relative;
+  transition: background-color 0.3s;
+  color: #ffffff;
+  background: linear-gradient(
+    90deg,
+    #1a31a9 -26.4%,
+    #3f66e4 58.74%,
+    #b6caff 143.88%
+  );
+
+  @media screen and (max-width: 1440px) {
+    padding: 10px 35px;
+  }
+
+  @media screen and (max-width: 1024px) {
+    padding: 10px 20px;
+  }
+}
+
+body.dark-theme .header-banner {
+  background: linear-gradient(90deg, #202725 0%, #4cb7a3 98.5%);
+}
+
+.header-banner-content {
+  flex-grow: 1;
+  font-size: 14px;
+  font-weight: 400;
+
+  a {
+    color: #ffffff;
+    text-decoration: underline;
+    font-weight: 500;
+    text-wrap: nowrap;
+  }
+}
+
+.dismiss-btn {
+  background: none;
+  border: none;
+  padding: 0;
+  cursor: pointer;
+  flex-shrink: 0;
+
+  img {
+    width: 24px;
+    height: 24px;
+  }
+}
+</style>
