2d7cc0bdb65c9e2dad9a1ec9cf16d298257ac4c9
Anne Juan <anne.juan@okta.com>
2024-07-22 14:35:38 -0700

    Deprecate XAAS page on Vuepress

--- packages/@okta/vuepress-theme-prose/const/navbar.const.js
+++ packages/@okta/vuepress-theme-prose/const/navbar.const.js
@@ -1025,7 +1025,7 @@ export const reference = [
                { title: "Features", path: "https://developer.okta.com/docs/api/openapi/okta-management/management/tag/Feature/" },
                { title: "Groups", path: "/docs/reference/api/groups/" },
                { title: "Groups Owners", path: "https://developer.okta.com/docs/api/openapi/okta-management/management/tag/GroupOwner/" },
-               { title: "Identity Sources", path: "/docs/reference/api/xaas/" },
+               { title: "Identity Sources", path: "https://developer.okta.com/docs/api/openapi/okta-management/management/tag/IdentitySource/" },
                { title: "Key Management", path: "/docs/reference/api/hook-keys/" },
                { title: "Linked Objects", path: "/docs/reference/api/linked-objects/" },
                { title: "Log Streaming", path: "https://developer.okta.com/docs/api/openapi/okta-management/management/tag/LogStream/#tag/LogStream" },
