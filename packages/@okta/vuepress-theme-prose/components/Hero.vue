9abf0ba1c9a920c2d2e4467991e01cf2a9453eb0
Brian Duffield <brian.duffield@okta.com>
2025-05-02 08:26:14 -0400

    Updated content

--- packages/@okta/vuepress-theme-prose/components/Hero.vue
+++ packages/@okta/vuepress-theme-prose/components/Hero.vue
@@ -39,7 +39,7 @@
           Coming May 22, 2025!
           </template>
           <template #description>
-            Okta is introducing a new Integrator Free plan, designed for both developers and integrators. This new org replaces the existing Okta Developer Edition Service. If you are an existing Okta Developer Edition user, you'll need to sign up for a new org.
+            Okta is introducing a new Integrator Free Plan, designed for both developers and integrators. Existing Okta Developer Edition Service orgs will be deactivated on July 18, 2025. If you are an existing Okta Developer Edition user, you'll need to sign up for a new org.
           </template>
         </HomePageBanner>
       </div>
@@ -52,9 +52,9 @@ export default {
   name: "Hero",
   computed: {
     showBanner() {
-      const bannerStartTime = new Date('2025-04-28T01:00:00-04:00');
+      const bannerStartTime = new Date('2025-05-12T01:00:00-04:00');
       const bannerStartTimeEpoch = Math.floor(bannerStartTime.getTime() / 1000);
-      const bannerEndTime = new Date('2025-04-30T23:59:00-04:00');
+      const bannerEndTime = new Date('2025-05-22T23:59:00-04:00');
       const bannerEndTimeEpoch = Math.floor(bannerEndTime.getTime() / 1000);
 
       const currentTimeEpoch = Math.floor(Date.now() / 1000);
