d2ab986253fde5ce80b2be8a622024a7e58aded4
Anne Juan <anne.juan@okta.com>
2025-08-05 15:39:29 -0700

    Removes changelog and related links

--- packages/@okta/vuepress-theme-prose/const/navbar.const.js
+++ packages/@okta/vuepress-theme-prose/const/navbar.const.js
@@ -1117,10 +1117,6 @@ export const releaseNotes = [
         path: "/docs/release-notes/archive",
         subLinks: [
           {
-            title: "2023-2024 Okta Identity Governance changelog",
-            path: "/docs/release-notes/oig-changelog/"
-          },
-          {
             title: "2024 - Classic Engine",
             path: "/docs/release-notes/2024/"
           },
