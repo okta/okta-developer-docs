5b8d292a9d50f363e92a10eb22074e0c0406ca40
ishan-krishna-okta <ishan.krishna@okta.com>
2025-09-29 14:20:46 +0530

    Made improvements to 404 page

---
+++ packages/@okta/vuepress-theme-prose/components/Error404.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="page-content">
+    <section class="errors">
+      <div class="error404--container">
+        <div class="errors--leader">
+          <img width="120" height="120" 
+          src="/img/icons/404.png"
+          alt="error 404 icon">
+          <h1 class="errors--title">
+            Oops! Page not found!
+          </h1>
+        </div>
+      </div>
+      <div class="error404--container">
+        <div class="errors--text">
+          <div class="errors--section-paragraph">
+            The page you requested is unavailable. It may have been moved, removed, or is no longer valid.
+          </div>
+
+          <div class="errors--section-paragraph possible-causes">
+            Possible causes:
+            <ul>
+              <li>
+                <span class="errors--section-paragraph-heading">Truncated URL:</span> Some email clients may break links across lines. Copy and paste the full URL into your browser.</li>
+              <li><span class="errors--section-paragraph-heading">Case sensitivity:</span> URLs are case sensitive. For example, <code>File.html</code> and <code>file.html</code> are treated as different files.</li>
+            </ul>
+          </div>
+
+          <div class="errors--section-paragraph">Archived content on unsupported technologies is available in the <SmartLink :item="{ link: '/archive/' }">
+            archive</SmartLink>.</div>
+          <div class="errors--section-paragraph">For further assistance, please <a href='mailto:info@okta.com'>contact us</a>.</div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    SmartLink: () => import("../components/SmartLink")
+  },
+}
+</script>
