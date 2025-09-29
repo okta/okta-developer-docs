725071050fdfebe613447f16ae1bc831da1fa516
ishan-krishna-okta <ishan.krishna@okta.com>
2025-09-29 14:55:30 +0530

    Fixed lint issues

--- packages/@okta/vuepress-theme-prose/components/Error404.vue
+++ packages/@okta/vuepress-theme-prose/components/Error404.vue
@@ -3,9 +3,12 @@
     <section class="errors">
       <div class="error404--container">
         <div class="errors--leader">
-          <img width="120" height="120" 
-          src="/img/icons/404.png"
-          alt="error 404 icon">
+          <img
+            width="120"
+            height="120" 
+            src="/img/icons/404.png"
+            alt="error 404 icon"
+          >
           <h1 class="errors--title">
             Oops! Page not found!
           </h1>
@@ -21,24 +24,25 @@
             Possible causes:
             <ul>
               <li>
-                <span class="errors--section-paragraph-heading">Truncated URL:</span> Some email clients may break links across lines. Copy and paste the full URL into your browser.</li>
+                <span class="errors--section-paragraph-heading">Truncated URL:</span> Some email clients may break links across lines. Copy and paste the full URL into your browser.
+              </li>
               <li><span class="errors--section-paragraph-heading">Case sensitivity:</span> URLs are case sensitive. For example, <code>File.html</code> and <code>file.html</code> are treated as different files.</li>
             </ul>
           </div>
 
-          <div class="errors--section-paragraph">Archived content on unsupported technologies is available in the <SmartLink :item="{ link: '/archive/' }">
-            archive</SmartLink>.</div>
-          <div class="errors--section-paragraph">For further assistance, please <a href='mailto:info@okta.com'>contact us</a>.</div>
+          <div class="errors--section-paragraph">
+            Archived content on unsupported technologies is available in the
+            <RouterLink
+              :to="'/archive/'"
+            >
+              <span>archive</span>
+            </RouterLink>.
+          </div>
+          <div class="errors--section-paragraph">
+            For further assistance, please <a href="mailto:info@okta.com">contact us</a>.
+          </div>
         </div>
       </div>
     </section>
   </div>
 </template>
-
-<script>
-export default {
-  components: {
-    SmartLink: () => import("../components/SmartLink")
-  },
-}
-</script>
