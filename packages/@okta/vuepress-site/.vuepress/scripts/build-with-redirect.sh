c89a7cb39a1ddacd93763c6796853487245a71fb
ishan-krishna-okta <ishan.krishna@okta.com>
2025-11-06 13:48:38 +0530

    Added _redirect file

--- packages/@okta/vuepress-site/.vuepress/scripts/build-with-redirect.sh
+++ packages/@okta/vuepress-site/.vuepress/scripts/build-with-redirect.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cp .vuepress/scripts/updateBuildScript.js ../../../node_modules/@vuepress/core/lib/node/build/index.js
 cp .vuepress/scripts/addWorkerScript.js ../../../node_modules/@vuepress/core/lib/node/build/worker.js
-vuepress build . && cp _redirect dist/_redirect
+vuepress build . && cp _redirects dist/_redirects
