7f0ed95427b15721a844da66594e4632df3a5d8c
ishan-krishna-okta <ishan.krishna@okta.com>
2025-11-07 12:05:18 +0530

    Addressed review comments

--- packages/@okta/vuepress-site/.vuepress/scripts/build-with-redirect.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cp .vuepress/scripts/updateBuildScript.js ../../../node_modules/@vuepress/core/lib/node/build/index.js
-cp .vuepress/scripts/addWorkerScript.js ../../../node_modules/@vuepress/core/lib/node/build/worker.js
-vuepress build . && cp _redirects dist/_redirects
