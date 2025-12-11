efded886257829044c840c5ee5aff200cc4ac4b7
ishan-krishna-okta <ishan.krishna@okta.com>
2025-12-11 21:39:05 +0530

    Test slt

--- scripts/test-new-token.sh
+++ scripts/test-new-token.sh
@@ -2,12 +2,7 @@
 
 export AURM_BASE_URL="https://api.aurm.prod.aue1k.saasure.net"
 export ES_APP_NAME="info-dev-bot-okta"
-export AURM_ACCESS_KEY=${ACCESS_KEY}
-export AURM_SECRET_KEY=${SECRET_KEY}
-
-ocm --version
-
-export AURM_TOKEN=$(ocm auth aurm -l --force --verbose)
+export AURM_TOKEN=$(generate_aurm_token "$ACCESS_KEY" "$SECRET_KEY")
 
 export GH_TOKEN_RESPONSE=$(curl --ssl-no-revoke --location 'https://aurm-prod.aue1e.saasure.net/services/github/resources/'${ES_APP_NAME}'/credential-source/vend' \
     --header 'x-aurm-token: '${AURM_TOKEN}'' \
@@ -21,6 +16,8 @@ export GH_TOKEN_RESPONSE=$(curl --ssl-no-revoke --location 'https://aurm-prod.au
 export GITHUB_TOKEN=$(echo $GH_TOKEN_RESPONSE | jq -r '.result.token')
 
 echo "Obtained GitHub token from AURM ${GITHUB_TOKEN}"
+
+setup_github_token
 
 # export OUTPUT_REPO=output_repo
 # export GIT_REPO=https://oauth2:${GITHUB_TOKEN}@github.com/atko-eng/okta-help.git
