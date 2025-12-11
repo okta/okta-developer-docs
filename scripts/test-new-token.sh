a8aa7da688a922467e5fa62fb9e76565c5d7bcd7
ishan-krishna-okta <ishan.krishna@okta.com>
2025-12-11 21:41:24 +0530

    Test slt

--- scripts/test-new-token.sh
+++ scripts/test-new-token.sh
@@ -2,7 +2,18 @@
 
 export AURM_BASE_URL="https://api.aurm.prod.aue1k.saasure.net"
 export ES_APP_NAME="info-dev-bot-okta"
-export AURM_TOKEN=$(generate_aurm_token "$ACCESS_KEY" "$SECRET_KEY")
+export ACCESS_KEY=""
+export SECRET_KEY=""
+
+get_vault_secret_key repo_gh-atko-eng-info-dev/default ACCESS_KEY ACCESS_KEY
+get_vault_secret_key repo_gh-atko-eng-info-dev/default SECRET_KEY SECRET_KEY
+
+set -x
+
+export AURM_SECRET_KEY=$SECRET_KEY
+export AURM_ACCESS_KEY=$ACCESS_KEY
+
+export AURM_TOKEN=$(generate_aurm_token "$AURM_ACCESS_KEY" "$AURM_SECRET_KEY")
 
 export GH_TOKEN_RESPONSE=$(curl --ssl-no-revoke --location 'https://aurm-prod.aue1e.saasure.net/services/github/resources/'${ES_APP_NAME}'/credential-source/vend' \
     --header 'x-aurm-token: '${AURM_TOKEN}'' \
