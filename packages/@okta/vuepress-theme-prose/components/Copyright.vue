492b3b04e2e3d134c9c7601afaea77bbc870d34f
Brian Duffield <brian.duffield@okta.com>
2026-02-12 17:53:59 -0500

    update copyright after incorrect merge conflict fix

--- packages/@okta/vuepress-theme-prose/components/Copyright.vue
+++ packages/@okta/vuepress-theme-prose/components/Copyright.vue
@@ -9,30 +9,12 @@
         </div>
       </div>
       <div class="copyright--container">
-        <div class="copyright--text">
-          <p
-            v-for="(section, sectionIdx) in processedSections"
-            :key="sectionIdx"
-            class="copyright--section-paragraph"
-            v-html="section"
-          />
-        </div>
+        <Content class="copyright--text" />
       </div>
     </section>
   </div>
 </template>
 
 <script>
-export default {
-  computed: {
-    year () {
-      return new Date().getFullYear();
-    },
-    processedSections () {
-      return this.$page.frontmatter.sections.map(section => 
-        section.replace(/\{\{\s*year\s*\}\}/g, this.year)
-      );
-    }
-  }
-}
+export default {};
 </script>
