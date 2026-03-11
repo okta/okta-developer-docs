9c1593142dfde4fd518c1ce53a6be9437173af67
ishan-krishna-okta <ishan.krishna@okta.com>
2026-03-11 21:46:16 +0530

    Final changes

--- scripts/build-preview.sh
+++ scripts/build-preview.sh
@@ -11,6 +11,10 @@ if [ -z "$NETLIFY_AUTH_TOKEN" ] || [ -z "$NETLIFY_SITE_ID" ]; then
   exit 1
 fi
 
+Xvfb :99 -screen 0 1366x768x16 &
+yum update -y
+yum -y install gtk2-2.24* xorg-x11-server-Xvfb libXtst* libXScrnSaver* GConf2* alsa-lib* gtk3
+
 setup_service node v16.14.0
 setup_service yarn 1.22.22
 
