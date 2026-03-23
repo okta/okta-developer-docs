049ba1556fa16f1580b746c697186d827af025b5
Anne Juan <anne.juan@okta.com>
2026-03-23 12:03:57 -0700

    Remove Brokered Consent label

--- packages/@okta/vuepress-theme-prose/util/frameworks.js
+++ packages/@okta/vuepress-theme-prose/util/frameworks.js
@@ -104,7 +104,7 @@ const COMMON_NAME_TO_FANCY_NAME = {
   authserver: 'Authorization server (ID-JAG)',
   secret: 'Secret',
   'service-account': 'Service account',
-  application: 'Application (Brokered Consent)',
+  application: 'Application',
 };
 
 const COMMON_NAME_TO_ICON_NAME = {
