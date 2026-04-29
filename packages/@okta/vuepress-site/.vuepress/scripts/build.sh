386edd185efd6d7c68257fa87eed92e90dfb392b
Thomas Cavanagh <thomas.cavanagh@okta.com>
2026-04-29 13:03:29 -0400

    Rebase

--- packages/@okta/vuepress-site/.vuepress/scripts/build.sh
+++ packages/@okta/vuepress-site/.vuepress/scripts/build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 export NODE_OPTIONS="--max-old-space-size=4096"
-export BUILD_WORKER_THREADS="${BUILD_WORKER_THREADS:-2}"
 cp .vuepress/scripts/updateBuildScript.js ../../../node_modules/@vuepress/core/lib/node/build/index.js
 cp .vuepress/scripts/addWorkerScript.js ../../../node_modules/@vuepress/core/lib/node/build/worker.js
 vuepress build . && cp conductor.yml dist/conductor.yml && node .vuepress/scripts/fix-robots.js
