b0a21b8a0db7414ebd9fe9e76dfb2c9e7a8dc2dc
ishan-krishna-okta <154415906+ishan-krishna-okta@users.noreply.github.com>
2026-05-12 11:18:24 +0530

    OKTA-1156924: Added new signup form (#6140)
    * OKTA-1156924: Added new signup form
    
    * Fixed lint errors
    
    * Added hover state, tooltip to builder type cards
    
    * Fixed lint error
    
    * Fixed cypress test

---
+++ packages/@okta/vuepress-theme-prose/components/signup/SignUpHeroSection.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="signup-hero">
+    <h1 class="signup-hero__title">
+      Start your Integration<br>
+      building journey.
+    </h1>
+
+    <div class="signup-hero__features">
+      <SignUpFeatureCard
+        title="Build for Security"
+        description="Leverage industry standards to meet enterprise identity demands like SSO, provisioning, AI governance, session termination, and more."
+        :icon="theme === 'dark' ? '/img/signup/secured-laptop-dark.svg' : '/img/signup/secured-laptop.svg'"
+      />
+      <SignUpFeatureCard
+        title="Attract Enterprise Customers"
+        description="Build credibility with an OIN listing that shows enterprise buyers your integration is tested, validated, and ready to deploy."
+        :icon="theme === 'dark' ? '/img/signup/container-bucket-dark.svg' : '/img/signup/container-bucket.svg'"
+      />
+    </div>
+
+    <div class="signup-hero__divider" />
+
+    <SignUpPartnerLogos :theme="theme" />
+  </div>
+</template>
+
+<script>
+import SignUpFeatureCard from "./SignUpFeatureCard.vue";
+import SignUpPartnerLogos from "./SignUpPartnerLogos.vue";
+
+export default {
+  name: "SignUpHeroSection",
+  components: {
+    SignUpFeatureCard,
+    SignUpPartnerLogos,
+  },
+  props: {
+    theme: {
+      type: String,
+      default: 'light',
+    },
+  },
+};
+</script>
