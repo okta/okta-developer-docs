743136224fd9245914da439af3678029cf05006c
ishan-krishna-okta <ishan.krishna@okta.com>
2026-05-15 12:07:52 +0530

    OKTA-1175576: Added cypress tests for new signup page

--- tests/cypress/integration/signup.cy.js
+++ tests/cypress/integration/signup.cy.js
@@ -1,91 +1,304 @@
-import { DialogWindow } from "../page-objects/DialogWindow";
 import { SignUpPage } from "../page-objects/SignUpPage";
 
-describe('Sign up page check spec', () => {
+describe('Sign up page', () => {
   const signUpPage = new SignUpPage();
-  const dialogWindow = new DialogWindow();
 
   beforeEach(() => {
     signUpPage.visitSignUpPage();
   });
 
-  // it('should show all the fields and buttons', () => {
-  //   signUpPage.getEmailInput().should('exist');
-  //   signUpPage.getFirsNameInput().should('exist');
-  //   signUpPage.getLastNameInput().should('exist');
-  //   signUpPage.getCountrySelect().should('exist');
-  //   signUpPage.getGithubButtonInput().should('exist');
-  //   signUpPage.getGoogleButtonInput().should('exist');
-  //   signUpPage.getSignUpSubmitInput().should('exist');
-  //   signUpPage.getGoogleCaptcha().its('0.contentDocument').should('exist');
-  //   signUpPage.getSignInLink().should('exist');
-  // });
+  describe('page structure', () => {
+    it('should render the signup section', () => {
+      signUpPage.getSignupSection().should('exist').and('be.visible');
+    });
 
-  // it('should show agree checkbox for Canada', () => {
-  //   signUpPage.getCountrySelect().select("Canada");
-  //   signUpPage.getAgreeCheckbox().should('be.visible');
-  // });
+    it('should display the mobile title', () => {
+      signUpPage.getMobileTitle().should('exist');
+      signUpPage.getMobileTitle().should('contain.text', 'Start your Integration');
+    });
 
-  // it('should show Province selection for Canada', () => {
-  //   signUpPage.getCountrySelect().select('Canada');
-  //   signUpPage.getStateProviceSelect().should('be.visible');
-  //   cy.get('label[for="state"]').should(($label) => {
-  //     expect($label[0].firstChild.data.trim()).equal('Province');
-  //   })
-  // });
+    it('should display the form heading', () => {
+      signUpPage.getFormHeading().should('exist');
+      signUpPage.getFormHeading().should('contain.text', 'Create your integrator free plan now!');
+    });
+  });
 
-  // it('shows State selection for United States', () => {
-  //   signUpPage.getCountrySelect().select('United States');
-  //   signUpPage.getStateProviceSelect().should('be.visible');
-  //   cy.get('label[for="state"]').should(($label) => {
-  //     expect($label[0].firstChild.data.trim()).equal('State');
-  //   })
-  // });
+  describe('form fields', () => {
+    it('should render all required form inputs', () => {
+      signUpPage.getFirstNameInput().should('exist').and('be.visible');
+      signUpPage.getLastNameInput().should('exist').and('be.visible');
+      signUpPage.getEmailInput().should('exist').and('be.visible');
+      signUpPage.getDepartmentSelect().should('exist').and('be.visible');
+      signUpPage.getCountrySelect().should('exist').and('be.visible');
+    });
 
-  // it('does not shows State selection for Albania', () => {
-  //   signUpPage.getCountrySelect().select('Albania');
-  //   signUpPage.getStateProviceSelect().should('not.exist');
-  // });
+    it('should show correct placeholder for first name', () => {
+      signUpPage.getFirstNameInput().should('have.attr', 'placeholder', 'E.g. John');
+    });
 
-  // it('does not shows agree checkbox for United States', () => {
-  //   signUpPage.getCountrySelect().select('United States');
+    it('should show correct placeholder for last name', () => {
+      signUpPage.getLastNameInput().should('have.attr', 'placeholder', 'E.g. Doe');
+    });
 
-  //   signUpPage.getAgreeCheckbox().should('not.be.visible');
-  // });
+    it('should show correct placeholder for email', () => {
+      signUpPage.getEmailInput().should('have.attr', 'placeholder', 'E.g. john@okta.com');
+    });
 
-  // describe('verify button clicks', () => {
-  //   it('sign up with github button click display dialog window for "Terms and Conditions"', () => {
-  //     signUpPage.getGithubButtonInput().click();
-
-  //     dialogWindow.getDialogWindow().should('exist');
-  //     dialogWindow.getDialogWindow().contains('Tell us more about yourself');
-  //   });
-
-  //   it('sign up with google button click display dialog window for "Terms and Conditions"', () => {
-  //     signUpPage.getGoogleButtonInput().click();
-
-  //     dialogWindow.getDialogWindow().should('exist');
-  //     dialogWindow.getDialogWindow().contains('Tell us more about yourself');
-  //   });
-
-  //   it('should contain proper router for signin link', () => {
-  //     signUpPage.getSignInLink().should('have.attr', 'href')
-  //                               .and('eq', '/login/');
-  //   });
-  // });
-
-  describe('verify button text', () => {
-    it('email signup button text verification', () => {
+    it('should have the submit button with value "Sign Up"', () => {
       signUpPage.getSignUpSubmitInput().should('have.value', 'Sign Up');
     });
 
-    // it('github signup button text verification', () => {
-    //   signUpPage.getGithubButtonInput().should('have.text', ' Continue with GitHub\n            ');
-    // });
+    it('should render the recaptcha widget', () => {
+      signUpPage.getRecaptcha().should('exist');
+    });
+  });
 
-    // it('google signup button text verification', () => {
-    //   signUpPage.getGoogleButtonInput().should('have.text', ' Continue with Google\n            ');
-    // });
-  })
+  describe('department dropdown', () => {
+    const expectedDepartments = [
+      'Engineering',
+      'Product',
+      'Developer Relations',
+      'Sales',
+      'Marketing',
+      'Security',
+      'IT',
+      'Operations',
+      'Legal',
+      'Others',
+    ];
 
+    it('should contain all department options', () => {
+      expectedDepartments.forEach((dept) => {
+        signUpPage.getDepartmentSelect().find('option').should('contain.text', dept);
+      });
+    });
+
+    it('should have "Select Department" as the default disabled option', () => {
+      signUpPage.getDepartmentSelect()
+        .find('option:first-child')
+        .should('contain.text', 'Select Department')
+        .and('be.disabled');
+    });
+  });
+
+  describe('builder type descriptors', () => {
+    it('should display 4 descriptor options', () => {
+      signUpPage.getDescriptorOptions().should('have.length', 4);
+    });
+
+    it('should contain expected descriptor labels', () => {
+      const labels = [
+        'Independent Software Vendor (ISV)',
+        'Solution Builders (GSI, SI, MSP, Solution Provider)',
+        'Other Developers Exploring APIs, SDKs etc',
+        'Existing Customers at Okta',
+      ];
+      labels.forEach((label) => {
+        signUpPage.getDescriptorByLabel(label).should('exist');
+      });
+    });
+
+    it('should mark a descriptor as selected when clicked', () => {
+      signUpPage.getDescriptorByLabel('Independent Software Vendor (ISV)').click();
+
+      signUpPage.getSelectedDescriptor().should('have.length', 1);
+      signUpPage.getSelectedDescriptor().should('contain.text', 'Independent Software Vendor (ISV)');
+    });
+
+    it('should deselect previous descriptor when another is clicked', () => {
+      signUpPage.getDescriptorByLabel('Independent Software Vendor (ISV)').click();
+      signUpPage.getSelectedDescriptor().should('contain.text', 'Independent Software Vendor (ISV)');
+
+      signUpPage.getDescriptorByLabel('Existing Customers at Okta').click();
+      signUpPage.getSelectedDescriptor().should('have.length', 1);
+      signUpPage.getSelectedDescriptor().should('contain.text', 'Existing Customers at Okta');
+    });
+
+    it('should show a tooltip on hover of the info icon', () => {
+      signUpPage.getDescriptorOptions().first().find('.signup-descriptor__info').trigger('mouseenter');
+      signUpPage.getDescriptorTooltip().should('be.visible');
+    });
+
+    it('should hide the tooltip on mouse leave', () => {
+      signUpPage.getDescriptorOptions().first().find('.signup-descriptor__info').trigger('mouseenter');
+      signUpPage.getDescriptorTooltip().should('be.visible');
+
+      signUpPage.getDescriptorOptions().first().find('.signup-descriptor__info').trigger('mouseleave');
+      signUpPage.getDescriptorTooltip().should('not.exist');
+    });
+  });
+
+  describe('country and state interaction', () => {
+    it('should show State select when United States is chosen', () => {
+      signUpPage.getCountrySelect().select('United States');
+
+      signUpPage.getStateSelect().should('exist').and('be.visible');
+      cy.get('label[for="state"] .signup-form__label').should(($label) => {
+        expect($label.text()).to.contain('State');
+      });
+    });
+
+    it('should show Province select when Canada is chosen', () => {
+      signUpPage.getCountrySelect().select('Canada');
+
+      signUpPage.getStateSelect().should('exist').and('be.visible');
+      cy.get('label[for="state"] .signup-form__label').should(($label) => {
+        expect($label.text()).to.contain('Province');
+      });
+    });
+
+    it('should not show state/province select for other countries', () => {
+      signUpPage.getCountrySelect().select('Albania');
+
+      signUpPage.getStateSelect().should('not.exist');
+    });
+  });
+
+  describe('consent checkbox', () => {
+    it('should show agree checkbox for a GDPR country', () => {
+      signUpPage.getCountrySelect().select('Germany');
+      signUpPage.getAgreeCheckbox().should('be.visible');
+    });
+
+    it('should show agree checkbox for Canada', () => {
+      signUpPage.getCountrySelect().select('Canada');
+      signUpPage.getAgreeCheckbox().should('be.visible');
+    });
+
+    it('should not show agree checkbox for United States', () => {
+      signUpPage.getCountrySelect().select('United States');
+      signUpPage.getAgreeCheckbox().should('not.be.visible');
+    });
+  });
+
+  describe('email validation', () => {
+    it('should show error when email is left blank', () => {
+      signUpPage.getEmailInput().focus().blur();
+
+      cy.get('label[for="email"] .signup-form__error').should('contain.text', 'This field is required.');
+    });
+
+    it('should show error for an invalid email format', () => {
+      signUpPage.getEmailInput().type('not-an-email').blur();
+
+      cy.get('label[for="email"] .signup-form__error').should('contain.text', 'Invalid email.');
+    });
+
+    it('should clear error for a valid email format', () => {
+      signUpPage.getEmailInput().type('not-an-email').blur();
+      cy.get('label[for="email"] .signup-form__error').should('contain.text', 'Invalid email.');
+
+      signUpPage.getEmailInput().clear().type('john@okta.com').blur();
+      cy.get('label[for="email"] .signup-form__error').should('not.exist');
+    });
+  });
+
+  describe('empty field validation', () => {
+    it('should show error when first name is left blank', () => {
+      signUpPage.getFirstNameInput().focus().blur();
+
+      signUpPage.getFieldErrors().should('contain.text', 'This field cannot be left blank');
+    });
+
+    it('should show error when last name is left blank', () => {
+      signUpPage.getLastNameInput().focus().blur();
+
+      signUpPage.getFieldErrors().should('contain.text', 'This field cannot be left blank');
+    });
+
+    it('should clear first name error once a value is entered', () => {
+      signUpPage.getFirstNameInput().focus().blur();
+      signUpPage.getFieldErrors().should('contain.text', 'This field cannot be left blank');
+
+      signUpPage.getFirstNameInput().type('John').blur();
+      cy.get('label[for="firstName"] .signup-form__error').should('not.exist');
+    });
+
+    it('should clear last name error once a value is entered', () => {
+      signUpPage.getLastNameInput().focus().blur();
+      signUpPage.getFieldErrors().should('contain.text', 'This field cannot be left blank');
+
+      signUpPage.getLastNameInput().type('Doe').blur();
+      cy.get('label[for="lastName"] .signup-form__error').should('not.exist');
+    });
+
+    it('should clear department error once a value is selected', () => {
+      cy.get('form#signupForm').submit();
+      cy.get('label[for="department"] .signup-form__error')
+        .should('contain.text', 'This field cannot be left blank');
+
+      signUpPage.getDepartmentSelect().select('Engineering');
+      cy.get('label[for="department"] .signup-form__error').should('not.exist');
+    });
+  });
+
+  describe('login link', () => {
+    it('should display login link with correct href', () => {
+      signUpPage.getLoginLink().should('exist');
+      signUpPage.getLoginLink().should('have.attr', 'href', '/login');
+      signUpPage.getLoginLink().should('contain.text', 'Log in here');
+    });
+  });
+
+  describe('hero section', () => {
+    it('should display the hero title', () => {
+      signUpPage.getHeroTitle().should('exist');
+      signUpPage.getHeroTitle().invoke('text').then((text) => {
+        expect(text.replace(/\s+/g, ' ').trim()).to.equal('Start your Integration building journey.');
+      });
+    });
+
+    it('should display 2 feature cards', () => {
+      signUpPage.getFeatureCards().should('have.length', 2);
+    });
+
+    it('should display partner logos', () => {
+      signUpPage.getPartnerLogos().should('have.length.greaterThan', 0);
+    });
+  });
+
+  describe('bottom cards', () => {
+    it('should display 2 bottom cards', () => {
+      signUpPage.getBottomCards().should('have.length', 2);
+    });
+
+    it('should show Okta card with correct CTA text', () => {
+      signUpPage.getBottomCardCta('okta').should('contain.text', 'Try Okta Platform');
+    });
+
+    it('should show Auth0 card with correct CTA text', () => {
+      signUpPage.getBottomCardCta('auth0').should('contain.text', 'Try Auth0 Platform');
+    });
+
+    it('should have correct Okta CTA link', () => {
+      signUpPage.getBottomCardCta('okta')
+        .should('have.attr', 'href', 'https://okta.com/free-trial/workforce-identity');
+    });
+
+    it('should have correct Auth0 CTA link', () => {
+      signUpPage.getBottomCardCta('auth0')
+        .should('have.attr', 'href')
+        .and('contain', 'https://auth0.com/signup');
+    });
+  });
+
+  describe('consent text', () => {
+    it('should display the terms and privacy policy consent text', () => {
+      signUpPage.getConsentText().should('exist');
+      signUpPage.getConsentText().should('contain.text', 'By clicking "Sign up," I agree');
+      signUpPage.getConsentText().should('contain.text', 'Privacy Policy');
+    });
+
+    it('should link to the Integrator Free Plan Agreement', () => {
+      signUpPage.getConsentText().find('a').first()
+        .should('have.attr', 'href', '/terms/')
+        .and('contain.text', 'Integrator Free Plan Agreement');
+    });
+
+    it('should link to the Privacy Policy', () => {
+      signUpPage.getConsentText().find('a').last()
+        .should('have.attr', 'href', 'https://www.okta.com/privacy-policy')
+        .and('contain.text', 'Privacy Policy');
+    });
+  });
 });
