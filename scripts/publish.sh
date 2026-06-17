b3664ee10696072df5a85a729d44ac199c4885e3
ishan-krishna-okta <ishan.krishna@okta.com>
2026-06-17 16:21:44 +0530

    Added slack notification for prod deployment

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -15,7 +15,25 @@ if [[ "$BRANCH" != "master" ]]; then
     exit ${SUCCESS}
 fi
 
+export BACON_ARTIFACT="okta-developer-docs"
+export BACON_LINK="https://bacon-go.aue1e.saasure.net/commits?artifact=${BACON_ARTIFACT}&sha=${SHA}"
+export SLACK_CHANNEL='#tmp-test-slack-notif'
+
+on_failure() {
+  local exit_code=$?
+  if [[ $exit_code -ne 0 ]]; then
+    send_slack_message "${SLACK_CHANNEL}" \
+      "Production deployment failed for developer docs " \
+      "Please check bacon task (${BACON_LINK}) for more details. " \
+      "danger"
+  fi
+}
+trap on_failure EXIT
+
 DEPLOY_ENVIRONMENT="vuepress-site-prod"
+
+# test exit with error
+exit ${BUILD_FAILURE}
 
 # "${MAIN_BRANCH}" branch indecates that current deploy is for production.
 # In such case, PROD will take 'prod' value.
