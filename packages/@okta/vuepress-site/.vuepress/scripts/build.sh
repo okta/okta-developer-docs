a30770a3a806bf6b87dde5930c97daa63788973b
Adith A Danthi <adith.danthi@okta.com>
2026-07-27 15:02:52 +0530

    build: add logging for NODE_OPTIONS and V8 heap limit in build script

--- packages/@okta/vuepress-site/.vuepress/scripts/build.sh
+++ packages/@okta/vuepress-site/.vuepress/scripts/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+echo "[build.sh] NODE_OPTIONS inherited from environment: '${NODE_OPTIONS}'"
 export NODE_OPTIONS="${NODE_OPTIONS:-"--max-old-space-size=6144"}"
+echo "[build.sh] NODE_OPTIONS in effect: '${NODE_OPTIONS}'"
+node -e "console.log('[build.sh] effective V8 heap limit (MB):', Math.round(require('v8').getHeapStatistics().heap_size_limit/1024/1024))"
 export BUILD_WORKER_THREADS="${BUILD_WORKER_THREADS:-2}"
 cp .vuepress/scripts/updateBuildScript.js ../../../node_modules/@vuepress/core/lib/node/build/index.js
 cp .vuepress/scripts/addWorkerScript.js ../../../node_modules/@vuepress/core/lib/node/build/worker.js
