aafcd0f80c5a171a29b51ff212b4b47c87ddac78
Van Ngo <van.ngo@okta.com>
2026-08-04 18:45:26 -0400

    Update nav title

--- packages/@okta/vuepress-theme-prose/const/navbar.const.js
+++ packages/@okta/vuepress-theme-prose/const/navbar.const.js
@@ -833,7 +833,7 @@ export const guides = [
             ]
           },
           {
-            title: "XAA",
+            title: "Cross App Access (XAA)",
             subLinks: [
               {
                 title: "Expose XAA metadata for your resource app",
