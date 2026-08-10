fd7c83a967029742dd4c2f3927fcf6f30e6bfb4d
Adith A Danthi <adith.danthi@okta.com>
2026-08-10 16:36:01 +0530

    build: update resource class for CircleCI jobs (#6376)
    * build: update resource class for node_build_checks job to arm.large
    
    * build: update resource class for node_build_checks job and increase NODE_OPTIONS memory limit

--- packages/@okta/vuepress-site/.vuepress/scripts/build.sh
+++ packages/@okta/vuepress-site/.vuepress/scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "[build.sh] NODE_OPTIONS inherited from environment: '${NODE_OPTIONS}'"
-export NODE_OPTIONS="${NODE_OPTIONS:-"--max-old-space-size=6144"}"
+export NODE_OPTIONS="${NODE_OPTIONS:-"--max-old-space-size=8192"}"
 echo "[build.sh] NODE_OPTIONS in effect: '${NODE_OPTIONS}'"
 node -e "console.log('[build.sh] effective V8 heap limit (MB):', Math.round(require('v8').getHeapStatistics().heap_size_limit/1024/1024))"
 export BUILD_WORKER_THREADS="${BUILD_WORKER_THREADS:-2}"
